166e9b98f68940330e1e0af9adcde36c1283247e
Bernardo <programming@bernardomg.com>
2022-08-19 23:41:28 +0200

    Removed back button

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -2,13 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormControlsComponent } from './form-controls/form-controls.component';
-import { NavigationReturnButtonComponent } from './navigation-return-button/navigation-return-button.component';
 
 
 
 @NgModule({
   declarations: [
-    NavigationReturnButtonComponent,
     FormControlsComponent
   ],
   imports: [
@@ -16,7 +14,6 @@ import { NavigationReturnButtonComponent } from './navigation-return-button/navi
     FontAwesomeModule
   ],
   exports: [
-    NavigationReturnButtonComponent,
     FormControlsComponent
   ]
 })
