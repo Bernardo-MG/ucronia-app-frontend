7d382e5eddd2f91aea13b25036dce02bc490c470
Bernardo <programming@bernardomg.com>
2022-08-21 23:33:41 +0200

    Fixed test dependencies

--- src/app/admin/members/views/admin-member-list-view/admin-member-list-view.component.spec.ts
+++ src/app/admin/members/views/admin-member-list-view/admin-member-list-view.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -10,7 +11,8 @@ describe('AdminMemberListViewComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AdminMemberListViewComponent
