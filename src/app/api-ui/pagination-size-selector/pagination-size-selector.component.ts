c7dc01dfb449bad5bb7e6ab0c31b88b56feda010
Bernardo <programming@bernardomg.com>
2022-09-08 01:52:27 +0200

    Selects initial size

--- src/app/api-ui/pagination-size-selector/pagination-size-selector.component.ts
+++ src/app/api-ui/pagination-size-selector/pagination-size-selector.component.ts
@@ -1,17 +1,21 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'pagination-size-selector',
   templateUrl: './pagination-size-selector.component.html',
   styleUrls: ['./pagination-size-selector.component.sass']
 })
-export class PaginationSizeSelectorComponent {
+export class PaginationSizeSelectorComponent implements OnInit {
 
   public sizes: number[] = [5, 10, 15, 20];
-  
+
   @Output() select = new EventEmitter<number>();
 
   constructor() { }
+
+  ngOnInit(): void {
+    this.select.emit(this.sizes[0]);
+  }
 
   public selectSize(event: any) {
     if (event.value) {
