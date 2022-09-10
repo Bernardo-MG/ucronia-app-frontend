58861ea8e8cb22cc037d7f582dd811858e76f0ae
Bernardo <programming@bernardomg.com>
2022-09-10 13:51:30 +0200

    Added all pagination fields to page info

--- src/app/api/models/page-info.ts
+++ src/app/api/models/page-info.ts
@@ -1,6 +1,8 @@
 export class PageInfo {
     page: number = 0;
     size: number = 0;
+    elementsInPage: number = 0;
+    totalElements: number = 0;
     totalPages: number = 0;
     first: boolean = false;
     last: boolean = false;
--- src/app/api/models/paginated-response.ts
+++ src/app/api/models/paginated-response.ts
@@ -1,11 +1,11 @@
 import { ApiResponse } from "./api-response";
 
 export class PaginatedResponse<T> extends ApiResponse<T> {
-    first: boolean = false;
-    last: boolean = false;
     page: number = 0;
-    elementsInPage: number = 0;
     size: number = 0;
+    elementsInPage: number = 0;
     totalElements: number = 0;
     totalPages: number = 0;
+    first: boolean = false;
+    last: boolean = false;
 }
