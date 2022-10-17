3ca23667468b8456b2ce440a36a7d39b6dda6aff
Bernardo <programming@bernardomg.com>
2022-10-17 08:37:27 +0200

    Extended test

--- src/app/api-ui/components/order-button-template/order-button-template.component.spec.ts
+++ src/app/api-ui/components/order-button-template/order-button-template.component.spec.ts
@@ -26,33 +26,41 @@ describe('OrderButtonTemplateComponent', () => {
 
   it('should send an event to change to descending direction when clicking for the first time', () => {
     spyOn(component.descending, 'emit');
-    spyOn(component.ascending, 'emit');
 
     const option = fixture.debugElement.query(By.css('button'));
     option.triggerEventHandler('click');
 
-    expect(component.descending.emit).toHaveBeenCalled();
+    expect(component.descending.emit).toHaveBeenCalledTimes(1);
   });
 
   it('should not send an event to change to ascending direction when clicking for the first time', () => {
-    spyOn(component.descending, 'emit');
     spyOn(component.ascending, 'emit');
 
     const option = fixture.debugElement.query(By.css('button'));
     option.triggerEventHandler('click');
 
-    expect(component.ascending.emit).not.toHaveBeenCalled();
+    expect(component.ascending.emit).not.toHaveBeenCalledTimes(1);
   });
 
   it('should send an event to change to ascending direction when clicking for the second time', () => {
-    spyOn(component.descending, 'emit');
     spyOn(component.ascending, 'emit');
 
     const option = fixture.debugElement.query(By.css('button'));
     option.triggerEventHandler('click');
     option.triggerEventHandler('click');
 
-    expect(component.ascending.emit).toHaveBeenCalled();
+    expect(component.ascending.emit).toHaveBeenCalledTimes(1);
+  });
+
+  it('should send an event to loop back to descending direction when clicking for the third time', () => {
+    spyOn(component.descending, 'emit');
+
+    const option = fixture.debugElement.query(By.css('button'));
+    option.triggerEventHandler('click');
+    option.triggerEventHandler('click');
+    option.triggerEventHandler('click');
+
+    expect(component.descending.emit).toHaveBeenCalledTimes(2);
   });
 
 });
