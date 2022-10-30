40b28e82332237492cdf2ccb7579e34907f1cc9c
Bernardo-MG <programming@bernardo-mg.com>
2022-10-30 11:52:09 +0100

    Fixed login routes

--- src/app/security/login/login.module.ts
+++ src/app/security/login/login.module.ts
@@ -5,6 +5,7 @@ import { AuthenticationModule } from '@app/authentication/authentication.module'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 import { LogoutButtonComponent } from './containers/logout-button/logout-button.component';
+import { LoginRoutingModule } from './login-routing.module';
 import { LoginService } from './service/login.service';
 import { LoginViewComponent } from './view/login-view/login-view.component';
 
@@ -17,6 +18,7 @@ import { LoginViewComponent } from './view/login-view/login-view.component';
     LoginViewComponent
   ],
   imports: [
+    LoginRoutingModule,
     FontAwesomeModule,
     AuthenticationModule,
     CommonModule,
