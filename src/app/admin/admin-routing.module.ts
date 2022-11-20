c7b5cdfa6a09d9817b6c06b3af3ed425673f5cf5
Bernardo <programming@bernardomg.com>
2022-11-20 16:21:03 +0100

    Added sidenav

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const dashboardModule = () => import('@app/admin/dashboard/admin-dashboard.module').then(m => m.AdminDashboardModule);
 const balanceModule = () => import('@app/admin/balance/balance.module').then(m => m.BalanceModule);
 const feeModule = () => import('@app/admin/fees/admin-fees.module').then(m => m.AdminFeesModule);
 const memberModule = () => import('@app/admin/members/admin-members.module').then(m => m.AdminMembersModule);
@@ -11,7 +10,6 @@ const routes: Routes = [
     {
         path: '',
         children: [
-            { path: '', loadChildren: dashboardModule },
             { path: 'balance', loadChildren: balanceModule },
             { path: 'fees', loadChildren: feeModule },
             { path: 'members', loadChildren: memberModule }
