28455d59c399b87a5352df66474deed4d9569128
Bernardo-MG <programming@bernardo-mg.com>
2022-11-20 01:11:29 +0100

    Removed unused lifecycle hook

--- src/app/navigation/navigation-side-menu/navigation-side-menu.component.ts
+++ src/app/navigation/navigation-side-menu/navigation-side-menu.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'navigation-side-menu',
   templateUrl: './navigation-side-menu.component.html',
   styleUrls: ['./navigation-side-menu.component.sass']
 })
-export class NavigationSideMenuComponent implements OnInit {
+export class NavigationSideMenuComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
