2bacca39fc78daeeb003221f282fd92b94914144
Bernardo-MG <programming@bernardo-mg.com>
2022-12-08 23:08:38 +0100

    Delete confirm with modal

--- src/app/crud/members/views/member-list-view/member-list-view.component.ts
+++ src/app/crud/members/views/member-list-view/member-list-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageInfo } from '@app/api/models/page-info';
 import { PaginationRequest } from '@app/api/models/pagination-request';
@@ -24,6 +24,8 @@ export class MemberListViewComponent implements OnInit {
   public pageInfo = new PageInfo();
 
   private routePaginationObserver: RoutePaginationRequestObserver;
+
+  private selected = new Member();
 
   constructor(
     private service: MemberService,
@@ -52,6 +54,19 @@ export class MemberListViewComponent implements OnInit {
 
   public isLoading(): boolean {
     return this.loading;
+  }
+
+  public select(data: Member) {
+    this.selected = data;
+  }
+
+  public onDelete() {
+    if (this.selected.id > 0) {
+      this.service.delete(this.selected.id).subscribe(r => {
+        const pagination = this.routePaginationObserver.pagination.value;
+        this.load(pagination);
+      });
+    }
   }
 
   private load(pagination: PaginationRequest | undefined) {
