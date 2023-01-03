8c75bb61bc0808b62b640ac1287fe6e09d869134
Bernardo-MG <programming@bernardo-mg.com>
2023-01-03 23:40:24 +0100

    Cleaned up code

--- src/app/route/actuator/url-parameters-extractor.ts
+++ src/app/route/actuator/url-parameters-extractor.ts
@@ -37,25 +37,33 @@ export class UrlParamsExtractor {
         return resultUrlParams;
     }
 
-    private addParameters(pair: string[], resultUrlParams: { [key: string]: any }) {
-        if (pair[0] in resultUrlParams) {
+    /**
+     * Adds the parameters pair to the received store.
+     * 
+     * If the parameter key already exists, then it is added to a list of values. If the value is already a list, then the current value is added to it.
+     * 
+     * @param paramsPair parameter key-value pair
+     * @param paramsStore store containing the resulting parameters
+     */
+    private addParameters(paramsPair: string[], paramsStore: { [key: string]: any }) {
+        if (paramsPair[0] in paramsStore) {
             // The pair already exists in the resulting parameters
             // This is a list of parameters
 
             // Find the existing parameter pair
-            const existingParamsPair = resultUrlParams[pair[0]];
+            const existingParamsPair = paramsStore[paramsPair[0]];
             if (Array.isArray(existingParamsPair)) {
                 // It is an array
                 // Can add the current value
-                existingParamsPair.push(pair[1]);
+                existingParamsPair.push(paramsPair[1]);
             } else {
                 // It isn't an array
                 // Creates a new array with the values and sets it into the result
-                resultUrlParams[pair[0]] = [existingParamsPair, pair[1]];
+                paramsStore[paramsPair[0]] = [existingParamsPair, paramsPair[1]];
             }
         } else {
             // New pair of parameters
-            resultUrlParams[pair[0]] = pair[1];
+            paramsStore[paramsPair[0]] = paramsPair[1];
         }
     }
 
