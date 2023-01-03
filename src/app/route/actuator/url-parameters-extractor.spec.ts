daa09bfd61db5a3ee3295a882b115e77ac956933
Bernardo-MG <programming@bernardo-mg.com>
2023-01-03 23:43:23 +0100

    Added tests

--- src/app/route/actuator/url-parameters-extractor.spec.ts
+++ src/app/route/actuator/url-parameters-extractor.spec.ts
@@ -14,10 +14,22 @@ describe('UrlParamsProcessor', () => {
     extractor = new UrlParamsExtractor();
   });
 
-  it('should extract a param pair', () => {
+  it('should extract no param pairs when there are no parameters', () => {
+    const params = extractor.getUrlParams('route');
+
+    expect(params).toEqual({});
+  });
+
+  it('should extract a param pair when there is only a single pair', () => {
     const params = extractor.getUrlParams('route?key=value');
 
     expect(params).toEqual({ 'key': 'value' });
+  });
+
+  it('should extract a param pair when there are two pairs', () => {
+    const params = extractor.getUrlParams('route?key1=value1&key2=value2');
+
+    expect(params).toEqual({ 'key1': 'value1', 'key2': 'value2' });
   });
 
   it('should extract values for a param key duplicated two times', () => {
