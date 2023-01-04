66feb376491237d0a36fb61878d111aa2d8245e3
Bernardo-MG <programming@bernardo-mg.com>
2023-01-04 20:39:23 +0100

    Simplified code

--- src/app/route/observer/route-params-observer.ts
+++ src/app/route/observer/route-params-observer.ts
@@ -6,17 +6,13 @@ export class RouteParametersObserver<T> {
 
   public subject = new BehaviorSubject<T | undefined>(undefined);
 
-  private parser: ParametersParser<T>;
-
   constructor(
     route: ActivatedRoute,
     parser: ParametersParser<T>
   ) {
-    this.parser = parser;
-
     // Listens to parameter changes
     route.queryParamMap.subscribe(params => {
-      const value = this.parser.parse(params);
+      const value = parser.parse(params);
 
       this.subject.next(value);
     });
