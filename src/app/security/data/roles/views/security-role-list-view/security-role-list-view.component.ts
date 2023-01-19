a67ca8360f32e6ac63b75213ce57d1a416eed832
Bernardo-MG <programming@bernardo-mg.com>
2023-01-19 20:59:28 +0100

    Reusing layout for users and roles

--- src/app/security/data/roles/views/security-role-list-view/security-role-list-view.component.ts
+++ src/app/security/data/roles/views/security-role-list-view/security-role-list-view.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PageInfo } from '@app/api/models/page-info';
+import { PaginationRequest } from '@app/api/models/pagination-request';
 import { PaginationRequestRouteObserver } from '@app/api/route/observer/pagination-request-route-observer';
 import { Role } from '@app/security/models/role';
-import { mergeMap } from 'rxjs';
 import { SecurityRoleService } from '../../service/security-role.service';
 
 @Component({
@@ -13,9 +13,16 @@ import { SecurityRoleService } from '../../service/security-role.service';
 })
 export class SecurityRoleListViewComponent implements OnInit {
 
+  /**
+   * Loading flag.
+   */
+  public loading = false;
+
   public roles: Role[] = [];
 
   public pageInfo = new PageInfo();
+
+  private selected: { id: number } = { id: -1 };
 
   private routePaginationObserver: PaginationRequestRouteObserver;
 
@@ -27,11 +34,42 @@ export class SecurityRoleListViewComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.routePaginationObserver.subject.pipe(mergeMap(p => this.service.getAll(p)))
-      .subscribe(page => {
+    this.routePaginationObserver.subject.subscribe(p => {
+      this.load(p);
+    });
+  }
+
+  public isLoading(): boolean {
+    return (this.roles.length == 0) && this.loading;
+  }
+
+  public select(data: { id: number }) {
+    this.selected = data;
+  }
+
+  public onDelete() {
+    if (this.selected.id > 0) {
+      this.service.delete(this.selected.id).subscribe(r => {
+        const pagination = this.routePaginationObserver.subject.value;
+        this.load(pagination);
+      });
+    }
+  }
+
+  private load(pagination: PaginationRequest | undefined) {
+    this.loading = true;
+    this.service.getAll(pagination).subscribe({
+      next: page => {
         this.roles = page.content;
         this.pageInfo = page;
-      });
+        // Reactivate view
+        this.loading = false;
+      },
+      error: error => {
+        // Reactivate view
+        this.loading = false;
+      }
+    });
   }
 
 }
--- src/app/security/data/users/users.module.ts
+++ src/app/security/data/users/users.module.ts
@@ -3,15 +3,16 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ApiUiModule } from '@app/api-ui/api-ui.module';
 import { ControlsModule } from '@app/controls/controls.module';
+import { LayoutModule } from '@app/layout/layout.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SecurityUserFormComponent } from './components/security-user-form/security-user-form.component';
+import { SecurityUserRoleFormComponent } from './components/security-user-role-form/security-user-role-form.component';
+import { SecurityUserRoleSelectionComponent } from './components/security-user-role-selection/security-user-role-selection.component';
 import { SecurityUserService } from './service/security-user.service';
 import { UserRoutingModule } from './users-routing.module';
 import { SecurityUserCreateViewComponent } from './views/security-user-create-view/security-user-create-view.component';
 import { SecurityUserEditViewComponent } from './views/security-user-edit-view/security-user-edit-view.component';
 import { SecurityUserListViewComponent } from './views/security-user-list-view/security-user-list-view.component';
-import { SecurityUserRoleFormComponent } from './components/security-user-role-form/security-user-role-form.component';
-import { SecurityUserRoleSelectionComponent } from './components/security-user-role-selection/security-user-role-selection.component';
 
 
 
@@ -30,7 +31,8 @@ import { SecurityUserRoleSelectionComponent } from './components/security-user-r
     ReactiveFormsModule,
     FontAwesomeModule,
     ControlsModule,
-    ApiUiModule
+    ApiUiModule,
+    LayoutModule
   ],
   providers: [
     SecurityUserService
