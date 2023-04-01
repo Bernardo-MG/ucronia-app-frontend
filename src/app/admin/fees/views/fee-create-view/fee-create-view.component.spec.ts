ac0ee339f47532ea5249e9c132cb18f7aefda2bf
Bernardo-MG <programming@bernardo-mg.com>
2023-04-01 14:08:24 +0200

    Fixed test initialization

--- src/app/admin/fees/views/fee-create-view/fee-create-view.component.spec.ts
+++ src/app/admin/fees/views/fee-create-view/fee-create-view.component.spec.ts
@@ -2,32 +2,41 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ApiUiModule } from '@app/api-ui/api-ui.module';
 import { FormControlsComponent } from '@app/controls/form-controls/form-controls.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EMPTY } from 'rxjs';
 import { FeeFormComponent } from '../../components/fee-form/fee-form.component';
+import { MemberSelectionComponent } from '../../components/member-selection/member-selection.component';
 import { FeeService } from '../../services/fee.service';
 import { FeeCreateViewComponent } from './fee-create-view.component';
 
 describe('FeeCreateViewComponent', () => {
   let component: FeeCreateViewComponent;
   let fixture: ComponentFixture<FeeCreateViewComponent>;
+  let service: FeeService;
 
   beforeEach(async () => {
+    service = jasmine.createSpyObj('FeeService', ['getMembers']);
+    (service as any).getMembers.and.returnValue(EMPTY);
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
         FontAwesomeModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ApiUiModule
       ],
       declarations: [
         FeeCreateViewComponent,
         FormControlsComponent,
-        FeeFormComponent
+        FeeFormComponent,
+        MemberSelectionComponent
       ],
       providers: [
-        FeeService
+        { provide: FeeService, useValue: service }
       ]
     })
       .compileComponents();
--- src/app/admin/fees/views/fee-edit-view/fee-edit-view.component.spec.ts
+++ src/app/admin/fees/views/fee-edit-view/fee-edit-view.component.spec.ts
@@ -2,32 +2,40 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { ApiUiModule } from '@app/api-ui/api-ui.module';
 import { FormControlsComponent } from '@app/controls/form-controls/form-controls.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EMPTY } from 'rxjs';
 import { FeeFormComponent } from '../../components/fee-form/fee-form.component';
+import { MemberSelectionComponent } from '../../components/member-selection/member-selection.component';
 import { FeeService } from '../../services/fee.service';
 import { FeeEditViewComponent } from './fee-edit-view.component';
 
 describe('FeeEditViewComponent', () => {
   let component: FeeEditViewComponent;
   let fixture: ComponentFixture<FeeEditViewComponent>;
+  let service: FeeService;
 
   beforeEach(async () => {
+    service = jasmine.createSpyObj('FeeService', ['getMembers']);
+    (service as any).getMembers.and.returnValue(EMPTY);
+
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule,
         FormsModule,
         FontAwesomeModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ApiUiModule
       ],
       declarations: [
         FeeEditViewComponent,
         FormControlsComponent,
-        FeeFormComponent
+        FeeFormComponent,
+        MemberSelectionComponent
       ],
       providers: [
-        FeeService
+        { provide: FeeService, useValue: service }
       ]
     })
       .compileComponents();
