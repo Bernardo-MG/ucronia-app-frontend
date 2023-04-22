c5c040a9d26886c2941c241786a246586c0696c7
Bernardo-MG <programming@bernardo-mg.com>
2023-04-22 22:38:49 +0200

    Using generic query

--- src/app/core/api/client/association-api-client.ts
+++ src/app/core/api/client/association-api-client.ts
@@ -1,21 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Fee } from "@app/association/models/fee";
 import { Member } from "@app/association/models/member";
+import { Transaction } from "@app/association/models/transaction";
 import { ReadOperations } from "@app/core/api/client/read-operations";
 import { environment } from "environments/environment";
-import { AngularCreateOperations } from "./angular-create-operations";
-import { AngularDeleteOperations } from "./angular-delete-operations";
 import { AngularReadOperations } from "./angular-read-operations";
-import { AngularUpdateOperations } from "./angular-update-operations";
-import { CreateOperations } from "./create-operations";
-import { DeleteOperations } from "./delete-operations";
 import { BalanceQuery } from "./query/balance-query";
 import { CrudQuery } from "./query/crud-query";
 import { FeeCalendarQuery } from "./query/fee-calendar-query";
-import { FeeQuery } from "./query/fee-query";
-import { TransactionQuery } from "./query/transaction-query";
 import { TransactionRangeQuery } from "./query/transaction-range-query";
-import { UpdateOperations } from "./update-operations";
 
 @Injectable({
   providedIn: 'root'
@@ -32,8 +26,8 @@ export class AssociationApiClient {
     return new BalanceQuery(this.getReadOperations());
   }
 
-  public fee(): FeeQuery {
-    return new FeeQuery(this.getCreateOperations(), this.getReadOperations(), this.getUpdateOperations(), this.getDeleteOperations());
+  public fee(): CrudQuery<Fee> {
+    return new CrudQuery<Fee>(this.http, this.rootUrl + '/transaction');
   }
 
   public feeCalendar(): FeeCalendarQuery {
@@ -44,8 +38,8 @@ export class AssociationApiClient {
     return new CrudQuery<Member>(this.http, this.rootUrl + '/member');
   }
 
-  public transaction(): TransactionQuery {
-    return new TransactionQuery(this.getCreateOperations(), this.getReadOperations(), this.getUpdateOperations(), this.getDeleteOperations());
+  public transaction(): CrudQuery<Transaction> {
+    return new CrudQuery<Transaction>(this.http, this.rootUrl + '/transaction');
   }
 
   public transactionRange(): TransactionRangeQuery {
@@ -54,18 +48,6 @@ export class AssociationApiClient {
 
   private getReadOperations(): ReadOperations {
     return new AngularReadOperations(this.http, this.rootUrl);
-  }
-
-  private getCreateOperations(): CreateOperations {
-    return new AngularCreateOperations(this.http, this.rootUrl);
-  }
-
-  private getUpdateOperations(): UpdateOperations {
-    return new AngularUpdateOperations(this.http, this.rootUrl);
-  }
-
-  private getDeleteOperations(): DeleteOperations {
-    return new AngularDeleteOperations(this.http, this.rootUrl);
   }
 
 }
