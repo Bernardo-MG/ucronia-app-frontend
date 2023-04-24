84cd70d31331445756c0afb769088fcacdd73be0
Bernardo-MG <programming@bernardo-mg.com>
2023-04-24 02:01:24 +0200

    Returns failures

--- src/app/core/api/repository/angular-http-operations.ts
+++ src/app/core/api/repository/angular-http-operations.ts
@@ -3,6 +3,8 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { PaginationRequest } from '../models/pagination-request';
 import { Sort } from '../models/sort';
 import { HttpOperations } from './http-operations';
+import { FailureResponse } from '../models/failure-response';
+import { Failure } from '../models/failure';
 
 export class AngularHttpOperations implements HttpOperations {
 
@@ -139,17 +141,35 @@ export class AngularHttpOperations implements HttpOperations {
   }
 
   private handleError(error: HttpErrorResponse) {
+    let response: any;
+
     if (error.status === 0) {
       // A client-side or network error occurred. Handle it accordingly.
       console.error('An error occurred:', error.error);
+      response = new Error('Something bad happened; please try again later.');
+    } else if (error.error.failures) {
+      // Failures response
+      console.error(
+        `Backend returned code ${error.status}, body was: `, error.error);
+      response = new FailureResponse();
+      response.failures = error.error.failures.map((failure: any) => {
+        const result = new Failure();
+        result.code = failure.code;
+        result.field = failure.field;
+        result.message = failure.message;
+        result.value = failure.value;
+
+        return result;
+      });
     } else {
       // The backend returned an unsuccessful response code.
       // The response body may contain clues as to what went wrong.
       console.error(
         `Backend returned code ${error.status}, body was: `, error.error);
+      response = new Error('Something bad happened; please try again later.');
     }
     // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(() => response);
   }
 
   private getFinalUrl(route: string) {
