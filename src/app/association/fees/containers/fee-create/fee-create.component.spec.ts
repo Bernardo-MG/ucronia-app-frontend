5de4a226799595690ea9889fa840c539661d977f
Bernardo-MG <programming@bernardo-mg.com>
2023-04-30 15:55:19 +0200

    Fixed tests

--- src/app/association/fees/containers/fee-create/fee-create.component.spec.ts
+++ src/app/association/fees/containers/fee-create/fee-create.component.spec.ts
@@ -1,14 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { EditionModule } from '@app/shared/edition/edition.module';
 import { IconsModule } from '@app/shared/icons/icons.module';
 import { LayoutModule } from '@app/shared/layout/layout.module';
 import { EMPTY } from 'rxjs';
-import { FeeFormComponent } from '../../components/fee-form/fee-form.component';
 import { MemberSelectionComponent } from '../../components/member-selection/member-selection.component';
 import { FeeService } from '../../services/fee.service';
 import { FeeCreateComponent } from './fee-create.component';
-import { EditionModule } from '@app/shared/edition/edition.module';
 
 describe('FeeCreateComponent', () => {
   let component: FeeCreateComponent;
@@ -30,7 +29,6 @@ describe('FeeCreateComponent', () => {
       ],
       declarations: [
         FeeCreateComponent,
-        FeeFormComponent,
         MemberSelectionComponent
       ],
       providers: [
