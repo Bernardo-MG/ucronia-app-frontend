b1d3a4d5e31d0b73d47b8ee9d5887a59cad0c973
Bernardo-MG <programming@bernardomg.com>
2023-07-12 23:42:48 +0200

    Moved options to upper menu

--- src/app/core/layout/services/layout.service.ts
+++ src/app/core/layout/services/layout.service.ts
@@ -41,7 +41,25 @@ export class LayoutService {
   }
 
   public getMenus(): Menu[] {
-    return [];
+    return [
+      {
+        title: 'Association', links:
+          this.filterNodes([
+            { title: 'Stats', path: '/', resource: 'member', action: 'read' },
+            { title: 'Fees calendar', path: '/fees/calendar', resource: 'fee', action: 'read' },
+            { title: 'Fees', path: '/fees/list', resource: 'fee', action: 'read' },
+            { title: 'Members', path: '/members/list', resource: 'member', action: 'read' },
+            { title: 'Transaction calendar', path: '/transactions/calendar', resource: 'transaction', action: 'read' },
+            { title: 'Transactions', path: '/transactions/list', resource: 'transaction', action: 'read' }
+          ])
+      },
+      {
+        title: 'Security', links: this.filterNodes([
+          { title: 'Users', path: '/security/users', resource: 'user', action: 'read' },
+          { title: 'Roles', path: '/security/roles', resource: 'role', action: 'read' }
+        ])
+      }
+    ];
   }
 
   private filterNodes(links: AuthMenuLink[]): MenuLink[] {
