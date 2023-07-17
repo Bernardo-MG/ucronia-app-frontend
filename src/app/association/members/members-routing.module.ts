fa7fe388e6bb13be7d2003e0cf258adb19dabb7c
Bernardo-MG <programming@bernardomg.com>
2023-07-18 00:36:36 +0200

    Improved layout

--- src/app/association/members/members-routing.module.ts
+++ src/app/association/members/members-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaddedFrameComponent } from '@app/core/layout/components/padded-frame/padded-frame.component';
 import { MemberCreateComponent } from './components/member-create/member-create.component';
 import { MemberDetailsComponent } from './components/member-details/member-details.component';
 import { MemberListComponent } from './components/member-list/member-list.component';
@@ -8,6 +9,7 @@ import { MemberListComponent } from './components/member-list/member-list.compon
 const routes: Routes = [
   {
     path: '',
+    component: PaddedFrameComponent,
     children: [
       { path: 'list', component: MemberListComponent },
       { path: 'create', component: MemberCreateComponent },
