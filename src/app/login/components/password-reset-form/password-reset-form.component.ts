d62461dd7fe47934fc3264f34e556d1af8568aa8
Bernardo-MG <programming@bernardomg.com>
2023-07-30 21:03:22 +0200

    Corrected code location

--- src/app/login/components/password-reset-form/password-reset-form.component.ts
+++ src/app/login/components/password-reset-form/password-reset-form.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { LoginService } from '@app/login/services/login.service';
 
 @Component({
   selector: 'app-password-reset-form',
@@ -8,27 +6,5 @@ import { LoginService } from '@app/login/services/login.service';
   styleUrls: ['./password-reset-form.component.sass']
 })
 export class PasswordResetFormComponent {
-
-  private token = '';
-
-  constructor(
-    private route: ActivatedRoute,
-    private loginService: LoginService
-  ) { }
-
-  public ngOnInit(): void {
-    // Get token
-    this.route.paramMap.subscribe(params => {
-      if (params.has('token')) {
-        this.load(params.get('token'));
-      }
-    });
-  }
-
-  private load(token: string | null): void {
-    if (token) {
-      this.loginService.validateResetPasswordToken(token).subscribe(r => this.token = token);
-    }
-  }
 
 }
--- src/app/login/components/password-reset/password-reset.component.ts
+++ src/app/login/components/password-reset/password-reset.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoginService } from '@app/login/services/login.service';
 
 @Component({
   selector: 'app-password-reset',
   templateUrl: './password-reset.component.html',
   styleUrls: ['./password-reset.component.sass']
 })
-export class PasswordResetComponent {
+export class PasswordResetComponent implements OnInit {
+
+  private token = '';
+
+  constructor(
+    private route: ActivatedRoute,
+    private loginService: LoginService
+  ) { }
+
+  public ngOnInit(): void {
+    // Get token
+    this.route.paramMap.subscribe(params => {
+      if (params.has('token')) {
+        this.load(params.get('token'));
+      }
+    });
+  }
+
+  private load(token: string | null): void {
+    if (token) {
+      this.loginService.validateResetPasswordToken(token).subscribe(r => this.token = token);
+    }
+  }
 
 }
