17c5ce3cad4ea524ec66411126639a706de9fc98
Bernardo-MG <programming@bernardomg.com>
2023-07-30 18:46:24 +0200

    Shows sent email message on success

--- src/app/login/components/password-reset/password-reset.component.ts
+++ src/app/login/components/password-reset/password-reset.component.ts
@@ -9,12 +9,14 @@ import { LoginService } from '@app/login/services/login.service';
 })
 export class PasswordResetComponent {
 
+  public finished = false;
+
   constructor(
     private loginService: LoginService
   ) { }
 
   public onPasswordReset(resetPassword: PasswordResetForm) {
-    this.loginService.resetPassword(resetPassword).subscribe();
+    this.loginService.resetPassword(resetPassword).subscribe(r => this.finished = true);
   }
 
 }
