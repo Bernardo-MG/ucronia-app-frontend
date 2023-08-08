c87d5fd3402907bd4aa0f5b86778860513f4d7c3
Bernardo-MG <programming@bernardomg.com>
2023-08-09 00:02:13 +0200

    Merged member selection with form

--- src/app/association/fees/components/fee-form/fee-form.component.ts
+++ src/app/association/fees/components/fee-form/fee-form.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Fee } from '@app/association/models/fee';
+import { Member } from '@app/association/models/member';
 import { FormComponent } from '@app/shared/form/components/form/form.component';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'assoc-fee-form',
@@ -9,6 +11,22 @@ import { FormComponent } from '@app/shared/form/components/form/form.component';
   styleUrls: ['./fee-form.component.sass']
 })
 export class FeeFormComponent extends FormComponent<Fee> {
+
+  @Input() public waitingMembers = false;
+
+  @Input() public members: Member[] = [];
+
+  @Input() public membersPage = 0;
+
+  @Input() public membersTotalPages = 0;
+
+  @Output() public goToMembersPage = new EventEmitter<number>();
+
+  public selectingMember = false;
+
+  public member = new Member();
+
+  public searchIcon = faMagnifyingGlass;
 
   constructor(
     fb: FormBuilder
@@ -19,6 +37,25 @@ export class FeeFormComponent extends FormComponent<Fee> {
       date: [new Date(), Validators.required],
       paid: [false, Validators.required]
     }));
+  }
+
+  public onSelectMember(member: Member) {
+    this.member = member;
+    this.data = { ...this.data, memberId: member.id };
+    this.selectingMember = false;
+  }
+
+  public onStartSelectingMember() {
+    this.onGoToMembersPage(0);
+    this.selectingMember = true;
+  }
+
+  public onCancelSelectMember() {
+    this.selectingMember = false;
+  }
+
+  public onGoToMembersPage(page: number) {
+    this.goToMembersPage.emit(page);
   }
 
 }
