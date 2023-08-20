47c4afe20d6388dd4c407cb5552adbc901f2318c
Bernardo-MG <programming@bernardomg.com>
2023-08-20 16:34:24 +0200

    Added resource guard

--- src/app/association/association-routing.module.ts
+++ src/app/association/association-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResourceGuard } from '@app/core/authentication/guards/resource.guard';
 
 const statsModule = () => import('@app/association/stats/association-stats.module').then(m => m.AssociationStatsModule);
 const memberModule = () => import('@app/association/members/members.module').then(m => m.MembersModule);
@@ -10,10 +11,26 @@ const routes: Routes = [
     {
         path: '',
         children: [
-            { path: 'stats', loadChildren: statsModule },
-            { path: 'transactions', loadChildren: transactionModule },
-            { path: 'members', loadChildren: memberModule },
-            { path: 'fees', loadChildren: feeModule }
+            {
+                path: 'stats',
+                canActivate: [ResourceGuard("stats")],
+                loadChildren: statsModule
+            },
+            {
+                path: 'transactions',
+                canActivate: [ResourceGuard("transaction")],
+                loadChildren: transactionModule
+            },
+            {
+                path: 'members',
+                canActivate: [ResourceGuard("member")],
+                loadChildren: memberModule
+            },
+            {
+                path: 'fees',
+                canActivate: [ResourceGuard("fee")],
+                loadChildren: feeModule
+            }
         ]
     }
 ];
