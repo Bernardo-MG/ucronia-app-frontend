f36b437ab2ddc7f1b21b169aa73dff83ab3db009
Bernardo-MG <programming@bernardomg.com>
2023-08-22 22:21:36 +0200

    Added tests

--- src/app/shared/pagination/components/pagination-navigation/pagination-navigation.component.spec.ts
+++ src/app/shared/pagination/components/pagination-navigation/pagination-navigation.component.spec.ts
@@ -20,7 +20,59 @@ describe('PaginationNavigationComponent', () => {
     fixture.detectChanges();
   });
 
+  // **************************************************************************
+  // General tests
+  // **************************************************************************
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // **************************************************************************
+  // Current page
+  // **************************************************************************
+
+  it('should by default have a current page of 1', () => {
+    expect(component.current).toEqual(1);
+  });
+
+  it('should not change the current page when going to a page', () => {
+    component.current = 1;
+
+    component.onGoTo(5);
+    expect(component.current).toEqual(1);
+  });
+
+  // **************************************************************************
+  // Moving buttons
+  // **************************************************************************
+
+  it('should disable backward button when the current page is 1', () => {
+    component.current = 1;
+    component.pages = 10;
+
+    expect(component.isBackwardDisabled()).toEqual(true);
+  });
+
+  it('should enable backward button when the current page is above 1', () => {
+    component.current = 2;
+    component.pages = 10;
+
+    expect(component.isBackwardDisabled()).toEqual(false);
+  });
+
+  it('should disable forward button when the current page is equal to the number of pages', () => {
+    component.current = 10;
+    component.pages = 10;
+
+    expect(component.isForwardDisabled()).toEqual(true);
+  });
+
+  it('should enable forward button when the current page is below the number of pages', () => {
+    component.current = 9;
+    component.pages = 10;
+
+    expect(component.isForwardDisabled()).toEqual(false);
+  });
+
 });
