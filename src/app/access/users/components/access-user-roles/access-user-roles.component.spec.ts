d144c3945427bc006f38911614f988ed5043428c
Bernardo-MG <programming@bernardomg.com>
2023-08-23 21:02:12 +0200

    Fixed test imports

--- src/app/access/users/components/access-user-roles/access-user-roles.component.spec.ts
+++ src/app/access/users/components/access-user-roles/access-user-roles.component.spec.ts
@@ -1,8 +1,12 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CoreModule } from '@app/core/core.module';
+import { LayoutModule } from '@app/shared/layout/layout.module';
 import { PaginationModule } from '@app/shared/pagination/pagination.module';
+import { AccessUserService } from '../../services/access-user.service';
+import { AccessUserRoleSelectionComponent } from '../access-user-role-selection/access-user-role-selection.component';
 import { AccessUserRoleFormComponent } from './access-user-roles.component';
 
 describe('AccessUserRoleFormComponent', () => {
@@ -12,14 +16,20 @@ describe('AccessUserRoleFormComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        HttpClientTestingModule,
         FormsModule,
         ReactiveFormsModule,
         RouterTestingModule,
         CoreModule,
+        LayoutModule,
         PaginationModule
       ],
       declarations: [
-        AccessUserRoleFormComponent
+        AccessUserRoleFormComponent,
+        AccessUserRoleSelectionComponent
+      ],
+      providers: [
+        AccessUserService
       ]
     })
       .compileComponents();
