b7b805b874409ced0fba441f04de1d1bd4fe13fe
Bernardo-MG <programming@bernardomg.com>
2023-08-28 13:02:56 +0200

    Removed dead code

--- src/app/association/members/components/member-selection-list/member-selection-list.component.ts
+++ src/app/association/members/components/member-selection-list/member-selection-list.component.ts
@@ -1,36 +1,16 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Member } from '@app/association/models/member';
-import { TableHeaderCell } from '@app/shared/layout/models/table-header-cell';
-import { TableRenderer } from '@app/shared/layout/models/table-renderer';
-import { TableRow } from '@app/shared/layout/models/table-row';
 import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'member-selection-list',
   templateUrl: './member-selection-list.component.html'
 })
-export class MemberSelectionListComponent implements OnChanges {
+export class MemberSelectionListComponent {
 
   @Input() public members: Member[] = [];
 
-  public header: TableHeaderCell[] = [{ name: 'Name', property: 'name' }, { name: 'Surname', property: 'surname' }, { name: 'Active', property: 'active' }];
-
-  public renderers: TableRenderer[] = [];
-
-  public rows: TableRow[] = [];
-
   public activeIcon = faCheck;
   public inactiveIcon = faX;
-
-  public ngOnChanges(changes: SimpleChanges): void {
-    if (changes['members']) {
-      this.rows = this.members.map(m => {
-        return {
-          id: m.id,
-          cells: [m.name, m.surname, m.active]
-        };
-      });
-    }
-  }
 
 }
