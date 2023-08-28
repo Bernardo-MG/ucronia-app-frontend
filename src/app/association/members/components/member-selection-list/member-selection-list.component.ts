45fbd2a7a4b7476647742186799ed7123e2be03d
Bernardo-MG <programming@bernardomg.com>
2023-08-28 12:58:45 +0200

    Improved member list

--- src/app/association/members/components/member-selection-list/member-selection-list.component.ts
+++ src/app/association/members/components/member-selection-list/member-selection-list.component.ts
@@ -1,7 +1,9 @@
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Member } from '@app/association/models/member';
 import { TableHeaderCell } from '@app/shared/layout/models/table-header-cell';
+import { TableRenderer } from '@app/shared/layout/models/table-renderer';
 import { TableRow } from '@app/shared/layout/models/table-row';
+import { faCheck, faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'member-selection-list',
@@ -13,9 +15,14 @@ export class MemberSelectionListComponent implements OnChanges {
 
   public header: TableHeaderCell[] = [{ name: 'Name', property: 'name' }, { name: 'Surname', property: 'surname' }, { name: 'Active', property: 'active' }];
 
+  public renderers: TableRenderer[] = [];
+
   public rows: TableRow[] = [];
 
-  ngOnChanges(changes: SimpleChanges): void {
+  public activeIcon = faCheck;
+  public inactiveIcon = faX;
+
+  public ngOnChanges(changes: SimpleChanges): void {
     if (changes['members']) {
       this.rows = this.members.map(m => {
         return {
