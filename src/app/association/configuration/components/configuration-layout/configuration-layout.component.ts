61bf5c1a32eb76736509125c97ecf1d6005eace9
Bernardo-MG <programming@bernardomg.com>
2023-09-06 19:23:15 +0200

    Removed styles

--- src/app/association/configuration/components/configuration-layout/configuration-layout.component.ts
+++ src/app/association/configuration/components/configuration-layout/configuration-layout.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'assoc-configuration-layout',
-  templateUrl: './configuration-layout.component.html',
-  styleUrls: ['./configuration-layout.component.sass']
+  templateUrl: './configuration-layout.component.html'
 })
 export class ConfigurationLayoutComponent {
 
--- src/app/association/configuration/components/configuration/configuration.component.ts
+++ src/app/association/configuration/components/configuration/configuration.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'assoc-configuration',
-  templateUrl: './configuration.component.html',
-  styleUrls: ['./configuration.component.sass']
+  templateUrl: './configuration.component.html'
 })
 export class ConfigurationComponent {
 
