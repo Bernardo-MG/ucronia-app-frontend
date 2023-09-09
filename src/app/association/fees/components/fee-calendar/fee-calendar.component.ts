0f3d20c12bf0b4c760a74620c6ebc9f5d915773e
Bernardo-MG <programming@bernardomg.com>
2023-09-09 12:58:42 +0200

    Works through a list of years

--- src/app/association/fees/components/fee-calendar/fee-calendar.component.ts
+++ src/app/association/fees/components/fee-calendar/fee-calendar.component.ts
@@ -1,42 +1,48 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { FeeCalendarRow } from '@app/association/models/fee-calendar-row';
 
 @Component({
   selector: 'assoc-fee-calendar',
   templateUrl: './fee-calendar.component.html'
 })
-export class FeeCalendarComponent {
+export class FeeCalendarComponent implements OnChanges {
 
   @Input() public year = 0;
 
-  @Input() start = 0;
-
-  @Input() end = 0;
+  @Input() public years: number[] = [];
 
   @Input() public rows: FeeCalendarRow[] = [];
 
   @Output() public yearChange = new EventEmitter<number>();
 
+  private index = 0;
+
   public months: number[] = Array(12).fill(0).map((x, i) => i + 1);
 
-  public feesTable: FeeCalendarRow[] = []
+  public feesTable: FeeCalendarRow[] = [];
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if ((changes['year']) || (changes['years'])) {
+      this.index = this.years.indexOf(this.year);
+    }
+  }
 
   public onGoPrevious() {
-    this.year = this.year - 1;
+    this.year = this.years[this.index - 1];
     this.yearChange.emit(this.year);
   }
 
   public onGoNext() {
-    this.year = this.year + 1;
+    this.year = this.years[this.index + 1];
     this.yearChange.emit(this.year);
   }
 
   public isAbleToGoNext() {
-    return (this.year < this.end);
+    return ((this.index + 1) < this.years.length);
   }
 
   public isAbleToGoPrevious() {
-    return (this.year > this.start);
+    return (this.index > 0);
   }
 
 }
