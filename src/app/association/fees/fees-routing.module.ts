2c2860528c3378247e9380257aee638f66179037
Bernardo-MG <programming@bernardomg.com>
2023-09-10 02:28:43 +0200

    Removed fee list

--- src/app/association/fees/fees-routing.module.ts
+++ src/app/association/fees/fees-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { FeeCalendarInfoComponent } from './components/fee-calendar-info/fee-calendar-info.component';
 import { FeeDetailsComponent } from './components/fee-details/fee-details.component';
 import { FeeLayoutComponent } from './components/fee-layout/fee-layout.component';
-import { FeeListComponent } from './components/fee-list/fee-list.component';
 import { FeePayComponent } from './components/fee-pay/fee-pay.component';
 
 
@@ -13,7 +12,6 @@ const routes: Routes = [
     component: FeeLayoutComponent,
     children: [
       { path: '', component: FeeCalendarInfoComponent },
-      { path: 'list', component: FeeListComponent },
       { path: 'calendar', component: FeeCalendarInfoComponent },
       { path: 'pay', component: FeePayComponent },
       { path: ':id', component: FeeDetailsComponent }
