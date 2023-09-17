31f6ed1c391fc0858fbface61f1c3494bf00893b
Bernardo-MG <programming@bernardomg.com>
2023-09-18 01:26:33 +0200

    Added docs

--- src/app/access/password-reset/components/password-reset-request/password-reset-request.component.ts
+++ src/app/access/password-reset/components/password-reset-request/password-reset-request.component.ts
@@ -12,20 +12,31 @@ import { PasswordResetService } from '../../services/password-reset.service';
 })
 export class PasswordResetRequestComponent {
 
+  /**
+   * Finished flag. If set to true the component is finished and allows no furter operation.
+   */
   public finished = false;
 
   /**
-   * Loading flag. Shows the loading visual cue and disables the form. Its status depends on the login request.
+   * Password reset flag. If set to true the component is waiting for the password change request to finish.
    */
   public reseting = false;
 
+  /**
+   * Failures when reseting the password.
+   */
   public failures: { [key: string]: Failure[] } = {};
 
   constructor(
     private service: PasswordResetService
   ) { }
 
-  public onPasswordReset(resetPassword: PasswordResetRequest) {
+  /**
+   * Handles the password reset request.
+   * 
+   * @param resetPassword password reset data
+   */
+  public onPasswordResetRequest(resetPassword: PasswordResetRequest) {
     this.reseting = true;
     this.failures = {};
     this.service.requestResetPassword(resetPassword)
@@ -35,12 +46,12 @@ export class PasswordResetRequestComponent {
           this.reseting = false;
         },
         error: error => {
-          this.reseting = false;
           if (error.failures) {
             this.failures = error.failures;
           } else {
             this.failures = {};
           }
+          this.reseting = false;
         }
       });
   }
