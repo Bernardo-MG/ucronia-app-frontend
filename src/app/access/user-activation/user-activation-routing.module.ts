54b3f003d786bc6f644e665c563a43ed9fe51bb8
Bernardo-MG <programming@bernardomg.com>
2023-09-18 00:53:36 +0200

    Corrected layout

--- src/app/access/user-activation/user-activation-routing.module.ts
+++ src/app/access/user-activation/user-activation-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CenteredFrameComponent } from '@app/shared/layout/components/centered-frame/centered-frame.component';
 import { UserActivateUserComponent } from './components/activate-user/user-activate-user.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: CenteredFrameComponent,
     children: [
       { path: ':token', component: UserActivateUserComponent }
     ]
