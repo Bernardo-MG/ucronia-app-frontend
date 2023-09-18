6ed5784bac149f3ee5158a0e2c2a58fe67cfcfc4
Bernardo-MG <programming@bernardomg.com>
2023-09-18 08:59:04 +0200

    Members list show totals

--- src/app/association/stats/association-stats.module.ts
+++ src/app/association/stats/association-stats.module.ts
@@ -2,14 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LayoutModule } from '@app/shared/layout/layout.module';
 import { PaginationModule } from '@app/shared/pagination/pagination.module';
-import { MemberStatsComponent } from './components/member-stats/member-stats.component';
 import { BalanceInfoComponent } from './components/balance-info/balance-info.component';
 
 
 
 @NgModule({
   declarations: [
-    MemberStatsComponent,
     BalanceInfoComponent
   ],
   imports: [
@@ -18,7 +16,6 @@ import { BalanceInfoComponent } from './components/balance-info/balance-info.com
     LayoutModule
   ],
   exports: [
-    MemberStatsComponent,
     BalanceInfoComponent
   ]
 })
