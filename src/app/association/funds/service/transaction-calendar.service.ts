834694b325d0b6b694a1ce1175050fa06a7edfdc
Bernardo-MG <programming@bernardomg.com>
2023-09-20 22:55:57 +0200

    Shows values for the previous and next months

--- src/app/association/funds/service/transaction-calendar.service.ts
+++ src/app/association/funds/service/transaction-calendar.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TransactionApi } from '@app/association/api/transaction-api';
-import { Observable, map } from 'rxjs';
+import { Observable, concat, map, mergeMap, toArray } from 'rxjs';
 import { Transaction } from '../models/transaction';
 
 @Injectable()
@@ -14,7 +14,33 @@ export class TransactionCalendarService {
   ) { }
 
   public getCalendar(year: number, month: number): Observable<Transaction[]> {
-    return this.transactionApi.calendarMonth(year, month).pipe(map(r => r.content));
+    let previousYear;
+    let previousMonth;
+    if (month > 1) {
+      previousYear = year;
+      previousMonth = month - 1;
+    } else {
+      previousYear = year - 1;
+      previousMonth = 12;
+    }
+    let nextYear;
+    let nextMonth;
+    if (month > 1) {
+      nextYear = year;
+      nextMonth = month + 1;
+    } else {
+      nextYear = year - 1;
+      nextMonth = 12;
+    }
+
+    const previousMonthQuery = this.transactionApi.calendarMonth(previousYear, previousMonth).pipe(map(r => r.content));
+    const thisMonthQuery = this.transactionApi.calendarMonth(year, month).pipe(map(r => r.content));
+    const nextMonthQuery = this.transactionApi.calendarMonth(nextYear, nextMonth).pipe(map(r => r.content));
+
+    return concat(previousMonthQuery, thisMonthQuery, nextMonthQuery).pipe(
+      mergeMap((data) => data), // Flatten the arrays emitted by each observable
+      toArray(), // Collect all emissions into a single array
+    );
   }
 
   public getRange(): Observable<Date[]> {
