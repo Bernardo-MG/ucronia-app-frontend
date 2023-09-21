f7ba635151d78718406c6dff42eb196dbec438b0
Bernardo-MG <programming@bernardomg.com>
2023-09-21 19:10:17 +0200

    Encapsulated calendar

--- src/app/association/fees/components/fee-calendar/fee-calendar.component.ts
+++ src/app/association/fees/components/fee-calendar/fee-calendar.component.ts
@@ -1,51 +1,95 @@
-import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FeeCalendarRange } from '@app/association/models/fee-calendar-range';
 import { FeeMonth } from '@app/association/models/fee-month';
 import { UserFeeCalendar } from '@app/association/models/user-fee-calendar';
+import { RouteParametersActuator } from '@app/shared/utils/route/actuator/route-parameters-actuator';
+import { YearRouteObserver } from '../../observer/year-route-observer';
+import { FeeCalendarService } from '../../services/fee-calendar.service';
 
 @Component({
   selector: 'assoc-fee-calendar',
   templateUrl: './fee-calendar.component.html'
 })
-export class FeeCalendarComponent implements OnChanges {
+export class FeeCalendarComponent implements OnInit, OnChanges {
 
-  @Input() public waiting = false;
+  @Input() public onlyActive = true;
 
-  @Input() public year = 0;
+  public year = new Date().getFullYear();
 
-  @Input() public years: number[] = [];
+  /**
+   * Loading flag. Shows the loading visual cue.
+   */
+  public readingCalendar = false;
 
-  @Input() public rows: UserFeeCalendar[] = [];
+  public rows: UserFeeCalendar[] = [];
 
-  @Output() public yearChange = new EventEmitter<number>();
+  public range = new FeeCalendarRange();
 
   private index = 0;
 
   public months: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-  ngOnChanges(changes: SimpleChanges): void {
-    if ((changes['year']) || (changes['years'])) {
-      this.index = this.years.indexOf(this.year);
+  private routeActuator;
+
+  private routeObserver;
+
+  constructor(
+    router: Router,
+    route: ActivatedRoute,
+    private service: FeeCalendarService
+  ) {
+    this.routeActuator = new RouteParametersActuator(router);
+    this.routeObserver = new YearRouteObserver(route);
+  }
+
+  public ngOnInit(): void {
+    // Load range
+    this.service.getRange().subscribe(d => {
+      this.range = d;
+      const lastYear = this.range.years[this.range.years.length - 1];
+      if (this.year > lastYear) {
+        this.year = lastYear;
+      }
+      this.index = this.range.years.indexOf(this.year);
+    });
+
+    // Watch for year changes
+    this.routeObserver.subject.subscribe(y => {
+      if (y) {
+        this.load(y);
+      } else {
+        // Load initial year
+        this.load(new Date().getFullYear());
+      }
+    });
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['onlyActive']) {
+      this.load(this.year);
     }
   }
 
   public onGoTo(event: any) {
-    this.yearChange.emit(Number(event.target.value));
+    this.year = Number(event.target.value);
+    this.load(this.year);
   }
 
   public onGoPrevious() {
     this.index = this.index - 1;
-    this.year = this.years[this.index];
-    this.yearChange.emit(this.year);
+    this.year = this.range.years[this.index];
+    this.load(this.year);
   }
 
   public onGoNext() {
     this.index = this.index + 1;
-    this.year = this.years[this.index];
-    this.yearChange.emit(this.year);
+    this.year = this.range.years[this.index];
+    this.load(this.year);
   }
 
   public isAbleToGoNext() {
-    return ((this.index >= 0) && ((this.index + 1) < this.years.length));
+    return ((this.index >= 0) && ((this.index + 1) < this.range.years.length));
   }
 
   public isAbleToGoPrevious() {
@@ -58,6 +102,23 @@ export class FeeCalendarComponent implements OnChanges {
 
   public getMonth(months: FeeMonth[], month: number): FeeMonth | undefined {
     return months.find(m => m.month === month);
+  }
+
+  private load(year: number) {
+    this.year = year;
+    this.readingCalendar = true;
+    this.routeActuator.addParameters({ year });
+
+    this.service.getCalendar(year, this.onlyActive).subscribe({
+      next: data => {
+        this.rows = data;
+        this.readingCalendar = false;
+      },
+      error: error => {
+        // Reactivate view
+        this.readingCalendar = false;
+      }
+    });
   }
 
 }
--- src/app/association/fees/components/fee-frontpage/fee-frontpage.component.ts
+++ src/app/association/fees/components/fee-frontpage/fee-frontpage.component.ts
@@ -14,78 +14,21 @@ import { FeeCalendarService } from '../../services/fee-calendar.service';
 })
 export class FeeFrontpageComponent implements OnInit {
 
-  /**
-   * Loading flag. Shows the loading visual cue.
-   */
-  public readingCalendar = false;
-
   public createPermission = false;
-
-  public range = new FeeCalendarRange();
 
   public onlyActive = true;
 
-  public rows: UserFeeCalendar[] = [];
-
-  public year = new Date().getFullYear();
-
-  private routeActuator;
-
-  private routeObserver;
-
   constructor(
-    router: Router,
-    route: ActivatedRoute,
-    private service: FeeCalendarService,
     private authService: AuthService
-  ) {
-    this.routeActuator = new RouteParametersActuator(router);
-    this.routeObserver = new YearRouteObserver(route);
-  }
+  ) {}
 
   ngOnInit(): void {
     // Check permissions
     this.createPermission = this.authService.hasPermission("fee", "create");
-
-    // Load range
-    this.service.getRange().subscribe(d => this.range = d);
-
-    // Watch for year changes
-    this.routeObserver.subject.subscribe(y => {
-      if (y) {
-        this.load(y);
-      } else {
-        // Load initial year
-        this.load(new Date().getFullYear());
-      }
-    });
   }
 
   public onFilterActiveMembers(event: any) {
     this.onlyActive = event.checked;
-    this.load(this.year);
-  }
-
-  public onYearChange(year: number) {
-    this.year = year;
-    this.load(year);
-  }
-
-  private load(year: number) {
-    this.year = year;
-    this.readingCalendar = true;
-    this.routeActuator.addParameters({ year });
-
-    this.service.getCalendar(year, this.onlyActive).subscribe({
-      next: data => {
-        this.rows = data;
-        this.readingCalendar = false;
-      },
-      error: error => {
-        // Reactivate view
-        this.readingCalendar = false;
-      }
-    });
   }
 
 }
