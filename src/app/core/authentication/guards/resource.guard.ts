fb828a8dcfa9692cc452704d588cf8e658a2ffd6
Bernardo-MG <programming@bernardomg.com>
2023-09-24 01:12:21 +0200

    Corrected guards

--- src/app/core/authentication/guards/resource.guard.ts
+++ src/app/core/authentication/guards/resource.guard.ts
@@ -1,37 +1,28 @@
 import { inject } from '@angular/core';
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-const LoggedInGuard = (resource: string, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
-  const router = inject(Router);
-  const authService = inject(AuthService)
-  const loginRoute = '/login';
-  let active;
+/**
+ * Resource guard. Allows access only if the user has view permission for the resource.
+ * Otherwise it redirects to the root.
+ */
+export const ResourceGuard = (resource: string) => {
+  return () => {
+    const router = inject(Router);
+    const authService = inject(AuthService)
+    const rootRoute = '/';
+    let active;
 
-  if (authService.hasPermission(resource, "view")) {
-    // Logged in
-    active = true;
-  } else {
-    // Not logged in
-    // Redirect to login
-
-    // Prepare URL redirection for successful login
-    let params;
-    if ((state.url.length) && (state.url.split('?')[0] !== loginRoute)) {
-      params = { queryParams: { returnUrl: state.url } };
+    if (authService.hasPermission(resource, "view")) {
+      // Logged in
+      active = true;
     } else {
-      params = {};
+      // No permission
+      // Redirect to root
+      router.navigate([rootRoute], {});
+      active = false;
     }
 
-    router.navigate([loginRoute], params);
-    active = false;
+    return active;
   }
-
-  return active;
 }
-
-/**
- * Logged in guard. Allows access only if the user in session is logged in. Otherwise redirects
- * to the login form.
- */
-export const ResourceGuard = (resource: string) => (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => LoggedInGuard(resource, route, state);
