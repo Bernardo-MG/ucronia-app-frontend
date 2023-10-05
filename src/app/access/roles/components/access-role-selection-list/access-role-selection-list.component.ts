ce2ab1b15ac422909263f24f84dc546c48bbd2c8
Bernardo-MG <programming@bernardomg.com>
2023-10-06 00:28:18 +0200

    Moved logic to component

--- src/app/access/roles/components/access-role-selection-list/access-role-selection-list.component.ts
+++ src/app/access/roles/components/access-role-selection-list/access-role-selection-list.component.ts
@@ -1,12 +1,55 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { PaginationRequest } from '@app/core/api/models/pagination-request';
+import { Sort } from '@app/core/api/models/sort';
+import { Permission } from '@app/core/authentication/models/permission';
 import { Role } from '@app/core/authentication/models/role';
+import { AccessRoleService } from '../../services/access-role.service';
 
 @Component({
   selector: 'access-role-selection-list',
   templateUrl: './access-role-selection-list.component.html'
 })
-export class AccessRoleSelectionListComponent {
+export class AccessRoleSelectionListComponent  implements OnInit {
 
-  @Input() public roles: Role[] = [];
+  @Output() public changeDirection = new EventEmitter<Sort<Permission>>();
+
+  /**
+   * Loading flag.
+   */
+  public readingRoles = false;
+
+  public roles: Role[] = [];
+
+  public totalPages = 0;
+
+  constructor(
+    private service: AccessRoleService
+  ) {}
+
+  ngOnInit(): void {
+    this.load(undefined);
+  }
+
+  public onChangeDirection(sort: Sort<Permission>) {
+    this.changeDirection.emit(sort);
+  }
+
+  private load(pagination: PaginationRequest | undefined) {
+    this.readingRoles = true;
+    this.service.getAll(pagination).subscribe({
+      next: page => {
+
+        this.roles = page.content;
+
+        this.totalPages = page.totalPages;
+        // Reactivate view
+        this.readingRoles = false;
+      },
+      error: error => {
+        // Reactivate view
+        this.readingRoles = false;
+      }
+    });
+  }
 
 }
