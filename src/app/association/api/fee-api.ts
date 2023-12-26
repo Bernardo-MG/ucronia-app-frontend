0fdb944b49037c940920c66eb62b8aa6f222443b
Bernardo-MG <programming@bernardomg.com>
2023-12-26 17:04:02 +0100

    Simplified API

--- src/app/association/api/fee-api.ts
+++ src/app/association/api/fee-api.ts
@@ -1,31 +1,31 @@
 import { HttpClient } from "@angular/common/http";
 import { FeeCalendarRange } from "@app/association/membership/models/fee-calendar-range";
 import { FeePayment } from "@app/association/membership/models/fee-payment";
-import { CrudApi } from "@app/core/api/crud-api";
 import { ApiResponse } from "@app/core/api/models/api-response";
+import { Direction } from "@app/core/api/models/direction";
 import { PaginatedQuery } from "@app/core/api/models/paginated-query";
+import { Sort } from "@app/core/api/models/sort";
 import { AngularRequest } from "@app/core/api/request/angular-request";
+import { Request } from "@app/core/api/request/request";
 import { environment } from "environments/environment";
 import { Observable } from "rxjs";
 import { Fee } from "../membership/models/fee";
 import { UserFeeCalendar } from "../membership/models/user-fee-calendar";
 
-export class FeeApi extends CrudApi<Fee> {
+export class FeeApi {
 
   constructor(
     private http: HttpClient
-  ) {
-    super(() => new AngularRequest(this.http, environment.apiUrl + '/fee'))
-  }
+  ) { }
 
   public pay(data: FeePayment): Observable<ApiResponse<FeePayment>> {
-    const request = this.requestProvider();
+    const request = this.getRequest();
 
     return request.create(data);
   }
 
   public updateByFeeId(date: string, memberId: number, data: Fee): Observable<ApiResponse<Fee>> {
-    const request = this.requestProvider();
+    const request = this.getRequest();
 
     request.appendRoute(`/${date}/${memberId}`);
 
@@ -33,7 +33,7 @@ export class FeeApi extends CrudApi<Fee> {
   }
 
   public deleteByFeeId(date: string, memberId: number): Observable<ApiResponse<boolean>> {
-    const request = this.requestProvider();
+    const request = this.getRequest();
 
     request.appendRoute(`/${date}/${memberId}`);
 
@@ -41,7 +41,7 @@ export class FeeApi extends CrudApi<Fee> {
   }
 
   public readByFeeId(date: string, memberId: number): Observable<ApiResponse<Fee>> {
-    const request = this.requestProvider();
+    const request = this.getRequest();
 
     request.appendRoute(`/${date}/${memberId}`);
     return request.read();
@@ -56,11 +56,44 @@ export class FeeApi extends CrudApi<Fee> {
   }
 
   public calendarRange(): Observable<ApiResponse<FeeCalendarRange>> {
-    const request = this.requestProvider();
+    const request = this.getRequest();
 
     request.appendRoute("/calendar/range");
 
     return request.read();
+  }
+
+  private getRequest() {
+    return new AngularRequest(this.http, environment.apiUrl + '/fee');
+  }
+
+  protected requestWithQuery(query: PaginatedQuery<any>): Request {
+    const request = this.getRequest();
+
+    // Sort
+    if (query.sort.length > 0) {
+      this.applySort(query.sort, request);
+    } else if (query.defaultSort.length > 0) {
+      this.applySort(query.defaultSort, request);
+    }
+
+    // Other parameters
+    for (const key in query.parameters) {
+      const value = query.parameters[key];
+      if (value) {
+        request.parameter(key, value);
+      }
+    }
+
+    return request;
+  }
+
+  private applySort(sort: Sort<FeePayment>[], request: Request) {
+    const validSort = sort.filter(s => s.direction !== Direction.Unsorted);
+    for (let i = 0; i < validSort.length; i += 1) {
+      const fieldSort = validSort[i];
+      request.parameter('sort', `${String(fieldSort.property)},${fieldSort.direction}`);
+    }
   }
 
 }
