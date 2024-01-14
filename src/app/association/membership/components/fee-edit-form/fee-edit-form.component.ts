e7aff50f6e4c89593260684a480d10d7866124b3
Bernardo-MG <programming@bernardomg.com>
2024-01-14 18:26:00 +0100

    Fixed form

--- src/app/association/membership/components/fee-edit-form/fee-edit-form.component.ts
+++ src/app/association/membership/components/fee-edit-form/fee-edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { FormComponent } from '@app/shared/form/components/form/form.component';
 import { Fee } from '../../models/fee';
@@ -9,6 +9,8 @@ import { Fee } from '../../models/fee';
 })
 export class FeeEditFormComponent extends FormComponent<Fee> {
 
+  @Input() public info = new Fee();
+
   constructor(
     fb: FormBuilder
   ) {
@@ -17,10 +19,14 @@ export class FeeEditFormComponent extends FormComponent<Fee> {
     this.form = fb.group({
       memberNumber: [null, [Validators.required, Validators.min(0)]],
       fullName: [null, Validators.required],
-      date: [null, Validators.required],
-      paid: [false, Validators.required],
-      paymentDate: [null, Validators.required]
+      date: [null, Validators.required]
     });
+  }
+
+  protected override loadData(data: Fee) {
+    super.loadData(data);
+    this.form.controls['memberNumber'].setValue(data.member.number);
+    this.form.controls['fullName'].setValue(data.member.fullName);
   }
 
 }
