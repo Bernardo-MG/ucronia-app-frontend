d136d4baa0de2a5ba79049d5957df7bcca0f5158
Bernardo-MG <programming@bernardomg.com>
2024-01-21 23:45:04 +0100

    Extracted error interceptor and applied to login

--- src/app/access/password-reset/services/password-reset.service.ts
+++ src/app/access/password-reset/services/password-reset.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserTokenStatus } from '@app/access/models/user-token-status';
 import { ApiResponse } from '@app/core/api/models/api-response';
+import { AngularErrorRequestInterceptor } from '@app/core/api/request/angular-error-request-interceptor';
 import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { PasswordReset } from '../models/password-reset';
 import { PasswordResetRequest } from '../models/password-reset-request';
 
@@ -12,6 +13,8 @@ export class PasswordResetService {
 
   private passwordResetRequestUrl = environment.apiUrl + "/password/reset";
 
+  private errorInteceptor = new AngularErrorRequestInterceptor();
+
   constructor(
     private http: HttpClient
   ) { }
@@ -19,19 +22,28 @@ export class PasswordResetService {
   public requestResetPassword(request: PasswordResetRequest): Observable<ApiResponse<void>> {
     return this.http
       // Reset password request
-      .post<ApiResponse<void>>(this.passwordResetRequestUrl, request);
+      .post<ApiResponse<void>>(this.passwordResetRequestUrl, request)
+      .pipe(
+        catchError(this.errorInteceptor.handle)
+      );;
   }
 
   public resetPassword(token: string, reset: PasswordReset): Observable<ApiResponse<void>> {
     return this.http
       // Validate token request
-      .post<ApiResponse<void>>(`${this.passwordResetRequestUrl}/${token}`, reset);
+      .post<ApiResponse<void>>(`${this.passwordResetRequestUrl}/${token}`, reset)
+      .pipe(
+        catchError(this.errorInteceptor.handle)
+      );;
   }
 
   public validateToken(token: string): Observable<ApiResponse<UserTokenStatus>> {
     return this.http
       // Validate token request
-      .get<ApiResponse<UserTokenStatus>>(`${this.passwordResetRequestUrl}/${token}`);
+      .get<ApiResponse<UserTokenStatus>>(`${this.passwordResetRequestUrl}/${token}`)
+      .pipe(
+        catchError(this.errorInteceptor.handle)
+      );;
   }
 
 }
