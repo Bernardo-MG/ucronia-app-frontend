c5feb81806102c7776754f1f3740e68828472f0e
Bernardo-MG <programming@bernardomg.com>
2024-03-11 21:25:41 +0100

    Removed sorting from pagination parser

--- src/app/shared/utils/api/route/observer/parser/pagination-request-parameters-parser.ts
+++ src/app/shared/utils/api/route/observer/parser/pagination-request-parameters-parser.ts
@@ -19,35 +19,31 @@ export class PaginationRequestParametersParser implements ParametersParser<Pagin
   private sortParser = new SortParametersParser();
 
   public parse(params: ParamMap): Pagination | undefined {
-    let request;
+    let pagination;
 
     if ((params.has('page')) || (params.has('size')) || (params.has('sort'))) {
-      request = new Pagination();
+      pagination = new Pagination();
 
       if (params.has('page')) {
-        request.page = Number(params.get('page'));
-        if (Number.isNaN(request.page)) {
+        pagination.page = Number(params.get('page'));
+        if (Number.isNaN(pagination.page)) {
           // The page was not a number
-          request.page = undefined;
+          pagination.page = undefined;
         }
       }
 
       if (params.has('size')) {
-        request.size = Number(params.get('size'));
-        if (Number.isNaN(request.size)) {
+        pagination.size = Number(params.get('size'));
+        if (Number.isNaN(pagination.size)) {
           // The size was not a number
-          request.size = undefined;
+          pagination.size = undefined;
         }
       }
-
-      if (params.has('sort')) {
-        request.sort = this.sortParser.parse(params);
-      }
     } else {
-      request = undefined;
+      pagination = undefined;
     }
 
-    return request;
+    return pagination;
   }
 
 }
