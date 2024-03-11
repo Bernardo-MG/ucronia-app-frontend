e890581fd2b918a35a85234a6cf5654379da6312
Bernardo-MG <programming@bernardomg.com>
2024-03-11 23:35:01 +0100

    Added routes

--- src/app/association/library/components/library-frontpage/library-frontpage.component.ts
+++ src/app/association/library/components/library-frontpage/library-frontpage.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LayoutModule } from '@app/shared/layout/layout.module';
 import { LibraryBookListComponent } from '../library-book-list/library-book-list.component';
+import { AuthContainer } from '@app/core/authentication/services/auth.service';
 
 @Component({
   selector: 'app-library-frontpage',
@@ -11,5 +12,14 @@ import { LibraryBookListComponent } from '../library-book-list/library-book-list
 export class LibraryFrontpageComponent {
 
   public createPermission = false;
+
+  constructor(
+    private authContainer: AuthContainer
+  ) { }
+
+  ngOnInit(): void {
+    // Check permissions
+    this.createPermission = this.authContainer.hasPermission("library_book", "create");
+  }
 
 }
--- src/app/association/library/library-routing.module.ts
+++ src/app/association/library/library-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LibraryBookCreateComponent } from './components/library-book-create/library-book-create.component';
+import { LibraryBookDetailsComponent } from './components/library-book-details/library-book-details.component';
 import { LibraryFrontpageComponent } from './components/library-frontpage/library-frontpage.component';
 
 
 const routes: Routes = [
-  { path: '', component: LibraryFrontpageComponent }
+  { path: '', component: LibraryFrontpageComponent },
+  { path: 'books/add', component: LibraryBookCreateComponent },
+  { path: 'books/:isbn', component: LibraryBookDetailsComponent }
 ];
 
 @NgModule({
