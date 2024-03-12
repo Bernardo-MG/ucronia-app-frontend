4abd38b1deb01ea8e82cffd504ebe6343a6328fc
Bernardo-MG <programming@bernardomg.com>
2024-03-13 00:19:28 +0100

    Game system form

--- src/app/association/library/components/library-game-system-create-form/library-game-system-create-form.component.ts
+++ src/app/association/library/components/library-game-system-create-form/library-game-system-create-form.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from '@app/shared/form/components/form/form.component';
+import { LayoutModule } from '@app/shared/layout/layout.module';
 import { GameSystem } from '../../models/game-system';
 
 @Component({
   selector: 'library-game-system-create-form',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule, FormsModule, ReactiveFormsModule, LayoutModule ],
   templateUrl: './library-game-system-create-form.component.html'
 })
 export class LibraryGameSystemCreateFormComponent extends FormComponent<GameSystem> {
