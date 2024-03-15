959ee4da32a8377198b11b3a64febec4dde5bdae
Bernardo-MG <programming@bernardomg.com>
2024-03-16 00:02:00 +0100

    Added comments

--- src/app/shared/layout/components/info-editor/info-editor.component.ts
+++ src/app/shared/layout/components/info-editor/info-editor.component.ts
@@ -5,69 +5,93 @@ import { Observable, throwError } from 'rxjs';
 export abstract class InfoEditorComponent<Data> {
 
   /**
-   * Reading flag.
+   * Reading flag. Active while the data is being read.
    */
   public reading = false;
 
   /**
-   * Saving flag.
+   * Saving flag. Active while the data is being saved.
    */
   public saving = false;
 
+  /**
+   * Editing flag. Active while the form is active for editing.
+   */
   public editing = false;
 
+  /**
+   * Editable flag. Active if the user has permissions to edit the data.
+   */
   public editable = false;
 
+  /**
+   * Deletable flag. Active if the user has permissions to delete the data.
+   */
   public deletable = false;
 
+  /**
+   * Error flag. Act
+   */
   public error = false;
 
+  /**
+   * Failures after saving.
+   */
   public failures = new FieldFailures();
 
   /**
-   * Edit button is active flag.
+   * Edit button active flag.
    */
   public get editEnabled() {
-    return (!this.error) && (!this.reading) && this.editable && !this.editing;
+    return (!this.error) && (!this.reading) && (this.editable) && (!this.editing);
   }
 
   /**
-   * Delete button is active flag.
+   * Delete button active flag.
    */
   public get deleteEnabled() {
-    return (!this.error) && (!this.reading) && this.deletable && (!this.editing);
+    return (!this.error) && (!this.reading) && (this.deletable) && (!this.editing);
   }
 
   /**
    * Form enabled flag.
    */
   public get formEnabled() {
-    return this.editable && this.editing && (!this.error);
+    return (this.editable) && (this.editing) && (!this.error);
   }
 
   /**
    * Waiting flag.
    */
   public get waiting() {
-    return this.reading || this.saving;
+    return (this.reading) || (this.saving);
   }
 
   constructor(
     public data: Data
   ) { }
 
+  /**
+   * Start editing template method, changes the component status accordingly.
+   */
   public onStartEditing(): void {
     this.editing = true;
   }
 
+  /**
+   * Save template method. Calls the save hook and updated the component status.
+   *
+   * @param toSave data to save
+   */
   public onSave(toSave: Data): void {
     this.saving = true;
     this.save(toSave).subscribe({
-      next: d => {
-        this.data = d;
+      next: response => {
+        this.data = response;
 
         this.failures.clear();
-        // Reactivate view
+
+        // Reactivate component
         this.saving = false;
         this.editing = false;
       },
@@ -75,9 +99,12 @@ export abstract class InfoEditorComponent<Data> {
         if (error instanceof FailureResponse) {
           this.failures = error.failures;
         } else {
+          // No failure response
+          // Just remove the failures
           this.failures.clear();
         }
-        // Reactivate view
+
+        // Reactivate component
         this.saving = false;
 
         return throwError(() => error);
