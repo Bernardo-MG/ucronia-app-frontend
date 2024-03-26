98d0af7b1ad8ce1a6c4cdde8379b9004abbdb61e
Bernardo-MG <programming@bernardomg.com>
2024-03-26 23:08:03 +0100

    Corrections to service

--- src/app/core/layout/services/layout.service.ts
+++ src/app/core/layout/services/layout.service.ts
@@ -13,15 +13,24 @@ export class LayoutService {
     private authContainer: AuthContainer
   ) { }
 
-
+  /**
+   * Retrieves the title of the application.
+   * 
+   * @returns The title of the application.
+   */
   public getTitle(): string {
     return 'AR Ucronía';
   }
 
+  /**
+   * Retrieves the menu options for the application.
+   * 
+   * @returns An array of menu options.
+   */
   public getMenus(): Menu[] {
-    const menus = [];
+    const menus: Menu[] = [];
 
-    const associationLinks = this.filterNodes([
+    const associationMenuLinks = this.filterNodes([
       { title: 'Members', path: '/members', resource: 'member' },
       { title: 'Fees', path: '/fees', resource: 'fee' },
       { title: 'Funds', path: '/funds', resource: 'funds' },
@@ -29,27 +38,35 @@ export class LayoutService {
       { title: 'Configuration', path: '/configuration', resource: 'association_configuration' }
     ]);
 
-    if (associationLinks.length > 0) {
-      menus.push({ title: 'Association', links: associationLinks });
+    if (associationMenuLinks.length > 0) {
+      menus.push({ title: 'Association', links: associationMenuLinks });
     }
 
-    const securityLinks = this.filterNodes([
+    const securityMenuLinks = this.filterNodes([
       { title: 'Users', path: '/users', resource: 'user' },
       { title: 'Roles', path: '/roles', resource: 'role' },
       { title: 'User tokens', path: '/user-tokens', resource: 'user_token' }
     ]);
 
-    if (securityLinks.length > 0) {
-      menus.push({ title: 'Security', links: securityLinks });
+    if (securityMenuLinks.length > 0) {
+      menus.push({ title: 'Security', links: securityMenuLinks });
     }
 
     return menus;
   }
 
+  /**
+   * Filters the menu links based on user permissions.
+   * 
+   * @param links The menu links to filter.
+   * @returns An array of filtered menu links.
+   */
   private filterNodes(links: AuthMenuLink[]): MenuLink[] {
     return links
       // Only links the user has permissions for
-      .filter(link => this.authContainer.hasPermission(link.resource, 'view'));
+      .filter(link => this.authContainer.hasPermission(link.resource, 'view'))
+      // Map to MenuLink objects
+      .map(link => ({ title: link.title, path: link.path }));
   }
 
 }
