b4009379f8269af92a84b7eaa64e63b21543a296
Bernardo-MG <programming@bernardomg.com>
2024-03-29 15:24:51 +0100

    Reusing side menu

--- src/app/core/layout/components/main-layout/main-layout.component.ts
+++ src/app/core/layout/components/main-layout/main-layout.component.ts
@@ -4,13 +4,13 @@ import { RouterModule } from '@angular/router';
 import { Menu } from '@app/shared/menu/models/menu';
 import { LayoutService } from '../../services/layout.service';
 import { HeaderNavigationWrapperComponent } from '../header-navigation-wrapper/header-navigation-wrapper.component';
+import { SideMenuComponent } from '../side-menu/side-menu.component';
 
 @Component({
   selector: 'layout-main',
   standalone: true,
-  imports: [CommonModule, RouterModule, HeaderNavigationWrapperComponent],
-  templateUrl: './main-layout.component.html',
-  styleUrls: ['./main-layout.component.sass']
+  imports: [CommonModule, RouterModule, HeaderNavigationWrapperComponent, SideMenuComponent],
+  templateUrl: './main-layout.component.html'
 })
 export class MainLayoutComponent implements OnInit {
 
---
+++ src/app/core/layout/components/side-menu/side-menu.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Menu } from '@app/shared/menu/models/menu';
+
+@Component({
+  selector: 'layout-side-menu',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './side-menu.component.html',
+  styleUrl: './side-menu.component.sass'
+})
+export class SideMenuComponent {
+
+  @Input() public menus: Menu[] = [];
+
+}
