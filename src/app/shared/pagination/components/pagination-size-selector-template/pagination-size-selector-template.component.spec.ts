f3e05bdc53cf8c60d7413a2c0125b19a9d06ccd3
Bernardo-MG <programming@bernardomg.com>
2024-03-30 01:51:13 +0100

    Added comments to tests

--- src/app/shared/pagination/components/pagination-size-selector-template/pagination-size-selector-template.component.spec.ts
+++ src/app/shared/pagination/components/pagination-size-selector-template/pagination-size-selector-template.component.spec.ts
@@ -19,9 +19,17 @@ describe('PaginationSizeSelectorTemplateComponent', () => {
     fixture.detectChanges();
   });
 
+  // **************************************************************************
+  // General tests
+  // **************************************************************************
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // **************************************************************************
+  // Sizes selection values
+  // **************************************************************************
 
   it('should create no option when no values were received', () => {
     component.sizes = [];
@@ -75,6 +83,10 @@ describe('PaginationSizeSelectorTemplateComponent', () => {
 
     expect(size).toEqual(2);
   });
+
+  // **************************************************************************
+  // Disabled
+  // **************************************************************************
 
   it('should disable the input when the component is disabled', () => {
     component.disabled = true;
