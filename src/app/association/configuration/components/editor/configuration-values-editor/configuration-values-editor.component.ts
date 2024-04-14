b9d1520123b2ad9dd64c9041b3e01f9ea6231a4a
Bernardo-MG <programming@bernardomg.com>
2024-04-14 19:28:05 +0200

    Enable field edition

--- src/app/association/configuration/components/editor/configuration-values-editor/configuration-values-editor.component.ts
+++ src/app/association/configuration/components/editor/configuration-values-editor/configuration-values-editor.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { Configuration } from '@app/association/configuration/models/configuration';
 import { Failure } from '@app/core/api/models/failure';
 import { IconsModule } from '@app/shared/icons/icons.module';
@@ -8,12 +8,14 @@ import { IconsModule } from '@app/shared/icons/icons.module';
 @Component({
   selector: 'assoc-configuration-values-editor',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, IconsModule],
+  imports: [CommonModule, FormsModule, IconsModule],
   templateUrl: './configuration-values-editor.component.html'
 })
 export class ConfigurationValuesEditorComponent {
 
   @Input() public configurations: Configuration[] = [];
+
+  public activeConfig = '';
 
   public isFieldInvalid(property: string): boolean {
     return false;
@@ -21,6 +23,14 @@ export class ConfigurationValuesEditorComponent {
 
   public getFailures(property: string): Failure[] {
     return [];
+  }
+
+  public onEdit(key: string){
+    this.activeConfig = key;
+  }
+
+  public isEditing(key: string): boolean {
+    return this.activeConfig !== key;
   }
 
 }
