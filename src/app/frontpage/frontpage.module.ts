d61ffdc537d3628b126313f6b94ae7b2e3465bf7
Bernardo-MG <programming@bernardomg.com>
2024-04-14 22:27:44 +0200

    Recovered frontpage

--- src/app/frontpage/frontpage.module.ts
+++ src/app/frontpage/frontpage.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FrontpageRoutingModule } from './frontpage-routing.module';
 import { FrontpageService } from './service/frontpage.service';
 
 
 
 @NgModule({
+  imports: [
+    FrontpageRoutingModule
+  ],
   providers: [
     FrontpageService
   ]
