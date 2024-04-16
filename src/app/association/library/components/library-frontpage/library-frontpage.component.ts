1627c85ad86afbd328433e0975803d649a603dcc
Bernardo-MG <programming@bernardomg.com>
2024-04-17 00:31:29 +0200

    Library admin is accessible from the library view

--- src/app/association/library/components/library-frontpage/library-frontpage.component.ts
+++ src/app/association/library/components/library-frontpage/library-frontpage.component.ts
@@ -1,26 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthContainer } from '@app/core/authentication/services/auth.service';
+import { IconsModule } from '@app/shared/icons/icons.module';
 import { ArticleComponent } from '@app/shared/layout/components/article/article.component';
 import { LibraryBookListComponent } from '../library-book-list/library-book-list.component';
 
 @Component({
   selector: 'assoc-library-frontpage',
   standalone: true,
-  imports: [RouterModule, LibraryBookListComponent, ArticleComponent],
+  imports: [RouterModule, IconsModule, LibraryBookListComponent, ArticleComponent],
   templateUrl: './library-frontpage.component.html'
 })
 export class LibraryFrontpageComponent {
 
-  public createBookPermission = false;
-
-  public createBookTypePermission = false;
-
-  public createGameSystemPermission = false;
-
-  public createAuthorPermission = false;
-
-  public createPublisherPermission = false;
+  public adminPermission = false;
 
   constructor(
     private authContainer: AuthContainer
@@ -28,11 +21,7 @@ export class LibraryFrontpageComponent {
 
   ngOnInit(): void {
     // Check permissions
-    this.createBookPermission = this.authContainer.hasPermission("library_book", "create");
-    this.createBookTypePermission = this.authContainer.hasPermission("library_book_type", "create");
-    this.createGameSystemPermission = this.authContainer.hasPermission("library_game_system", "create");
-    this.createAuthorPermission = this.authContainer.hasPermission("library_author", "create");
-    this.createPublisherPermission = this.authContainer.hasPermission("library_publisher", "create");
+    this.adminPermission = this.authContainer.hasPermission("library_admin", "view");
   }
 
 }
--- src/app/core/layout/services/menu-options.ts
+++ src/app/core/layout/services/menu-options.ts
@@ -8,8 +8,7 @@ export const MENU_OPTIONS: { [key: string]: { title: string, links: AuthMenuLink
       { title: 'Members', path: '/members', resource: 'member' },
       { title: 'Fees', path: '/fees', resource: 'fee' },
       { title: 'Funds', path: '/funds', resource: 'funds' },
-      { title: 'Library', path: '/library', resource: 'library' },
-      { title: 'Library Admin', path: '/library/admin', resource: 'library_admin' }
+      { title: 'Library', path: '/library', resource: 'library' }
     ]
   },
   security: {
