3ab98273f58769610191abca4b78832adca186aa
Bernardo-MG <programming@bernardomg.com>
2024-04-18 23:27:48 +0200

    Sorted components

--- src/app/association/members/members-routing.module.ts
+++ src/app/association/members/members-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MemberCreateComponent } from './components/edit/member-create/member-create.component';
-import { MemberFrontpageComponent } from './components/frontpage/member-frontpage/member-frontpage.component';
+import { MemberCreateComponent } from './components/views/member-create/member-create.component';
 import { MemberInfoEditorComponent } from './components/edit/member-info-editor/member-info-editor.component';
+import { MemberFrontpageComponent } from './components/views/member-frontpage/member-frontpage.component';
 
 
 const routes: Routes = [
