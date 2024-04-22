4a34be8cada3a9efcaa7bf96a2bf8c45224ad80c
Bernardo-MG <programming@bernardomg.com>
2024-04-22 22:44:52 +0200

    Split chart

---
+++ src/app/association/fees/components/report/fee-payment-chart/fee-payment-chart.component.spec.ts
@@ -0,0 +1,30 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FeeReportService } from '@app/association/fees/services/fee-report.service';
+import { FeePaymentChartComponent } from './fee-payment-chart.component';
+
+describe('FeePaymentChartWidgetComponent', () => {
+  let component: FeePaymentChartComponent;
+  let fixture: ComponentFixture<FeePaymentChartComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        FeePaymentChartComponent
+      ],
+      providers: [
+        FeeReportService
+      ]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(FeePaymentChartComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
