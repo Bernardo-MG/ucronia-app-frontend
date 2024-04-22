db7ebc7c3083331fc652ccc2e2170f2511206b61
Bernardo-MG <programming@bernardomg.com>
2024-04-22 23:36:52 +0200

    Added new view to menu

---
+++ src/app/association/user-fees/user-fees-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { UserFeeFrontpageComponent } from './components/user-fee-frontpage/user-fee-frontpage.component';
+
+
+const routes: Routes = [
+  { path: '', component: UserFeeFrontpageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class UserFeesRoutingModule { }
--- src/app/association/user-fees/user-fees.module.ts
+++ src/app/association/user-fees/user-fees.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { UserFeeService } from './services/user-fee.service';
+import { UserFeesRoutingModule } from './user-fees-routing.module';
 
 
 
 @NgModule({
+  imports: [
+    UserFeesRoutingModule
+  ],
   providers: [
     UserFeeService
   ]
