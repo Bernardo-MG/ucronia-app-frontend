29e91219babe215f4f75192b0597a11299738aeb
Bernardo-MG <programming@bernardomg.com>
2024-05-12 23:51:59 +0200

    Fixed test imports

--- src/app/association/layout/components/association-layout/association-layout.component.spec.ts
+++ src/app/association/layout/components/association-layout/association-layout.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { AssociationLayoutComponent } from './association-layout.component';
 
 describe('AssociationLayoutComponent', () => {
@@ -8,7 +8,10 @@ describe('AssociationLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AssociationLayoutComponent]
+      imports: [
+        RouterTestingModule,
+        AssociationLayoutComponent
+      ]
     })
     .compileComponents();
     
