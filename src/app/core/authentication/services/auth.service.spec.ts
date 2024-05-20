90c61383899f77ccbb9ab4bd9213377107ebbddd
Bernardo-MG <programming@bernardomg.com>
2024-05-21 00:00:39 +0200

    Added test

--- src/app/core/authentication/services/auth.service.spec.ts
+++ src/app/core/authentication/services/auth.service.spec.ts
@@ -75,7 +75,7 @@ describe('AuhtContainer', () => {
     expect(service.isLogged()).toBeTrue();
   });
 
-  it('should validate the user has an existing action', () => {
+  it('should validate the user has an existing resource and action', () => {
     const loginStatus: LoginStatus = {
       logged: true,
       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0VXNlciIsImlhdCI6MTUxNjIzOTAyMiwicGVybWlzc2lvbnMiOnsicmVzb3VyY2UiOlsiYWN0aW9uIl19fQ.4mVqVsV7_v-TMGCKnObvoD55pLWwRSqv600RAZxqtVs'
@@ -85,7 +85,7 @@ describe('AuhtContainer', () => {
     expect(service.hasPermission('resource', 'action')).toBeTrue();
   });
 
-  it('should validate the user does not have a not existing action', () => {
+  it('should validate the user does not have an existing resource with a not existing action', () => {
     const loginStatus: LoginStatus = {
       logged: true,
       token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0VXNlciIsImlhdCI6MTUxNjIzOTAyMiwicGVybWlzc2lvbnMiOnsicmVzb3VyY2UiOlsiYWN0aW9uIl19fQ.4mVqVsV7_v-TMGCKnObvoD55pLWwRSqv600RAZxqtVs'
@@ -93,6 +93,16 @@ describe('AuhtContainer', () => {
 
     service.setDetails(loginStatus, false);
     expect(service.hasPermission('resource', 'invalidAction')).toBeFalse();
+  });
+
+  it('should validate the user does not have an existing resource and action', () => {
+    const loginStatus: LoginStatus = {
+      logged: true,
+      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0ZXN0VXNlciIsImlhdCI6MTUxNjIzOTAyMiwicGVybWlzc2lvbnMiOnsicmVzb3VyY2UiOlsiYWN0aW9uIl19fQ.4mVqVsV7_v-TMGCKnObvoD55pLWwRSqv600RAZxqtVs'
+    };
+
+    service.setDetails(loginStatus, false);
+    expect(service.hasPermission('invalidResource', 'action')).toBeFalse();
   });
 
   it('should load details from local storage', () => {
