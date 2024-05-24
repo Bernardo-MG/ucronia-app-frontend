373c7c620cf0bcfa855286affe68cb2a43804af9
Bernardo-MG <programming@bernardomg.com>
2024-05-24 21:28:35 +0200

    Removed unused import

--- src/app/frontpage/components/layout/public-navbar/public-navbar.component.ts
+++ src/app/frontpage/components/layout/public-navbar/public-navbar.component.ts
@@ -1,13 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AccountMenuComponent } from '@app/core/layout/components/header/account-dropdown/account-dropdown.component';
 import { IconsModule } from '@app/shared/icons/icons.module';
 
 @Component({
   selector: 'layout-public-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, IconsModule, AccountMenuComponent],
+  imports: [CommonModule, RouterModule, IconsModule],
   templateUrl: './public-navbar.component.html'
 })
 export class PublicNavbarComponent {
