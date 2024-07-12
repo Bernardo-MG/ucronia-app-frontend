70037c6e134847ae4b691ecdb3e483576c1c22d6
Bernardo-MG <programming@bernardomg.com>
2024-07-12 21:32:54 +0200

    Using more concrete component

--- src/app/association/library-lending/components/views/library-lending-frontpage/library-lending-frontpage.component.ts
+++ src/app/association/library-lending/components/views/library-lending-frontpage/library-lending-frontpage.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { LibraryLendingService } from '@app/association/library-lending/services/library-lending.service';
 import { Book } from '@app/association/library/models/book';
-import { BookLending } from '@app/association/library/models/book-lending';
 import { BookLent } from '@app/association/library/models/book-lent';
 import { Person } from '@app/association/library/models/person';
 import { PaginatedResponse } from '@app/core/api/models/paginated-response';
-import { AuthContainer } from '@app/core/authentication/services/auth.service';
-import { InfoEditorStatusComponent } from '@app/shared/form/components/info-editor-status/info-editor-status.component';
+import { CreateComponent } from '@app/shared/form/components/create/create.component';
 import { ArticleComponent } from '@app/shared/layout/components/article/article.component';
 import { Observable } from 'rxjs';
 import { BookLendingFormComponent } from '../../data/book-lending-form/book-lending-form.component';
@@ -17,7 +16,7 @@ import { BookLendingFormComponent } from '../../data/book-lending-form/book-lend
   imports: [ArticleComponent, BookLendingFormComponent],
   templateUrl: './library-lending-frontpage.component.html'
 })
-export class LibraryLendingFrontpageComponent extends InfoEditorStatusComponent<BookLent> implements OnInit {
+export class LibraryLendingFrontpageComponent extends CreateComponent<BookLent> implements OnInit {
 
   public memberPage = new PaginatedResponse<Person[]>([]);
 
@@ -29,16 +28,12 @@ export class LibraryLendingFrontpageComponent extends InfoEditorStatusComponent<
 
   constructor(
     private lendingService: LibraryLendingService,
-    private authContainer: AuthContainer
+    rt: Router
   ) {
-    super(new BookLent());
+    super(rt);
   }
 
   ngOnInit(): void {
-    // Check permissions
-    this.editable = this.authContainer.hasPermission("library_lending", "update");
-    this.deletable = this.authContainer.hasPermission("library_lending", "delete");
-
     // Load initial data
     this.onGoToBookPage(0);
     this.onGoToMemberPage(0);
@@ -78,16 +73,12 @@ export class LibraryLendingFrontpageComponent extends InfoEditorStatusComponent<
     });
   }
 
-  protected override delete(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  protected override read(): Observable<BookLent> {
-    throw new Error('Method not implemented.');
-  }
-
   protected override save(toSave: BookLent): Observable<BookLent> {
     return this.lendingService.lend(toSave);
+  }
+
+  protected override getReturnRoute(saved: BookLent): string {
+    return '/library';
   }
 
 }
