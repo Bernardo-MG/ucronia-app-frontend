87f222b7e5ed04a6bc9f94f9520570b1f907da42
Bernardo-MG <programming@bernardomg.com>
2024-07-31 17:47:38 +0200

    Testing new structure

--- src/app/frontpage/components/layout/public-layout/public-layout.component.ts
+++ src/app/frontpage/components/layout/public-layout/public-layout.component.ts
@@ -3,8 +3,6 @@ import { RouterModule } from '@angular/router';
 import { SideMenuComponent } from '@app/core/layout/components/side/side-menu/side-menu.component';
 import { LayoutService } from '@app/core/layout/services/layout.service';
 import { PublicNavbarComponent } from '@app/frontpage/components/layout/public-navbar/public-navbar.component';
-import { FrontpageService } from '@app/frontpage/services/frontpage.service';
-import { Menu } from '@app/shared/menu/models/menu';
 
 @Component({
   selector: 'app-public-layout',
@@ -16,19 +14,13 @@ export class PublicLayoutComponent {
 
   public title = '';
 
-  public menus: Menu[] = [];
-
   constructor(
-    private layoutService: LayoutService,
-    private frontpageService: FrontpageService
+    private layoutService: LayoutService
   ) { }
 
   ngOnInit(): void {
     // App title
     this.title = this.layoutService.getTitle();
-
-    // Public menus
-    this.menus = this.frontpageService.getMenus();
   }
 
 }
--- src/app/frontpage/frontpage-routing.module.ts
+++ src/app/frontpage/frontpage-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ActivitiesComponent } from './components/views/activities/activities.component';
 import { HighlightsComponent } from './components/views/highlights/highlights.component';
-import { ContactComponent } from './components/views/contact/contact.component';
 
 
 const routes: Routes = [
   {
     path: '',
     children: [
-      { path: '', component: HighlightsComponent },
-      { path: 'activities', component: ActivitiesComponent },
-      { path: 'contact', component: ContactComponent }
+      { path: '', component: HighlightsComponent }
     ]
   }
 ];
