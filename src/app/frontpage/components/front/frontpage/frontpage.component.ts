0b6f1f3520594c2e9e970b9d3ab5e93234b29105
Bernardo-MG <programming@bernardomg.com>
2024-08-11 14:28:37 +0200

    Removed unused import

--- src/app/frontpage/components/front/frontpage/frontpage.component.ts
+++ src/app/frontpage/components/front/frontpage/frontpage.component.ts
@@ -4,7 +4,6 @@ import { FrontpageService } from '@app/frontpage/services/frontpage.service';
 import { ArticleComponent } from '@app/shared/layout/components/article/article.component';
 import { WaitingOverlayComponent } from '@app/shared/layout/components/waiting-overlay/waiting-overlay.component';
 import { TeamupCalendarComponent } from '@app/shared/social/components/teamup-calendar/teamup-calendar.component';
-import * as bootstrap from 'bootstrap';
 import { FrontpageActivityCalendarWidgetComponent } from '../frontpage-activity-calendar/frontpage-activity-calendar.component';
 import { FrontpageContactUsComponent } from '../frontpage-contact-us/frontpage-contact-us.component';
 import { PublicLocationWidgetComponent } from '../frontpage-location/frontpage-location.component';
