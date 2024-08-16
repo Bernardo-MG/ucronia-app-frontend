f93b7742c6884d320dd2dfddaf4b23c071a060da
Bernardo-MG <programming@bernardomg.com>
2024-08-17 00:53:12 +0200

    First attempt at field validation status directive

--- src/app/association/members/components/data/member-form/member-form.component.ts
+++ src/app/association/members/components/data/member-form/member-form.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormComponent } from '@app/shared/form/components/form/form.component';
+import { InvalidFieldDirective } from '@app/shared/form/directives/invalid-field.directive';
 import { FormModule } from '@app/shared/form/form.module';
 import { WaitingButtonComponent } from '@app/shared/layout/components/waiting-button/waiting-button.component';
 import { Member } from '../../../models/member';
@@ -9,7 +10,7 @@ import { Member } from '../../../models/member';
 @Component({
   selector: 'assoc-member-form',
   standalone: true,
-  imports: [CommonModule, FormModule, ReactiveFormsModule, WaitingButtonComponent],
+  imports: [CommonModule, FormModule, ReactiveFormsModule, WaitingButtonComponent, InvalidFieldDirective],
   templateUrl: './member-form.component.html'
 })
 export class MemberFormComponent extends FormComponent<Member> {
@@ -21,7 +22,7 @@ export class MemberFormComponent extends FormComponent<Member> {
 
     this.form = fb.group({
       name: fb.group({
-        firstName: [null, Validators.required],
+        firstName: [null],
         lastName: ['']
       }),
       identifier: [''],
