9ea5c2f53f931547c963f753d6f9411f49e4e8de
Bernardo-MG <programming@bernardomg.com>
2024-08-17 02:36:28 +0200

    Fixed test initialization

--- src/app/shared/form/directives/invalid-field.directive.spec.ts
+++ src/app/shared/form/directives/invalid-field.directive.spec.ts
@@ -1,8 +1,28 @@
+import { ElementRef, Renderer2 } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { InvalidFieldDirective } from './invalid-field.directive';
 
 describe('InvalidFieldDirective', () => {
+  let directive: InvalidFieldDirective;
+  let elementRefMock: ElementRef;
+  let rendererMock: Renderer2;
+  
+  beforeEach(() => {
+    elementRefMock = new ElementRef(document.createElement('div'));
+    rendererMock = jasmine.createSpyObj('Renderer2', ['addClass']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        InvalidFieldDirective,
+        { provide: ElementRef, useValue: elementRefMock },
+        { provide: Renderer2, useValue: rendererMock }
+      ]
+    });
+
+    directive = TestBed.inject(InvalidFieldDirective);
+  });
+
   it('should create an instance', () => {
-    const directive = new InvalidFieldDirective();
     expect(directive).toBeTruthy();
   });
 });
