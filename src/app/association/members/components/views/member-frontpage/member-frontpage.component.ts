3147af23e51333779b92a0fcae117f39bc707d62
Bernardo-MG <programming@bernardomg.com>
2024-08-26 20:59:16 +0200

    Updated imports

--- src/app/association/members/components/views/member-frontpage/member-frontpage.component.ts
+++ src/app/association/members/components/views/member-frontpage/member-frontpage.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { ArticleComponent } from '@app/shared/layout/components/article/article.component';
 import { JustifyCenterDirective } from '@app/shared/style/directives/justify-center.directive';
-import { MemberBalanceChartWidgetComponent } from '../../balance/member-balance-chart-widget/member-balance-chart-widget.component';
-import { MemberListWidgetComponent } from '../../list/member-list-widget/member-list-widget.component';
+import { MemberBalanceChartWidgetComponent } from '../../core/member-balance-chart-widget/member-balance-chart-widget.component';
+import { MemberListWidgetComponent } from '../../core/member-list-widget/member-list-widget.component';
 
 @Component({
   selector: 'assoc-member-frontpage',
