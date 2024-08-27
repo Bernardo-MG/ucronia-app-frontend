2faa28c103bc9ec3b1b3ae00a228a3a36fd95d4f
Bernardo-MG <programming@bernardomg.com>
2024-08-27 12:20:35 +0200

    Changed transaction display

--- src/app/association/funds/core/components/transaction-info/transaction-info.component.ts
+++ src/app/association/funds/core/components/transaction-info/transaction-info.component.ts
@@ -1,14 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormModule } from '@app/shared/form/form.module';
 import { Transaction } from '../../../shared/models/transaction';
 
 @Component({
   selector: 'assoc-transaction-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormModule],
   templateUrl: './transaction-info.component.html'
 })
 export class TransactionInfoComponent {
 
   @Input() public data = new Transaction();
+
+  @Input() public showMenu = false;
+
+  @Input() public deletable = false;
+
+  @Input() public editable = false;
+
+  @Output() public delete = new EventEmitter<void>();
+
+  @Output() public startEditing = new EventEmitter<void>();
 
 }
