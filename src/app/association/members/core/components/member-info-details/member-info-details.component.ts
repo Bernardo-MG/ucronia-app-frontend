ad8a020ca9afb00924364749c3c70cf2c3deb39a
Bernardo-MG <programming@bernardomg.com>
2024-08-30 00:15:39 +0200

    Applied waiting directive

--- src/app/association/members/core/components/member-info-details/member-info-details.component.ts
+++ src/app/association/members/core/components/member-info-details/member-info-details.component.ts
@@ -2,15 +2,18 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Member } from '@app/association/members/shared/models/member';
 import { IconsModule } from '@app/shared/icons/icons.module';
+import { PlaceholderDirective } from '@app/shared/layout/directives/placeholder.directive';
 
 @Component({
   selector: 'assoc-member-info-details',
   standalone: true,
-  imports: [CommonModule, IconsModule],
+  imports: [CommonModule, IconsModule, PlaceholderDirective],
   templateUrl: './member-info-details.component.html'
 })
 export class MemberInfoDetailsComponent {
 
   @Input() data = new Member();
+
+  @Input() waiting = false;
 
 }
