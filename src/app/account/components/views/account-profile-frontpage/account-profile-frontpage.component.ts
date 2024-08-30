8cfc15a0a8637f4c6c53b14dc8d8ca12bb2d83da
Bernardo-MG <programming@bernardomg.com>
2024-08-30 10:53:41 +0200

    Corrected profile display

--- src/app/account/components/views/account-profile-frontpage/account-profile-frontpage.component.ts
+++ src/app/account/components/views/account-profile-frontpage/account-profile-frontpage.component.ts
@@ -3,15 +3,16 @@ import { Component, OnInit } from '@angular/core';
 import { Account } from '@app/account/models/account';
 import { AccountService } from '@app/account/services/account.service';
 import { Member } from '@app/association/members/shared/models/member';
+import { CardModule } from '@app/shared/card/card.module';
 import { ArticleComponent } from '@app/shared/layout/components/article/article.component';
+import { ResponsiveShortColumnsDirective } from '@app/shared/style/directives/responsive-columns.directive';
 import { AccountProfileInfoComponent } from '../../profile/account-profile-info/account-profile-info.component';
 import { AccountProfileMemberComponent } from '../../profile/account-profile-member/account-profile-member.component';
-import { CardModule } from '@app/shared/card/card.module';
 
 @Component({
   selector: 'account-profile-frontpage',
   standalone: true,
-  imports: [CommonModule, ArticleComponent, CardModule, AccountProfileInfoComponent, AccountProfileMemberComponent],
+  imports: [CommonModule, ArticleComponent, CardModule, AccountProfileInfoComponent, AccountProfileMemberComponent, ResponsiveShortColumnsDirective],
   templateUrl: './account-profile-frontpage.component.html'
 })
 export class AccountProfileFrontpageComponent implements OnInit {
