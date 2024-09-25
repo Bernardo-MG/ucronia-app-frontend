3e5babcd7ae06a60341d3306389c3543ec2f27ea
Bernardo-MG <programming@bernardomg.com>
2024-09-25 23:34:35 +0200

    Relocated layout components

--- src/app/core/layout/services/security-layout.service.ts
+++ src/app/core/layout/services/security-layout.service.ts
@@ -4,7 +4,7 @@ import { AuthMenuLink } from '@app/core/layout/model/auth-menu-link';
 import { Menu } from '@app/shared/menu/models/menu';
 import { MenuLink } from '@app/shared/menu/models/menu-link';
 import { MenuLoader } from '@app/shared/menu/utils/menu-loader';
-import { MENU_OPTIONS } from './menu-options';
+import { SECURITY_MENU_OPTIONS } from './security-menu-options';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,7 @@ export class SecurityLayoutService {
   constructor(
     private authContainer: AuthContainer
   ) {
-    this.menus = new MenuLoader().load(MENU_OPTIONS, (links) => this.filterNodes(links as AuthMenuLink[]));
+    this.menus = new MenuLoader().load(SECURITY_MENU_OPTIONS, (links) => this.filterNodes(links as AuthMenuLink[]));
   }
 
   /**
--- src/app/core/layout/services/security-menu-options.ts
+++ src/app/core/layout/services/security-menu-options.ts
@@ -2,7 +2,7 @@ import { AuthMenuLink } from '@app/core/layout/model/auth-menu-link';
 import { MenuOptions } from '@app/shared/menu/models/menu-options';
 
 // Define menu options with titles for each section
-export const MENU_OPTIONS: MenuOptions = {
+export const SECURITY_MENU_OPTIONS: MenuOptions = {
   security: {
     title: 'Seguridad',
     links: [
