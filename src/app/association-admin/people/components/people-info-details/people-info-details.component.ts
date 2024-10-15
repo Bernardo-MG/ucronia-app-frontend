a1c4c5e4e04dfd9a42686733db2d96dc2e1d9678
Bernardo-MG <programming@bernardomg.com>
2024-10-16 00:04:31 +0200

    Added missing dependency

--- src/app/association-admin/people/components/people-info-details/people-info-details.component.ts
+++ src/app/association-admin/people/components/people-info-details/people-info-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Person } from '@app/models/person/person';
+import { PlaceholderDirective } from '@app/shared/layout/directives/placeholder.directive';
 
 @Component({
   selector: 'assoc-people-info-details',
   standalone: true,
-  imports: [],
+  imports: [PlaceholderDirective],
   templateUrl: './people-info-details.component.html'
 })
 export class PeopleInfoDetailsComponent {
