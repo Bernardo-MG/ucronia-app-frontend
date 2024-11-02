e2fd75535928a84b939a7111834a172acc224f08
Bernardo-MG <programming@bernardomg.com>
2024-11-02 01:18:04 +0100

    Added return compponent

--- src/app/association-admin/library-lending/components/book-lending-return/book-lending-return.component.ts
+++ src/app/association-admin/library-lending/components/book-lending-return/book-lending-return.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { LibraryBookReturnComponent } from '../../shared/components/library-book-return/library-book-return.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BookAdminService } from '@app/association-admin/library-admin/services/book-admin.service';
+import { Book } from '@app/models/library/book';
 
 @Component({
   selector: 'assoc-book-lending-return',
   standalone: true,
-  imports: [],
+  imports: [LibraryBookReturnComponent],
   templateUrl: './book-lending-return.component.html'
 })
 export class BookLendingReturnComponent {
+
+  public data = new Book();
+
+  private index = -1;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: BookAdminService
+  ) { }
+
+  public ngOnInit(): void {
+    // Get id
+    this.route.paramMap.subscribe(params => {
+      const indexParam = params.get('number');
+      if (indexParam) {
+        this.index = Number(indexParam);
+      }
+      this.load();
+    });
+  }
+
+  public onSaved() {
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  private load() {
+    this.service.getOne(this.index).subscribe({
+      next: response => {
+        this.data = response;
+      },
+      error: error => {
+      }
+    });
+  }
 
 }
