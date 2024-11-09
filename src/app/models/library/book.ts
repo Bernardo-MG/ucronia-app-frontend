c0a09925262f7475cc57b60270eac764df3a87af
Bernardo-MG <programming@bernardomg.com>
2024-11-10 00:21:09 +0100

    Extended model

--- src/app/models/library/book.ts
+++ src/app/models/library/book.ts
@@ -4,13 +4,16 @@ import { BookLending } from "./book-lending";
 import { BookType } from "./book-type";
 import { GameSystem } from "./game-system";
 import { Publisher } from "./publisher";
+import { Title } from "./title";
 
 export class Book {
   number = -1;
-  title = '';
+  title = new Title();
   lent = false;
   isbn = '';
   language = '';
+  publishDate = '';
+  donationDate = '';
   authors: Author[] = [];
   donors: Person[] = [];
   lendings: BookLending[] = [];
---
+++ src/app/models/library/title.ts
@@ -0,0 +1,6 @@
+
+export class Title {
+  pretitle = '';
+  title = '';
+  subtitle = '';
+}
