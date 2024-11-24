eb968b18f2e83738e2a78bc8448498c8eecdd365
Bernardo-MG <programming@bernardomg.com>
2024-11-24 23:48:41 +0100

    Fixed test imports

--- src/app/association-admin/library-admin/components/library-admin-frontpage/library-admin-frontpage.component.spec.ts
+++ src/app/association-admin/library-admin/components/library-admin-frontpage/library-admin-frontpage.component.spec.ts
@@ -1,9 +1,9 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { BookAdminService } from '../../book/services/book-admin.service';
 import { AuthorAdminService } from '../../services/author-admin.service';
 import { BookTypeAdminService } from '../../services/book-type-admin.service';
-import { BookAdminService } from '../../services/book-admin.service';
 import { GameSystemAdminService } from '../../services/game-system-admin.service';
 import { PublisherAdminService } from '../../services/publisher-admin.service';
 import { LibraryAdminFrontpageComponent } from './library-admin-frontpage.component';
