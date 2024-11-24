4205e86af5ef1513ee083d2ded7490adbb0055fe
Bernardo-MG <programming@bernardomg.com>
2024-11-24 13:13:28 +0100

    Added todo

--- src/app/association-admin/library-admin/book/containers/library-admin-book-edition/library-admin-book-edition.container.ts
+++ src/app/association-admin/library-admin/book/containers/library-admin-book-edition/library-admin-book-edition.container.ts
@@ -107,6 +107,7 @@ export class LibraryAdminBookInfoEditorContainer extends InfoEditorStatusCompone
   }
 
   protected override delete(): void {
+    // TODO: shouldn't the delete return the observable, to keep consistency?
     this.service.delete(this.data.number).subscribe(r => {
       this.router.navigate(['../..'], { relativeTo: this.route });
     });
