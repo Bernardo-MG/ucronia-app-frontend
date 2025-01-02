a4650c8a1a18b7cbe9211c7bd5afa9288982a134
Bernardo-MG <programming@bernardomg.com>
2025-01-02 01:00:44 +0100

    Fixed bug

--- src/app/association-admin/library-admin/frontpage/containers/library-admin-listing/library-admin-listing.component.ts
+++ src/app/association-admin/library-admin/frontpage/containers/library-admin-listing/library-admin-listing.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { BookReportService } from '@app/association-admin/library-admin/report/services/book-report.service';
 import { PaginatedResponse } from '@app/core/api/models/paginated-response';
@@ -100,7 +100,8 @@ export class LibraryAdminListingContainer implements OnInit {
 
   constructor(
     private authContainer: AuthContainer,
-    private reportService: BookReportService
+    private reportService: BookReportService,
+    private cdr: ChangeDetectorRef
   ) { }
 
   public ngOnInit(): void {
@@ -118,6 +119,7 @@ export class LibraryAdminListingContainer implements OnInit {
 
   public onWait(wait: boolean) {
     this.waiting = wait;
+    this.cdr.detectChanges();
   }
 
   public load(page: number) {
