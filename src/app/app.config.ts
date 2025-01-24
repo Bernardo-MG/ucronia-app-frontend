184ee7f613f1f863dd53164a92a9e7b7dcd8df89
Bernardo-MG <programming@bernardomg.com>
2025-01-24 23:03:51 +0100

    Updated interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,15 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { jwtAuthenticationInterceptor } from './core/authentication/interceptors/jwt-authentication.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([jwtAuthenticationInterceptor, jwtAuthenticationInterceptor])
+    )
   ]
 };
--- src/app/core/authentication/interceptors/unauthorized.interceptor.ts
+++ src/app/core/authentication/interceptors/unauthorized.interceptor.ts
@@ -1,40 +1,29 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from 'environments/environment';
-import { Observable, catchError, throwError } from 'rxjs';
-import { AuthContainer } from '../services/auth.service';
+import { catchError, throwError } from 'rxjs';
 
 /**
  * Unauthorized error interceptor. Logs out the user in session on an authorization error.
  */
-@Injectable()
-export class UnauthorizedErrorInterceptor implements HttpInterceptor {
+export const jwtAuthenticationInterceptor: HttpInterceptorFn = (req, next) => {
+  return next(req).pipe(catchError(error => {
 
-  constructor(
-    private authContainer: AuthContainer
-  ) { }
+    const isApiUrl = req.url.startsWith(environment.apiUrl);
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request).pipe(catchError(error => {
+    if (isApiUrl) {
+      // It is a request to our API
 
-      const isApiUrl = request.url.startsWith(environment.apiUrl);
-
-      if (isApiUrl) {
-        // It is a request to our API
-
-        if (error.status === 401) {
-          // Unauthenticated
-          // Logs out
-          // this.authContainer.logout();
-          // location.reload();
-        }
-
-        return throwError(() => error);
-      } else {
-        // External API
-        return throwError(() => error);
+      if (error.status === 401) {
+        // Unauthenticated
+        // Logs out
+        // this.authContainer.logout();
+        // location.reload();
       }
-    }))
-  }
 
-}
+      return throwError(() => error);
+    } else {
+      // External API
+      return throwError(() => error);
+    }
+  }))
+};
