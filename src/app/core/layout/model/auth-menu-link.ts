38d1d3f2080d186fc7bca1db5d1daaadc6b0db3c
Bernardo-MG <programming@bernardomg.com>
2025-02-16 19:26:47 +0100

    Fixed typo

--- src/app/core/layout/model/auth-menu-link.ts
+++ src/app/core/layout/model/auth-menu-link.ts
@@ -3,7 +3,6 @@ import { MenuLink } from "@bernardo-mg/layout";
 export class AuthMenuLink extends MenuLink {
   resource = '';
 
-
   constructor(title: string, path: string, resource: string, icon?: any) {
     super(title, path, icon)
 
