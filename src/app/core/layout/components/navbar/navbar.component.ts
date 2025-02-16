84ce9945ce3db446c45cabbc9e4dcf707a5a6819
Bernardo-MG <programming@bernardomg.com>
2025-02-16 20:21:36 +0100

    Preparing new navbar

--- src/app/core/layout/components/navbar/navbar.component.ts
+++ src/app/core/layout/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { IconSettingsComponent, IconShieldComponent, IconLoginComponent } from '@bernardo-mg/icons';
+import { IconLoginComponent, IconSettingsComponent, IconShieldComponent } from '@bernardo-mg/icons';
+import { Menu } from '@bernardo-mg/layout';
+import { AssociationAdminLayoutService } from '../../services/association-admin-layout.service';
 import { AccountDropdownComponent } from '../account-dropdown/account-dropdown.component';
+import { LayoutService } from '../../services/layout.service';
 
 @Component({
-    selector: 'layout-navbar',
-    imports: [CommonModule, RouterModule, IconLoginComponent, IconShieldComponent, IconSettingsComponent, AccountDropdownComponent],
-    templateUrl: './navbar.component.html'
+  selector: 'layout-navbar',
+  imports: [CommonModule, RouterModule, IconLoginComponent, IconShieldComponent, IconSettingsComponent, AccountDropdownComponent],
+  templateUrl: './navbar.component.html'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
   @Input() public title = '';
 
@@ -21,10 +24,27 @@ export class NavbarComponent {
 
   @Input() public showAssociation = false;
 
-  @Input() public showAdmin = false;
+  public showAdmin = false;
+
+  public adminMenus: Menu[] = [];
+
+  constructor(
+    private layoutService: LayoutService,
+    private associationAdminLayoutService: AssociationAdminLayoutService
+  ) { }
 
   public get loggedOut() {
     return !this.loggedIn;
+  }
+
+  ngOnInit(): void {
+    // Load menus
+    this.showAdmin = this.layoutService.showAssociationAdminLink();
+    if(this.showAdmin) {
+      this.adminMenus = this.associationAdminLayoutService.getMenus();
+    } else {
+      this.adminMenus = [];
+    }
   }
 
 }
