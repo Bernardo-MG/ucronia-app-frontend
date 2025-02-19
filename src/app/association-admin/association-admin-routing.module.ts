82c96f559deeb74f17f22dab45737e3665018df0
Bernardo-MG <programming@bernardomg.com>
2025-02-19 23:36:43 +0100

    Changing layouts

--- src/app/association-admin/association-admin-routing.module.ts
+++ src/app/association-admin/association-admin-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ResourceGuard } from '@app/core/authentication/guards/resource.guard';
 import { AssociationAdminLayoutContainer } from '@app/core/layout/containers/association-admin-layout/association-admin-layout.container';
+import { SimpleLayoutContainer } from '@app/core/layout/containers/simple-layout/simple-layout.container';
 
 const fundsModule = () => import('@app/association-admin/funds/funds.module').then(m => m.FundsModule);
 const feesModule = () => import('@app/association-admin/fees/fees.module').then(m => m.FeesModule);
@@ -11,25 +12,28 @@ const libraryAdminModule = () => import('@app/association-admin/library-admin/li
 const routes: Routes = [
   {
     path: '',
-    component: AssociationAdminLayoutContainer,
     children: [
       {
         path: 'people',
+        component: SimpleLayoutContainer,
         canActivate: [ResourceGuard("person", "view")],
         loadChildren: peopleModule
       },
       {
         path: 'library',
+        component: AssociationAdminLayoutContainer,
         canActivate: [ResourceGuard("library_admin", "view")],
         loadChildren: libraryAdminModule
       },
       {
         path: 'fees',
+        component: AssociationAdminLayoutContainer,
         canActivate: [ResourceGuard("fee", "view")],
         loadChildren: feesModule
       },
       {
         path: 'funds',
+        component: AssociationAdminLayoutContainer,
         canActivate: [ResourceGuard("funds", "view")],
         loadChildren: fundsModule
       }
