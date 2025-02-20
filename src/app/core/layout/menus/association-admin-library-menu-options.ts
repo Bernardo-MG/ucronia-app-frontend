589b5b015d9096e5ded4277451ac8ae7a83b804f
Bernardo-MG <programming@bernardomg.com>
2025-02-20 23:29:21 +0100

    Added links

--- src/app/core/layout/menus/association-admin-library-menu-options.ts
+++ src/app/core/layout/menus/association-admin-library-menu-options.ts
@@ -7,7 +7,12 @@ export const ASSOCIATION_LIBRARY_ADMIN_MENU_OPTIONS: MenuOptions = {
   association: {
     title: 'Administración',
     links: [
-      new AuthMenuLink('Biblioteca', '/association/admin/library', 'library', IconBookComponent)
+      new AuthMenuLink('Biblioteca', '/association/admin/library', 'library', IconBookComponent),
+      new AuthMenuLink('Libros', '/association/admin/library/books', 'library_book', IconBookComponent),
+      new AuthMenuLink('Autores', '/association/admin/library/authors', 'library_author', IconBookComponent),
+      new AuthMenuLink('Editores', '/association/admin/library/publishers', 'library_publisher', IconBookComponent),
+      new AuthMenuLink('Tipos', '/association/admin/library/types', 'library_book_type', IconBookComponent),
+      new AuthMenuLink('Sistemas', '/association/admin/library/systems', 'library_game_system', IconBookComponent)
     ]
   }
 };
