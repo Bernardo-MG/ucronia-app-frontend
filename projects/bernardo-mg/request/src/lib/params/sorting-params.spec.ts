0c8565b54262b35f4df88e9b230a2557a9688398
Bernardo-MG <programming@bernardomg.com>
2025-02-22 00:53:07 +0100

    Cleaned up tests

--- projects/bernardo-mg/request/src/lib/params/sorting-params.spec.ts
+++ projects/bernardo-mg/request/src/lib/params/sorting-params.spec.ts
@@ -4,18 +4,76 @@ import { SortingParams } from "./sorting-params";
 
 describe("SortingParams", () => {
 
-  it('should return default sort when no custom sort is set', () => {
+  /** PROPERTY LISTS */
+
+  it('should return sorting properties when they are the only ones set', () => {
     const sortingParams = new SortingParams(
       [
         new SortProperty('name')
-      ]);
+      ]
+    );
 
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('name')
-    ]);
+    expect(sortingParams.getFinalProperties()).toEqual(
+      [
+        new SortProperty('name')
+      ]
+    );
   });
 
-  it('should override default sort', () => {
+  it('should use default sort when the property is missing from the properties list', () => {
+    const sortingParams = new SortingParams(
+      [
+        new SortProperty('name')
+      ],
+      [
+        new SortProperty('age')
+      ]
+    );
+
+    expect(sortingParams.getFinalProperties()).toEqual(
+      [
+        new SortProperty('name', SortDirection.Ascending),
+        new SortProperty('age', SortDirection.Ascending)
+      ]
+    );
+  });
+
+  it('should return an empty list when no property is received', () => {
+    const sortingParams = new SortingParams([]);
+
+    expect(sortingParams.getFinalProperties()).toEqual([]);
+  });
+
+  /** ERROR CORRECTION */
+
+  it('shouldn\'t return duplicated properties', () => {
+    const sortingParams = new SortingParams(
+      [
+        new SortProperty('name'),
+        new SortProperty('name')
+      ]
+    );
+
+    expect(sortingParams.getFinalProperties()).toEqual(
+      [
+        new SortProperty('name')
+      ]
+    );
+  });
+
+  it('should remove unsorted properties', () => {
+    const sortingParams = new SortingParams(
+      [
+        new SortProperty('name', SortDirection.Unsorted)
+      ]
+    );
+
+    expect(sortingParams.getFinalProperties()).toEqual([]);
+  });
+
+  /** OVERRIDE SORTINGS */
+
+  it('should override default sort when the property is received', () => {
     const sortingParams = new SortingParams(
       [
         new SortProperty('name'),
@@ -23,27 +81,15 @@ describe("SortingParams", () => {
       ],
       [
         new SortProperty('age', SortDirection.Ascending)
-      ]);
+      ]
+    );
 
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('name', SortDirection.Ascending),
-      new SortProperty('age', SortDirection.Descending)
-    ]);
-  });
-
-  it('should use default sort when missing', () => {
-    const sortingParams = new SortingParams(
+    expect(sortingParams.getFinalProperties()).toEqual(
       [
-        new SortProperty('name')
-      ],
-      [
-        new SortProperty('age', SortDirection.Ascending)
-      ]);
-
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('name', SortDirection.Ascending),
-      new SortProperty('age', SortDirection.Ascending)
-    ]);
+        new SortProperty('name', SortDirection.Ascending),
+        new SortProperty('age', SortDirection.Descending)
+      ]
+    );
   });
 
   it('should use default sort when unsorted', () => {
@@ -57,47 +103,30 @@ describe("SortingParams", () => {
       ]
     );
 
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('name', SortDirection.Ascending),
-      new SortProperty('age', SortDirection.Ascending)
-    ]);
+    expect(sortingParams.getFinalProperties()).toEqual(
+      [
+        new SortProperty('name', SortDirection.Ascending),
+        new SortProperty('age', SortDirection.Ascending)
+      ]
+    );
   });
 
-  it('should not sort properties', () => {
-    const sortingParams = new SortingParams([
-      new SortProperty('zname', SortDirection.Ascending),
-      new SortProperty('aname', SortDirection.Descending)
-    ]);
+  /** ORDERING */
 
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('zname', SortDirection.Ascending),
-      new SortProperty('aname', SortDirection.Descending)
-    ]);
-  });
+  it('should not change properties order', () => {
+    const sortingParams = new SortingParams(
+      [
+        new SortProperty('zname', SortDirection.Ascending),
+        new SortProperty('aname', SortDirection.Descending)
+      ]
+    );
 
-  it('should not short when no property is received', () => {
-    const sortingParams = new SortingParams([]);
-
-    expect(sortingParams.getFinalProperties()).toEqual([]);
-  });
-
-  it('should remove duplicated properties', () => {
-    const sortingParams = new SortingParams([
-      new SortProperty('name'),
-      new SortProperty('name')
-    ]);
-
-    expect(sortingParams.getFinalProperties()).toEqual([
-      new SortProperty('name')
-    ]);
-  });
-
-  it('should remove unsorted properties', () => {
-    const sortingParams = new SortingParams([
-      new SortProperty('name', SortDirection.Unsorted)
-    ]);
-
-    expect(sortingParams.getFinalProperties()).toEqual([]);
+    expect(sortingParams.getFinalProperties()).toEqual(
+      [
+        new SortProperty('zname', SortDirection.Ascending),
+        new SortProperty('aname', SortDirection.Descending)
+      ]
+    );
   });
 
 });
