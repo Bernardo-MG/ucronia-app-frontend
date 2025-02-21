afd3575e4f7473309ef1f7d02c72dd97762217b3
Bernardo-MG <programming@bernardomg.com>
2025-02-22 00:15:53 +0100

    Cleaned up tests

--- projects/bernardo-mg/request/src/lib/clients/angular-client.spec.ts
+++ projects/bernardo-mg/request/src/lib/clients/angular-client.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ParamLoader } from '../../public-api';
 import { AngularErrorRequestInterceptor } from '../interceptors/angular-error-request-interceptor';
 import { AngularClient } from './angular-client';
 
@@ -11,17 +12,18 @@ describe('AngularClient', () => {
   let errorInterceptor: AngularErrorRequestInterceptor;
 
   const rootUrl = 'http://test.com/api';
+  const testData = { name: 'test' };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-    imports: [],
-    providers: [
+      imports: [],
+      providers: [
         AngularErrorRequestInterceptor,
         { provide: 'ROOT_URL', useValue: rootUrl },
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting()
-    ]
-});
+      ]
+    });
 
     httpMock = TestBed.inject(HttpTestingController);
     httpClient = TestBed.inject(HttpClient);
@@ -38,14 +40,52 @@ describe('AngularClient', () => {
     expect(client).toBeTruthy();
   });
 
+  /** SEND REQUESTS */
+
   it('should send a POST request', () => {
+    client.create(testData).subscribe(data => { });
+
+    const req = httpMock.expectOne(`${rootUrl}`);
+    expect(req.request.method).toBe('POST');
+  });
+
+  it('should send a GET request', () => {
+    client.read().subscribe(data => { });
+
+    const req = httpMock.expectOne(`${rootUrl}`);
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should send a PUT request', () => {
+    client.update(testData).subscribe(data => { });
+
+    const req = httpMock.expectOne(`${rootUrl}`);
+    expect(req.request.method).toBe('PUT');
+  });
+
+  it('should send a DELETE request', () => {
+    client.delete().subscribe(data => { });
+
+    const req = httpMock.expectOne(`${rootUrl}`);
+    expect(req.request.method).toBe('DELETE');
+  });
+
+  it('should send a PATCH request', () => {
+    client.patch(testData).subscribe(data => { });
+
+    const req = httpMock.expectOne(`${rootUrl}`);
+    expect(req.request.method).toBe('PATCH');
+  });
+
+  /** RECEIVE RESPONSES */
+
+  it('should receive response from a POST request', () => {
     const testData = { name: 'test' };
     client.create(testData).subscribe(data => {
       expect(data).toEqual(testData);
     });
 
     const req = httpMock.expectOne(`${rootUrl}`);
-    expect(req.request.method).toBe('POST');
     req.flush(testData);
   });
 
@@ -56,7 +96,6 @@ describe('AngularClient', () => {
     });
 
     const req = httpMock.expectOne(`${rootUrl}`);
-    expect(req.request.method).toBe('GET');
     req.flush(testData);
   });
 
@@ -67,7 +106,6 @@ describe('AngularClient', () => {
     });
 
     const req = httpMock.expectOne(`${rootUrl}`);
-    expect(req.request.method).toBe('PUT');
     req.flush(testData);
   });
 
@@ -77,7 +115,6 @@ describe('AngularClient', () => {
     });
 
     const req = httpMock.expectOne(`${rootUrl}`);
-    expect(req.request.method).toBe('DELETE');
     req.flush(null);
   });
 
@@ -88,36 +125,42 @@ describe('AngularClient', () => {
     });
 
     const req = httpMock.expectOne(`${rootUrl}`);
-    expect(req.request.method).toBe('PATCH');
     req.flush(testData);
   });
 
+  /** ROUTES */
+
   it('should append route correctly', () => {
     client.appendRoute('/test').appendRoute('/route');
+
     expect(client['route']).toBe('/test/route');
   });
 
   it('should construct the final URL correctly', () => {
     client.appendRoute('/test').appendRoute('/route');
+
     const finalUrl = client['getFinalUrl']();
     expect(finalUrl).toBe('http://test.com/api/test/route');
   });
 
+  /** PARAMETERS */
+
   it('should set parameters correctly', () => {
     client.parameter('key', 'value');
+
     const params = new HttpParams().append('key', 'value');
     expect(client['options'].params).toEqual(params);
   });
 
-  it('should load parameters correctly', () => {
-    const mockParams = {
+  it('should apply a parameter loader correctly', () => {
+    const paramLoader = {
       load: (callback: (key: string, value: any) => void) => {
         callback('key1', 'value1');
         callback('key2', 'value2');
       },
     };
 
-    client.loadParameters(mockParams as any);
+    client.loadParameters(paramLoader);
 
     const params = new HttpParams()
       .append('key1', 'value1')
