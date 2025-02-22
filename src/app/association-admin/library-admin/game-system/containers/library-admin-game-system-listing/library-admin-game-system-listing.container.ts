16aae760ca7397097b01e4cd7517150e25276e32
Bernardo-MG <programming@bernardomg.com>
2025-02-22 23:18:13 +0100

    Changed listing

--- src/app/association-admin/library-admin/game-system/containers/library-admin-game-system-listing/library-admin-game-system-listing.container.ts
+++ src/app/association-admin/library-admin/game-system/containers/library-admin-game-system-listing/library-admin-game-system-listing.container.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthContainer } from '@app/core/authentication/services/auth.service';
 import { GameSystem } from '@app/models/library/game-system';
+import { PaginationInfoComponent } from '@app/shared/pagination/components/pagination-info/pagination-info.component';
 import { SortingButtonComponent } from '@app/shared/sorting/components/sorting-button/sorting-button.component';
-import { BlockUiDirective } from '@bernardo-mg/layout';
+import { IconAddComponent } from '@bernardo-mg/icons';
+import { ArticleComponent, BlockUiDirective, CardBodyComponent, CardComponent, CardFooterComponent, CardHeaderComponent } from '@bernardo-mg/layout';
 import { PaginatedResponse, Sorting, SortingProperty } from '@bernardo-mg/request';
 import { GameSystemAdminService } from '../../services/game-system-admin.service';
 
 @Component({
-    selector: 'assoc-library-admin-game-system-listing',
-    imports: [CommonModule, RouterModule, SortingButtonComponent, BlockUiDirective],
-    templateUrl: './library-admin-game-system-listing.container.html'
+  selector: 'assoc-library-admin-game-system-listing',
+  imports: [CommonModule, RouterModule, ArticleComponent, SortingButtonComponent, PaginationInfoComponent, IconAddComponent, CardComponent, CardBodyComponent, CardHeaderComponent, CardFooterComponent, BlockUiDirective],
+  templateUrl: './library-admin-game-system-listing.container.html'
 })
 export class LibraryAdminGameSystemListingContainer implements OnInit, OnChanges {
 
@@ -20,22 +23,27 @@ export class LibraryAdminGameSystemListingContainer implements OnInit, OnChanges
 
   @Output() public changePage = new EventEmitter<PaginatedResponse<any[]>>();
 
-  public data: GameSystem[] = [];
+  public data = new PaginatedResponse<GameSystem[]>([]);
 
   /**
    * Loading flag.
    */
   public reading = false;
 
+  public createPermission = false;
+
   private sort = new Sorting([]);
 
   constructor(
+    private authContainer: AuthContainer,
     private service: GameSystemAdminService
   ) { }
 
   public ngOnInit(): void {
     // Load books
     this.load(0)
+    // Check permissions
+    this.createPermission = this.authContainer.hasPermission("library_game_system", "create");
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
@@ -56,7 +64,7 @@ export class LibraryAdminGameSystemListingContainer implements OnInit, OnChanges
 
     this.service.getAll(page, this.sort).subscribe({
       next: response => {
-        this.data = response.content;
+        this.data = response;
         this.changePage.emit(response);
 
         // Reactivate view
