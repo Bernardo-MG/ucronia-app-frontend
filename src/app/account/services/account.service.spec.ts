12a9bbe93880d86d7996768adf11b93e13eeeec3
Bernardo-MG <programming@bernardomg.com>
2025-03-11 00:28:23 +0100

    Removed unused imports

--- src/app/account/services/account.service.spec.ts
+++ src/app/account/services/account.service.spec.ts
@@ -1,26 +1,10 @@
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { AuthContainer } from '@bernardo-mg/authentication';
-import { EMPTY } from 'rxjs';
 import { AccountService } from './account.service';
 
 describe('AccountService', () => {
   let service: AccountService;
-  let authContainer: AuthContainer;
 
   beforeEach(() => {
-    authContainer = jasmine.createSpyObj('authContainer', ['getStatus']);
-    (authContainer as any).getStatus.and.returnValue(EMPTY);
-
-    TestBed.configureTestingModule({
-      providers: [
-        AccountService,
-        { provide: AuthContainer, useValue: authContainer },
-        provideHttpClient(withInterceptorsFromDi()),
-        provideHttpClientTesting()
-      ]
-    });
     service = TestBed.inject(AccountService);
   });
 
