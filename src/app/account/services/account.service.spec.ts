ba6b125696831db6b672994a1efa889045c87f6a
Bernardo-MG <programming@bernardomg.com>
2025-03-11 17:32:02 +0100

    Fixed test

--- src/app/account/services/account.service.spec.ts
+++ src/app/account/services/account.service.spec.ts
@@ -1,10 +1,19 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { AccountService } from './account.service';
 
 describe('AccountService', () => {
   let service: AccountService;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting()
+      ]
+    })
+      .compileComponents();
     service = TestBed.inject(AccountService);
   });
 
