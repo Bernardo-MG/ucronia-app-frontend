fdc9f90057cd948c576839490a47957ac7c0d84a
Bernardo-MG <programming@bernardomg.com>
2025-04-19 21:48:49 +0200

    Removed title from article

--- projects/bernardo-mg/layout/src/lib/components/article/article.component.ts
+++ projects/bernardo-mg/layout/src/lib/components/article/article.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * Article component. Contains a heading and a body. Additionally it handles a waiting cue, to show it is loading the body content.
@@ -10,10 +10,5 @@ import { Component, Input } from '@angular/core';
   templateUrl: './article.component.html'
 })
 export class ArticleComponent {
-
-  /**
-   * Article title, to be shown on the heading.
-   */
-  @Input() public title = '';
 
 }
