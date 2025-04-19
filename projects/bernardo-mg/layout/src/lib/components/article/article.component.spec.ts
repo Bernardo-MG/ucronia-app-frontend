21c61e93ea46b035d0df128465c2092d904d01c1
Bernardo-MG <programming@bernardomg.com>
2025-04-19 21:58:42 +0200

    Corrected article tests

--- projects/bernardo-mg/layout/src/lib/components/article/article.component.spec.ts
+++ projects/bernardo-mg/layout/src/lib/components/article/article.component.spec.ts
@@ -22,30 +22,4 @@ describe('ArticleComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should render title in header', () => {
-    const title = 'Test Title';
-
-    component.title = title;
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-
-    expect(compiled.querySelector('h1').textContent).toContain(title);
-  });
-
-  it('should not render header if title is not provided', () => {
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('header')).toBeNull();
-  });
-
-  it('should render content inside the component', () => {
-    const compiled = fixture.nativeElement;
-    const content = 'Test Content';
-
-    component.title = 'Title';
-    fixture.detectChanges();
-    component.title = content;
-    fixture.detectChanges();
-
-    expect(compiled.textContent).toContain(content);
-  });
 });
