f0b25cfb34f49345ec707ca7702f619b192aa89f
Bernardo-MG <programming@bernardomg.com>
2025-07-24 00:50:04 +0200

    Password reset request is now async

--- src/app/access/password-reset/containers/password-reset-request/password-reset-request.container.ts
+++ src/app/access/password-reset/containers/password-reset-request/password-reset-request.container.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { CardBodyComponent, CardComponent } from '@bernardo-mg/ui';
-import { FailureResponse, FailureStore } from '@bernardo-mg/request';
-import { throwError } from 'rxjs';
 import { PasswordResetRequestFormComponent } from '../../components/password-reset-request-form/password-reset-request-form.component';
 import { PasswordResetRequest } from '../../models/password-reset-request';
 import { PasswordResetService } from '../../services/password-reset.service';
@@ -25,40 +23,13 @@ export class PasswordResetRequestContainer {
   public finished = false;
 
   /**
-   * Password reset flag. If set to true the component is waiting for the password change request to finish.
-   */
-  public reseting = false;
-
-  /**
-   * Failures when reseting the password.
-   */
-  public failures = new FailureStore();
-
-  /**
    * Handles the password reset request.
    * 
    * @param resetPassword password reset data
    */
   public onPasswordResetRequest(resetPassword: PasswordResetRequest) {
-    this.reseting = true;
-    this.failures.clear();
-    this.service.requestResetPassword(resetPassword)
-      .subscribe({
-        next: response => {
-          this.finished = true;
-          this.reseting = false;
-        },
-        error: error => {
-          if (error instanceof FailureResponse) {
-            this.failures = error.failures;
-          } else {
-            this.failures.clear();
-          }
-          this.reseting = false;
-
-          return throwError(() => error);
-        }
-      });
+    this.service.requestResetPassword(resetPassword).subscribe();
+    this.finished = true;
   }
 
 }
