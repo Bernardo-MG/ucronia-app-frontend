a28de8f1041d5b6920e1703d4fc71ef0b97c3010
Bernardo-MG <programming@bernardomg.com>
2025-07-31 22:38:40 +0200

    Added tests

--- src/app/core/layout/containers/navbar/navbar.container.spec.ts
+++ src/app/core/layout/containers/navbar/navbar.container.spec.ts
@@ -1,29 +1,108 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { NavbarContainer } from './navbar.container';
+import { AuthContainer } from '@bernardo-mg/authentication';
+import { LayoutService } from '../../services/layout.service';
+import { MenuLink } from '@bernardo-mg/ui';
 
 describe('NavbarContainer', () => {
   let component: NavbarContainer;
   let fixture: ComponentFixture<NavbarContainer>;
 
+  // Mocks
+  const mockAuthContainer = { logged: false };
+  let mockLayoutService: jasmine.SpyObj<LayoutService>;
+
   beforeEach(async () => {
+    mockLayoutService = jasmine.createSpyObj('LayoutService', [
+      'getTitle',
+      'showSettingsLink',
+      'showSecurityLink',
+      'showAssociationLink',
+      'getLinks'
+    ]);
+
     await TestBed.configureTestingModule({
-      imports: [
-        NavbarContainer
-      ],
+      imports: [NavbarContainer],
       providers: [
-        provideRouter([])
+        provideRouter([]),
+        { provide: AuthContainer, useValue: mockAuthContainer },
+        { provide: LayoutService, useValue: mockLayoutService }
       ]
     }).compileComponents();
   });
 
-  beforeEach(() => {
+  function createComponent() {
     fixture = TestBed.createComponent(NavbarContainer);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }
 
   it('should create', () => {
+    createComponent();
     expect(component).toBeTruthy();
   });
+
+  it('should use title from layout service', () => {
+    spyOn(mockLayoutService, 'getTitle').and.returnValue('My App');
+
+    createComponent();
+
+    expect(component.title).toBe('My App');
+  });
+
+  it('should not load menu when logged out', () => {
+    mockAuthContainer.logged = false;
+    spyOn(mockLayoutService, 'showAssociationLink').and.returnValue(true);
+    spyOn(mockLayoutService, 'getLinks').and.returnValue([
+      new MenuLink('Admin', '/admin')
+    ]);
+
+    createComponent();
+
+    expect(component.loggedIn).toBeFalse();
+    expect(component.menuItems.length).toBe(0);
+  });
+
+  describe('flags', () => {
+
+    it('should show all flags as true and populate menuItems', () => {
+      mockAuthContainer.logged = true;
+      spyOn(mockLayoutService, 'showSettingsLink').and.returnValue(true);
+      spyOn(mockLayoutService, 'showSecurityLink').and.returnValue(true);
+      spyOn(mockLayoutService, 'showAssociationLink').and.returnValue(true);
+      spyOn(mockLayoutService, 'getLinks').and.returnValue([
+        new MenuLink('Admin', '/admin')
+      ]);
+
+      createComponent();
+
+      expect(component.loggedIn).toBeTrue();
+      expect(component.loggedOut).toBeFalse();
+      expect(component.showSettings).toBeTrue();
+      expect(component.showSecurity).toBeTrue();
+      expect(component.showAssociation).toBeTrue();
+      expect(component.showAdmin).toBeTrue();
+      expect(component.menuItems.length).toBe(2); // Asociación + Administración
+    });
+
+    it('should show all flags as false and empty menu', () => {
+      mockAuthContainer.logged = true;
+      spyOn(mockLayoutService, 'showSettingsLink').and.returnValue(false);
+      spyOn(mockLayoutService, 'showSecurityLink').and.returnValue(false);
+      spyOn(mockLayoutService, 'showAssociationLink').and.returnValue(false);
+      spyOn(mockLayoutService, 'getLinks').and.returnValue([]);
+
+      createComponent();
+
+      expect(component.loggedIn).toBeTrue();
+      expect(component.showSettings).toBeFalse();
+      expect(component.showSecurity).toBeFalse();
+      expect(component.showAssociation).toBeFalse();
+      expect(component.showAdmin).toBeFalse();
+      expect(component.menuItems.length).toBe(0);
+    });
+
+  });
+
 });
