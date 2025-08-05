a08955e84dee1a078ce602fb452d353e67cb6051
Bernardo-MG <programming@bernardomg.com>
2025-08-06 00:32:54 +0200

    Reduced code

--- src/app/security/users/containers/access-user-list/access-user-list.container.ts
+++ src/app/security/users/containers/access-user-list/access-user-list.container.ts
@@ -1,11 +1,84 @@
-import { Component } from '@angular/core';
-import { AccessUserSelectionListWidgetContainer } from '../access-user-selection-list-widget/access-user-selection-list-widget.container';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { AuthContainer, User } from '@bernardo-mg/authentication';
+import { IconAddComponent } from '@bernardo-mg/icons';
+import { PaginatedResponse, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { CardModule } from 'primeng/card';
+import { TableModule, TablePageEvent } from 'primeng/table';
+import { AccessUserService } from '../../services/access-user.service';
 
 @Component({
     selector: 'access-user-list',
-    imports: [AccessUserSelectionListWidgetContainer],
+    imports: [CardModule, RouterModule, TableModule, IconAddComponent],
     templateUrl: './access-user-list.container.html'
 })
 export class AccessListContainer {
+
+  private readonly router = inject(Router);
+
+  private readonly service = inject(AccessUserService);
+
+  public readonly createPermission;
+
+  public get first() {
+    return (this.data.page - 1) * this.data.size;
+  }
+
+  public data = new PaginatedResponse<User>();
+
+  public selectedData = new User();
+
+  /**
+   * Loading flag.
+   */
+  public loading = false;
+
+  private sort = new Sorting();
+
+  constructor() {
+    const authContainer = inject(AuthContainer);
+
+    // Check permissions
+    this.createPermission = authContainer.hasPermission("user", "create");
+
+    this.load(0);
+  }
+
+  public onChangeDirection(sorting: { field: string, order: number }) {
+    let direction;
+    if (sorting.order == 1) {
+      direction = SortingDirection.Ascending;
+    } else {
+      direction = SortingDirection.Descending;
+    }
+    this.sort.addField(new SortingProperty(sorting.field, direction));
+
+    this.load(this.data.page);
+  }
+
+  public onPageChange(event: TablePageEvent) {
+    const page = (event.first / this.data.size) + 1;
+    this.load(page);
+  }
+
+  public onSelectRow() {
+    this.router.navigate([`/security/users/${this.selectedData.username}}`]);
+  }
+
+  private load(page: number) {
+    this.loading = true;
+    this.service.getAll(page, this.sort).subscribe({
+      next: response => {
+        this.data = response;
+
+        // Reactivate view
+        this.loading = false;
+      },
+      error: error => {
+        // Reactivate view
+        this.loading = false;
+      }
+    });
+  }
 
 }
