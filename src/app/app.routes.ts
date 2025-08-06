d69c7c29e3b5b7ad2d31a7f730a7244cfda1ce06
Bernardo-MG <programming@bernardomg.com>
2025-08-06 23:56:15 +0200

    Cleaned up routes config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,6 @@
 import { Routes } from '@angular/router';
-import { LoggedInGuard, LoggedOutGuard, ResourceGuard } from '@bernardo-mg/authentication';
+import { LoggedInGuard, LoggedOutGuard } from '@bernardo-mg/authentication';
 
-
-const frontpageModule = () => import('@app/frontpage/frontpage.module').then(m => m.FrontpageModule);
-const associationModule = () => import('@app/association/association.module').then(m => m.AssociationModule);
-const associationAdminModule = () => import('@app/association-admin/association-admin.module').then(m => m.AssociationAdminModule);
-const securityModule = () => import('@app/security/security.module').then(m => m.SecurityModule);
-const accountModule = () => import('@app/account/account.module').then(m => m.AccountModule);
-const loginModule = () => import('@app/access/login/login.module').then(m => m.LoginModule);
-const activateUserModule = () => import('@app/access/user-activation/user-activation.module').then(m => m.UserActivationModule);
-const resetPasswordModule = () => import('@app/access/password-reset/password-reset.module').then(m => m.PasswordResetModule);
-const settingsModule = () => import('@app/settings/settings.module').then(m => m.SettingsModule);
 
 export const routes: Routes = [
   // Main app
@@ -22,59 +12,59 @@ export const routes: Routes = [
         // Logged out
         path: '',
         loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
-        loadChildren: frontpageModule
+        loadChildren: () => import('@app/frontpage/frontpage.module').then(m => m.FrontpageModule)
       },
       {
         // Log in form
         path: 'login',
-        loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
         canActivate: [LoggedOutGuard],
-        loadChildren: loginModule
+        loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
+        loadChildren: () => import('@app/access/login/login.module').then(m => m.LoginModule)
       },
       {
         // Password reset form
         path: 'password/reset',
-        loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
         canActivate: [LoggedOutGuard],
-        loadChildren: resetPasswordModule
+        loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
+        loadChildren: () => import('@app/access/password-reset/password-reset.module').then(m => m.PasswordResetModule)
       },
       {
         // Activate user form
         path: 'users/activate',
+        canActivate: [LoggedOutGuard],
         loadComponent: () => import('./core/layout/components/simple-layout/simple-layout.component').then(m => m.SimpleLayoutComponent),
-        loadChildren: activateUserModule,
-        canActivate: [LoggedOutGuard]
+        loadChildren: () => import('@app/access/user-activation/user-activation.module').then(m => m.UserActivationModule)
       },
       // Private routes
       // Security
       {
         // Account
         path: 'account',
-        loadChildren: accountModule,
-        canActivate: [LoggedInGuard]
+        canActivate: [LoggedInGuard],
+        loadChildren: () => import('@app/account/account.module').then(m => m.AccountModule)
       },
       {
         // Association admin
         path: 'association/admin',
-        loadChildren: associationAdminModule,
-        canActivate: [LoggedInGuard]
+        canActivate: [LoggedInGuard],
+        loadChildren: () => import('@app/association-admin/association-admin.module').then(m => m.AssociationAdminModule)
       },
       {
         // Association
         path: 'association',
-        loadChildren: associationModule,
-        canActivate: [LoggedInGuard]
+        canActivate: [LoggedInGuard],
+        loadChildren: () => import('@app/association/association.module').then(m => m.AssociationModule)
       },
       {
         // Security
         path: 'security',
-        loadChildren: securityModule,
-        canActivate: [LoggedInGuard]
+        canActivate: [LoggedInGuard],
+        loadChildren: () => import('@app/security/security.module').then(m => m.SecurityModule)
       },
       {
         path: 'settings',
-        loadChildren: settingsModule,
-        canActivate: [LoggedInGuard, ResourceGuard("association_settings", "view")]
+        canActivate: [LoggedInGuard],
+        loadChildren: () => import('@app/settings/settings.module').then(m => m.SettingsModule)
       }
     ]
   }
