022c50c4f47b968e2258ec6efc25f9410d30996f
Bernardo-MG <programming@bernardomg.com>
2025-09-01 00:08:25 +0200

    Abstract entity list

--- src/app/association-admin/library-admin/author/containers/library-admin-author-list/library-admin-author-list.component.ts
+++ src/app/association-admin/library-admin/author/containers/library-admin-author-list/library-admin-author-list.component.ts
@@ -1,16 +1,17 @@
 
 import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { EntityList } from '@app/core/layout/components/entity-list/entity-list';
 import { Author } from '@app/domain/library/author';
 import { AuthContainer } from '@bernardo-mg/authentication';
-import { FailureResponse, FailureStore, PaginatedResponse, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { PaginatedResponse, Sorting } from '@bernardo-mg/request';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { DrawerModule } from 'primeng/drawer';
 import { MenuModule } from 'primeng/menu';
 import { PanelModule } from 'primeng/panel';
-import { TableModule, TablePageEvent } from 'primeng/table';
-import { throwError } from 'rxjs';
+import { TableModule } from 'primeng/table';
+import { Observable } from 'rxjs';
 import { LibraryAdminAuthorFormComponent } from '../../components/library-admin-author-form/library-admin-author-form.component';
 import { AuthorAdminService } from '../../services/author-admin.service';
 
@@ -19,163 +20,27 @@ import { AuthorAdminService } from '../../services/author-admin.service';
   imports: [CardModule, RouterModule, TableModule, PanelModule, MenuModule, ButtonModule, DrawerModule, LibraryAdminAuthorFormComponent],
   templateUrl: './library-admin-author-list.component.html'
 })
-export class LibraryAdminAuthorListContainer {
+export class LibraryAdminAuthorListContainer extends EntityList<Author> {
 
   private readonly service = inject(AuthorAdminService);
 
-  public get first() {
-    return (this.data.page - 1) * this.data.size;
-  }
-
-  public data = new PaginatedResponse<Author>();
-
-  /**
-   * Loading flag.
-   */
-  public loading = false;
-
-  private _shownForm: 'None' | 'Creation' | 'Edition' = 'None';
-
-  public get shownForm() {
-    return this._shownForm;
-  }
-
-  public set shownForm(form) {
-    this._shownForm = form;
-    if (form === 'None') {
-      this.showForm = false;
-    } else {
-      this.showForm = true;
-    }
-  }
-
-  public showForm = false;
-
-  public readonly editable;
-
-  public readonly createable;
-
-  private sort = new Sorting();
-
-  public selected: Author = new Author();
-
-  /**
-   * Failures after saving.
-   */
-  protected failures = new FailureStore();
-
   constructor() {
-    const authContainer = inject(AuthContainer);
+    super(inject(AuthContainer), "library_author");
 
     // First page
     this.load(0);
-
-    // Check permissions
-    this.createable = authContainer.hasPermission("library_author", "create");
-    this.editable = authContainer.hasPermission("library_author", "update");
-  }
-
-  public onChangeDirection(sorting: { field: string, order: number }) {
-    let direction;
-
-    if (sorting.order == 1) {
-      direction = SortingDirection.Ascending;
-    } else {
-      direction = SortingDirection.Descending;
-    }
-
-    this.sort.addField(new SortingProperty(sorting.field, direction));
-
-    this.load(this.data.page);
-  }
-
-  public onPageChange(event: TablePageEvent) {
-    const page = (event.first / this.data.size) + 1;
-    this.load(page);
-  }
-
-  public onStartEditing(author: Author): void {
-    this.selected = author;
-    this.shownForm = 'Edition';
-  }
-
-  public onStartCreating(): void {
-    this.shownForm = 'Creation';
   }
 
   public onCreate(toCreate: Author): void {
-    this.loading = true;
-    this.service.create(toCreate).subscribe({
-      next: response => {
-        this.failures.clear();
-
-        // Reactivate component
-        this.shownForm = 'None';
-        this.load(this.data.page);
-      },
-      error: error => {
-        if (error instanceof FailureResponse) {
-          this.failures = error.failures;
-        } else {
-          // No failure response
-          // Just remove the failures
-          this.failures.clear();
-        }
-
-        // Reactivate component
-        this.loading = false;
-
-        return throwError(() => error);
-      }
-    });
+    this.mutate(() => this.service.create(toCreate));
   }
 
   public onUpdate(toSave: Author): void {
-    this.loading = true;
-    this.service.update(toSave.number, toSave).subscribe({
-      next: response => {
-        this.failures.clear();
-
-        // Reactivate component
-        this.shownForm = 'None';
-        this.load(this.data.page);
-      },
-      error: error => {
-        if (error instanceof FailureResponse) {
-          this.failures = error.failures;
-        } else {
-          // No failure response
-          // Just remove the failures
-          this.failures.clear();
-        }
-
-        // Reactivate component
-        this.loading = false;
-
-        return throwError(() => error);
-      }
-    });
+    this.mutate(() => this.service.update(toSave.number, toSave));
   }
 
-  public onCancel(): void {
-    this.shownForm = 'None';
-  }
-
-  private load(page: number) {
-    this.loading = true;
-
-    this.service.getAll(page, this.sort).subscribe({
-      next: response => {
-        this.data = response;
-
-        // Reactivate view
-        this.loading = false;
-      },
-      error: error => {
-        // Reactivate view
-        this.loading = false;
-      }
-    });
+  protected override getAll(page: number, sort: Sorting): Observable<PaginatedResponse<Author>> {
+    return this.service.getAll(page, this.sort);
   }
 
 }
---
+++ src/app/core/layout/components/entity-list/entity-list.ts
@@ -0,0 +1,102 @@
+import { AuthContainer } from '@bernardo-mg/authentication';
+import { FailureResponse, FailureStore, PaginatedResponse, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { TablePageEvent } from 'primeng/table';
+import { Observable, throwError } from 'rxjs';
+
+export abstract class EntityList<E> {
+
+  public data = new PaginatedResponse<E>();
+
+  public loading = false;
+
+  private _shownForm: 'None' | 'Creation' | 'Edition' = 'None';
+
+  public get shownForm() { return this._shownForm; }
+
+  public set shownForm(form: 'None' | 'Creation' | 'Edition') {
+    this._shownForm = form;
+    this.showForm = form !== 'None';
+  }
+
+  public showForm = false;
+
+  public selected!: E;
+
+  protected sort = new Sorting();
+
+  protected failures = new FailureStore();
+
+  public readonly createable: boolean;
+
+  public readonly editable: boolean;
+
+  constructor(protected readonly auth: AuthContainer, entityKey: string) {
+    this.createable = auth.hasPermission(entityKey, "create");
+    this.editable = auth.hasPermission(entityKey, "update");
+  }
+
+  public get first() {
+    return (this.data.page - 1) * this.data.size;
+  }
+
+  public onChangeDirection(sorting: { field: string; order: number }) {
+    const direction = sorting.order === 1
+      ? SortingDirection.Ascending
+      : SortingDirection.Descending;
+
+    this.sort.addField(new SortingProperty(sorting.field, direction));
+    this.load(this.data.page);
+  }
+
+  public onPageChange(event: TablePageEvent) {
+    const page = (event.first / this.data.size) + 1;
+    this.load(page);
+  }
+
+  public onStartEditing(item: E): void {
+    this.selected = item;
+    this.shownForm = 'Edition';
+  }
+
+  public onStartCreating(): void {
+    this.shownForm = 'Creation';
+  }
+
+  public onCancel(): void {
+    this.shownForm = 'None';
+  }
+
+  protected load(page: number) {
+    this.loading = true;
+    this.getAll(page, this.sort).subscribe({
+      next: response => {
+        this.data = response;
+        this.loading = false;
+      },
+      error: () => this.loading = false
+    });
+  }
+
+  protected mutate(action: () => Observable<any>) {
+    this.loading = true;
+    action().subscribe({
+      next: () => {
+        this.failures.clear();
+        this.shownForm = 'None';
+        this.load(this.data.page);
+      },
+      error: error => {
+        if (error instanceof FailureResponse) {
+          this.failures = error.failures;
+        } else {
+          this.failures.clear();
+        }
+        this.loading = false;
+        return throwError(() => error);
+      }
+    });
+  }
+
+  protected abstract getAll(page: number, sort: Sorting): Observable<PaginatedResponse<E>>;
+
+}
