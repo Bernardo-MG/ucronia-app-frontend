1fb2d9039bbef9c789b4877dab4d1ed4cc1552e2
Bernardo-MG <programming@bernardomg.com>
2025-08-31 23:29:22 +0200

    Simplified buttons

--- src/app/association-admin/library-admin/author/components/library-admin-author-form/library-admin-author-form.component.ts
+++ src/app/association-admin/library-admin/author/components/library-admin-author-form/library-admin-author-form.component.ts
@@ -2,14 +2,16 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Author } from '@app/domain/library/author';
-import { FormComponent, SaveControlsComponent } from '@bernardo-mg/form';
+import { FormComponent } from '@bernardo-mg/form';
+import { JustifyBetweenDirective } from '@bernardo-mg/ui';
+import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'assoc-library-admin-author-form',
-  imports: [FormsModule, ReactiveFormsModule, InputTextModule, FloatLabelModule, MessageModule, SaveControlsComponent],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, FloatLabelModule, MessageModule, ButtonModule, JustifyBetweenDirective],
   templateUrl: './library-admin-author-form.component.html'
 })
 export class LibraryAdminAuthorFormComponent extends FormComponent<Author> {
