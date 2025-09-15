d2e2b8f7031adce1d0f799b603433b79c44746b5
Bernardo-MG <programming@bernardomg.com>
2025-09-16 00:22:36 +0200

    Updated form

--- src/app/association-admin/funds/core/transaction-form/transaction-form.ts
+++ src/app/association-admin/funds/core/transaction-form/transaction-form.ts
@@ -1,12 +1,16 @@
 
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { FormComponent, InputFailureFeedbackComponent, InvalidFieldDirective, SaveControlsComponent } from '@bernardo-mg/form';
 import { Transaction } from '@app/domain/transactions/transaction';
+import { FormComponent, SaveControlsComponent } from '@bernardo-mg/form';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'assoc-transaction-form',
-  imports: [FormsModule, ReactiveFormsModule, SaveControlsComponent, InputFailureFeedbackComponent, InvalidFieldDirective],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, FloatLabelModule, DatePickerModule, MessageModule, SaveControlsComponent],
   templateUrl: './transaction-form.html'
 })
 export class TransactionForm extends FormComponent<Transaction> {
