3a1274cd215728f511275204aa8cc2e6ea26cc7e
Bernardo-MG <programming@bernardomg.com>
2025-09-21 14:37:46 +0200

    Grouped routing

--- src/app/association-admin/association-admin-routing.module.ts
+++ src/app/association-admin/association-admin-routing.module.ts
@@ -14,7 +14,85 @@ const routes: Routes = [
       {
         path: 'library',
         canActivate: [ResourceGuard("library_admin", "view")],
-        loadChildren: () => import('@app/association-admin/library-admin/library-admin.module').then(m => m.LibraryAdminModule)
+        children: [
+          {
+            path: '',
+            redirectTo: 'books',
+            pathMatch: 'full'
+          },
+          {
+            path: 'authors',
+            data: { breadcrumb: 'Autores' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/author/library-admin-author-list/library-admin-author-list').then(m => m.LibraryAdminAuthorList),
+                canActivate: [ResourceGuard("library_author", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          },
+          {
+            path: 'books',
+            data: { breadcrumb: 'Libros' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/book/library-admin-book-list/library-admin-book-list').then(m => m.LibraryAdminBookList),
+                canActivate: [ResourceGuard("library_book", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          },
+          {
+            path: 'publishers',
+            data: { breadcrumb: 'Editores' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/publisher/library-admin-publisher-list/library-admin-publisher-list').then(m => m.LibraryAdminPublisherList),
+                canActivate: [ResourceGuard("library_publisher", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          },
+          {
+            path: 'types',
+            data: { breadcrumb: 'Tipos' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/book-type/library-admin-book-type-list/library-admin-book-type-list').then(m => m.LibraryAdminBookTypeList),
+                canActivate: [ResourceGuard("library_book_type", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          },
+          {
+            path: 'systems',
+            data: { breadcrumb: 'Sistemas' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/game-system/library-admin-game-system-list/library-admin-game-system-list').then(m => m.LibraryAdminGameSystemList),
+                canActivate: [ResourceGuard("library_game_system", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          },
+          {
+            path: 'lendings',
+            data: { breadcrumb: 'Préstamos' },
+            children: [
+              {
+                path: '',
+                loadComponent: () => import('./library-admin/lending/library-admin-lending-list/library-admin-lending-list').then(m => m.LibraryAdminLendingList),
+                canActivate: [ResourceGuard("library_lending", "view")],
+                data: { breadcrumb: '' }
+              }
+            ]
+          }
+        ]
       },
       {
         path: 'fees',
