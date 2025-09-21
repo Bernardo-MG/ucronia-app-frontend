640dc92f47b87a0f7ca7f7be1735509770e09110
Bernardo-MG <programming@bernardomg.com>
2025-09-21 14:05:46 +0200

    Updated form

--- src/app/association-admin/people/core/people-edition-form/people-edition-form.ts
+++ src/app/association-admin/people/core/people-edition-form/people-edition-form.ts
@@ -2,19 +2,23 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Person } from '@app/domain/person/person';
-import { FormComponent, InputFailureFeedbackComponent, InvalidFieldDirective, SaveControlsComponent } from '@bernardo-mg/form';
+import { FormComponent, SaveControlsComponent } from '@bernardo-mg/form';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'assoc-people-edition-form',
-  imports: [FormsModule, ReactiveFormsModule, SaveControlsComponent, InputFailureFeedbackComponent, InvalidFieldDirective],
+  imports: [FormsModule, ReactiveFormsModule, InputTextModule, FloatLabelModule, DatePickerModule, MessageModule, SaveControlsComponent],
   templateUrl: './people-edition-form.html'
 })
 export class PeopleEditionForm extends FormComponent<Person> {
 
   constructor() {
-    const fb = inject(FormBuilder);
-
     super();
+
+    const fb = inject(FormBuilder);
 
     this.form = fb.group({
       number: [-1],
