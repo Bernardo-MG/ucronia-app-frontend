5bacf0765a10d31c865dc7b1324caf56d9b5ca5d
Bernardo-MG <programming@bernardomg.com>
2025-09-24 22:50:47 +0200

    Show info

--- src/app/security/user-tokens/user-token-info/user-token-info.ts
+++ src/app/security/user-tokens/user-token-info/user-token-info.ts
@@ -1,19 +1,17 @@
 
+import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { UserToken } from '@bernardo-mg/authentication';
-import { FailureStore } from '@bernardo-mg/request';
 import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'access-user-token-info',
-  imports: [SkeletonModule],
+  imports: [SkeletonModule, DatePipe],
   templateUrl: './user-token-info.html'
 })
 export class UserTokenInfo {
 
   public readonly data = input(new UserToken());
-
-  public readonly failures = input(new FailureStore());
 
   public readonly loading = input(false);
 
--- src/app/security/user-tokens/user-token-list/user-token-list.ts
+++ src/app/security/user-tokens/user-token-list/user-token-list.ts
@@ -1,15 +1,20 @@
+import { DatePipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserTokenService } from '@app/security/user-tokens/user-token-service';
 import { UserToken } from '@bernardo-mg/authentication';
 import { PaginatedResponse, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { DrawerModule } from 'primeng/drawer';
 import { TableModule, TablePageEvent } from 'primeng/table';
 import { finalize } from 'rxjs';
+import { UserTokenInfo } from '../user-token-info/user-token-info';
+import { UserTokenStatus } from '../user-token-status/user-token-status';
 
 @Component({
   selector: 'access-user-token-list',
-  imports: [CardModule, TableModule],
+  imports: [CardModule, TableModule, DrawerModule, ButtonModule, UserTokenInfo, UserTokenStatus, DatePipe],
   templateUrl: './user-token-list.html'
 })
 export class UserTokenList implements OnInit {
@@ -32,9 +37,15 @@ export class UserTokenList implements OnInit {
    * Loading flag.
    */
   public loading = false;
+  public showing = false;
 
   public ngOnInit(): void {
     this.load(0);
+  }
+
+  public onShowInfo(token: UserToken) {
+    this.selectedData = token;
+    this.showing = true;
   }
 
   public onChangeDirection(sorting: { field: string, order: number }) {
