03e631e70005b46420fd1bdd67ac7d181a012b0d
Bernardo-MG <programming@bernardomg.com>
2025-09-27 17:58:46 +0200

    Removed unused code

--- projects/bernardo-mg/ui/src/public-api.ts
+++ projects/bernardo-mg/ui/src/public-api.ts
@@ -2,11 +2,6 @@
  * Public API Surface of ui
  */
 
-export * from './lib/components/button-list/button-list.component';
-export * from './lib/components/card-navigation/card-navigation.component';
-export * from './lib/components/centered-frame/centered-frame.component';
-export * from './lib/components/modal/modal.component';
-export * from './lib/components/router-breadcrumb/router-breadcrumb.component';
 export * from './lib/directives/block-ui.directive';
 export * from './lib/directives/justify-between.directive';
 export * from './lib/directives/justify-center.directive';
