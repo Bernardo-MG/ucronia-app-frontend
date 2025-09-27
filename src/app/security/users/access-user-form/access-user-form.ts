e879a501adb212d334c8014232f26a5735451a91
Bernardo-MG <programming@bernardomg.com>
2025-09-28 01:44:29 +0200

    Cleaned up forms

--- src/app/security/users/access-user-form/access-user-form.ts
+++ src/app/security/users/access-user-form/access-user-form.ts
@@ -1,21 +1,37 @@
 
-import { Component, inject, input } from '@angular/core';
-import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, Input, input, OnChanges, output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormStatus } from '@app/core/form/form-status/form-status';
+import { FeeCreation } from '@app/domain/fees/fee-creation';
 import { User } from '@bernardo-mg/authentication';
-import { FormComponent, InputFailureFeedbackComponent, InvalidFieldDirective, SaveControlsComponent } from '@bernardo-mg/form';
+import { InputFailureFeedbackComponent, InvalidFieldDirective } from '@bernardo-mg/form';
+import { FailureStore } from '@bernardo-mg/request';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'access-user-form',
-  imports: [FormsModule, ReactiveFormsModule, SaveControlsComponent, InputFailureFeedbackComponent, InvalidFieldDirective],
+  imports: [FormsModule, ReactiveFormsModule, ButtonModule, InputFailureFeedbackComponent, InvalidFieldDirective],
   templateUrl: './access-user-form.html'
 })
-export class AccessUserForm extends FormComponent<User> {
+export class AccessUserForm implements OnChanges {
+
+  public readonly loading = input(false);
+
+  public readonly failures = input(new FailureStore());
 
   public readonly create = input(true);
 
-  constructor() {
-    super();
+  @Input() public set data(value: User) {
+    this.form.patchValue(value as any);
+  }
 
+  public readonly save = output<FeeCreation>();
+
+  public formStatus: FormStatus;
+
+  public form: FormGroup;
+
+  constructor() {
     const fb = inject(FormBuilder);
 
     this.form = fb.group({
@@ -23,6 +39,28 @@ export class AccessUserForm extends FormComponent<User> {
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]]
     });
+
+    this.formStatus = new FormStatus(this.form);
+  }
+
+  public ngOnChanges({ loading }: SimpleChanges): void {
+    if (loading) {
+      this.formStatus.loading = this.loading();
+    }
+  }
+
+  /**
+   * Handler for the save event.
+   */
+  public onSave() {
+    if (this.form.valid) {
+      // Valid form, can emit data
+      this.save.emit(this.form.value);
+    }
+  }
+
+  public isFieldInvalid(property: string): boolean {
+    return this.formStatus.isFormFieldInvalid(property) || (this.failures().hasFailures(property));
   }
 
 }
