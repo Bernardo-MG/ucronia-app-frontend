64a3fba7d6098dfc0dacce6bc634b53ea6d19c3d
Bernardo-MG <programming@bernardomg.com>
2025-09-27 18:10:49 +0200

    Updated exports

--- projects/bernardo-mg/ui/src/public-api.ts
+++ projects/bernardo-mg/ui/src/public-api.ts
@@ -2,10 +2,8 @@
  * Public API Surface of ui
  */
 
-export * from './lib/directives/block-ui.directive';
 export * from './lib/directives/justify-between.directive';
 export * from './lib/directives/justify-center.directive';
 export * from './lib/directives/justify-end.directive';
 export * from './lib/directives/responsive-columns.directive';
 export * from './lib/directives/waiting.directive';
-export * from './lib/models/card-tab';
