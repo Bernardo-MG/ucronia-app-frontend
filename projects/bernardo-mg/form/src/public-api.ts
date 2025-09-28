4bb0d4a82f26a7984a4300ba58bcf4a9a20245fb
Bernardo-MG <programming@bernardomg.com>
2025-09-28 02:07:45 +0200

    Deleted unused code

--- projects/bernardo-mg/form/src/public-api.ts
+++ projects/bernardo-mg/form/src/public-api.ts
@@ -3,6 +3,4 @@
  */
 
 export * from './lib/components/form/form.component';
-export * from './lib/components/input-failure-feedback/input-failure-feedback.component';
-export * from './lib/components/save-controls/save-controls.component';
 export * from './lib/directives/invalid-field.directive';
