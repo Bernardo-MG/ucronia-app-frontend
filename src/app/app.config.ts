b68be7127241a429cc710afb9fac2350305ed1c0
Bernardo-MG <programming@bernardomg.com>
2025-09-30 00:25:28 +0200

    Moved to library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { jwtAuthenticationInterceptor, unauthorizedInterceptor } from '@bernardo-mg/authentication';
-import { dateInterceptor } from '@bernardo-mg/request';
+import { dateInterceptor, errorInterceptor } from '@bernardo-mg/request';
 import Aura from '@primeng/themes/aura';
 import { environment } from 'environments/environment';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { errorInterceptor } from './core/request/error-message-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
