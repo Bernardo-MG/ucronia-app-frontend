5bf79c9645ff5961975f91eae7b1a2195080851c
Bernardo-MG <programming@bernardomg.com>
2025-09-30 23:12:36 +0200

    Cleaned up validators

--- projects/bernardo-mg/form/src/lib/isbn-validator/isbn-validator.ts
+++ projects/bernardo-mg/form/src/lib/isbn-validator/isbn-validator.ts
@@ -1,24 +1,22 @@
-import { AbstractControl, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function isbnValidator(): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } | null => {
-    const value = control.value;
-    let result: { [key: string]: any } | null;
+export const isbnValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const value = control.value;
+  let result: ValidationErrors | null;
 
-    if (value) {
-      // Regular expressions for ISBN-10 and ISBN-13 with optional hyphens
-      const isbn10Regex = /^(?:\d{1,5}-\d{1,7}-\d{1,7}-[\dX])$/;
-      const isbn13Regex = /^(?:\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d)$/;
+  if (value) {
+    // Regular expressions for ISBN-10 and ISBN-13 with optional hyphens
+    const isbn10Regex = /^(?:\d{1,5}-\d{1,7}-\d{1,7}-[\dX])$/;
+    const isbn13Regex = /^(?:\d{3}-\d{1,5}-\d{1,7}-\d{1,7}-\d)$/;
 
-      if ((isbn10Regex.test(value)) || (isbn13Regex.test(value))) {
-        result = null;
-      } else {
-        result = { 'invalidISBN': true };
-      }
-    } else {
+    if ((isbn10Regex.test(value)) || (isbn13Regex.test(value))) {
       result = null;
+    } else {
+      result = { 'invalidISBN': true };
     }
+  } else {
+    result = null;
+  }
 
-    return result;
-  };
-}
+  return result;
+};
