91752d06a4c1e62f9badb1c4346fa309426b0056
Bernardo <programming@bernardomg.com>
2025-10-05 00:20:28 +0200

    Token corrections

--- src/app/security/user-tokens/user-token-extend-form/user-token-extend-form.ts
+++ src/app/security/user-tokens/user-token-extend-form/user-token-extend-form.ts
@@ -1,24 +1,33 @@
-import { CommonModule } from '@angular/common';
-import { Component, inject, Input, output } from '@angular/core';
+import { Component, inject, input, Input, output } from '@angular/core';
 import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormStatus } from '@bernardo-mg/form';
+import { FailureStore } from '@bernardo-mg/request';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'access-user-token-extend-form',
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, FloatLabelModule, DatePickerModule, ButtonModule],
+  imports: [FormsModule, ReactiveFormsModule, FloatLabelModule, DatePickerModule, ButtonModule, MessageModule],
   templateUrl: './user-token-extend-form.html'
 })
 export class UserTokenExtendForm {
 
+  public readonly loading = input(false);
+  public readonly failures = input(new FailureStore());
+  
   @Input() public set expirationDate(value: Date) {
     this.form.get('expirationDate')?.setValue(value);
   }
 
+  public formStatus: FormStatus;
+
   public readonly form;
   
   public readonly save = output<Date>();
+
+  public readonly today = new Date();
 
   constructor() {
     const fb = inject(FormBuilder);
@@ -26,6 +35,8 @@ export class UserTokenExtendForm {
     this.form = fb.group({
       expirationDate: [new Date(), Validators.required]
     });
+
+    this.formStatus = new FormStatus(this.form);
   }
   
   public onSave() {
