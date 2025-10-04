30c45bbe8485ec3f55b2fcf6279662029a66db27
Bernardo-MG <programming@bernardomg.com>
2025-10-04 12:07:49 +0200

    Merged components

--- src/app/association/people/people-info/people-info.ts
+++ src/app/association/people/people-info/people-info.ts
@@ -2,18 +2,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { Person } from '@app/domain/person/person';
-import { CardModule } from 'primeng/card';
-import { TabsModule } from 'primeng/tabs';
-import { PeopleInfoDetails } from '../people-info-details/people-info-details';
+import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'assoc-people-info',
-  imports: [CommonModule, CardModule, TabsModule, PeopleInfoDetails],
+  imports: [CommonModule, SkeletonModule],
   templateUrl: './people-info.html'
 })
 export class PeopleInfo {
 
   public readonly data = input(new Person());
+  public readonly loading = input(false);
 
   public get isMember() {
     return this.data().membership !== null;
