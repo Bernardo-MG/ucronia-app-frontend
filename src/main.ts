09992a64a8044c393e08b94a6a6298d906b515fa
Bernardo-MG <programming@bernardomg.com>
2025-11-18 14:03:35 +0100

    Fixed typo

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/app';
 
 if (environment.production) {
   enableProdMode();
