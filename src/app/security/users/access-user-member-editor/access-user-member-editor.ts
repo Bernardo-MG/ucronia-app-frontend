6ed5e7cb9262d17e18b20e19a99f9d946e29c446
Bernardo-MG <programming@bernardomg.com>
2025-12-02 23:48:21 +0100

    Corrections to the form

--- src/app/security/users/access-user-member-editor/access-user-member-editor.ts
+++ src/app/security/users/access-user-member-editor/access-user-member-editor.ts
@@ -1,17 +1,19 @@
 
-import { Component, input, output } from '@angular/core';
+import { Component, inject, input, OnChanges, output, SimpleChanges } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Member } from '@app/domain/members/member';
 import { PaginatedResponse } from '@bernardo-mg/request';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 import { EMPTY, Observable } from 'rxjs';
 import { AccessUserSelectMember } from '../access-user-select-member/access-user-select-member';
 
 @Component({
   selector: 'access-user-member-editor',
-  imports: [ButtonModule, AccessUserSelectMember],
+  imports: [FormsModule, ReactiveFormsModule, ButtonModule, AccessUserSelectMember, InputTextModule],
   templateUrl: './access-user-member-editor.html'
 })
-export class AccessUserMemberEditor {
+export class AccessUserMemberEditor implements OnChanges {
 
   public readonly getSelection = input<(page: number) => Observable<PaginatedResponse<Member>>>((page: number) => EMPTY);
   public readonly getMember = input<(username: string) => Observable<Member>>((username: string) => EMPTY);
@@ -23,6 +25,22 @@ export class AccessUserMemberEditor {
 
   public view: 'member' | 'select' = 'member';
 
+  public form: FormGroup;
+
+  constructor() {
+    const fb = inject(FormBuilder);
+
+    this.form = fb.group({
+      fullName: [{value: '', disabled: true}]
+    });
+  }
+
+  public ngOnChanges({ member }: SimpleChanges): void {
+    if (member) {
+      this.form.get('fullName')?.setValue(member.currentValue.name.fullName);
+    }
+  }
+
   public onShowSelectMember() {
     this.view = "select";
   }
@@ -32,6 +50,7 @@ export class AccessUserMemberEditor {
   }
 
   public onSelectMember(member: Member): void {
+    this.form.get('fullName')?.setValue(member.name.fullName);
     this.assignMember.emit(member);
     this.view = "member";
   }
