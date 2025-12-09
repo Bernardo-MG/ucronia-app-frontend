46f0fca6f38bf0afe414daee3f537cc1a77c9ac2
Bernardo <programming@bernardomg.com>
2025-12-10 00:09:12 +0100

    Extracted detail component

--- projects/bernardo-mg/ui/src/public-api.ts
+++ projects/bernardo-mg/ui/src/public-api.ts
@@ -6,3 +6,4 @@ export * from './lib/calendar/calendar-month/calendar-month';
 export * from './lib/calendar/models/month';
 export * from './lib/social/google-maps/google-maps';
 export * from './lib/social/teamup-calendar/teamup-calendar';
+export * from './lib/details/detail-field/detail-field';
--- src/app/association/members/member-contact-details/member-contact-details.ts
+++ src/app/association/members/member-contact-details/member-contact-details.ts
@@ -5,10 +5,11 @@ import { MemberContact } from '@app/domain/contact/member-contact';
 import { Member } from '@app/domain/members/member';
 import { CardModule } from 'primeng/card';
 import { SkeletonModule } from 'primeng/skeleton';
+import { DetailField } from '../../../../../projects/bernardo-mg/ui/src/public-api';
 
 @Component({
   selector: 'assoc-member-contact-details',
-  imports: [CommonModule, SkeletonModule, CardModule, DatePipe],
+  imports: [CommonModule, SkeletonModule, CardModule, DetailField, DatePipe],
   templateUrl: './member-contact-details.html'
 })
 export class MemberContactDetails {
