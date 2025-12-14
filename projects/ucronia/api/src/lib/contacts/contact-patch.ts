ac65c62e5759ae53a7a890821926ba6419851d58
Bernardo <programming@bernardomg.com>
2025-12-14 23:46:46 +0100

    Corrected model

--- projects/ucronia/api/src/lib/contacts/contact-patch.ts
+++ projects/ucronia/api/src/lib/contacts/contact-patch.ts
@@ -1,9 +1,10 @@
-import { ContactName } from "@ucronia/domain";
+import { ContactChannel, ContactName } from "@ucronia/domain";
 
 export class ContactPatch {
   public number = -1;
   public identifier?: string;
   public birthDate?: Date;
   public name?: ContactName;
+  public contactChannels: ContactChannel[] = [];
   public comments?: string;
 }
