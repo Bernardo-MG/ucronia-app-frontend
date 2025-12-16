543c3183e965198999c206f6afd6410d00da016f
Bernardo <programming@bernardomg.com>
2025-12-16 22:05:15 +0100

    Added test imports

--- src/app/association/contacts/contacts-service.spec.ts
+++ src/app/association/contacts/contacts-service.spec.ts
@@ -1,6 +1,7 @@
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { MessageService } from 'primeng/api';
 import { ContactsService } from './contacts-service';
 
 describe('ContactsService', () => {
@@ -9,6 +10,7 @@ describe('ContactsService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
+        MessageService,
         provideHttpClient(withInterceptorsFromDi()),
         provideHttpClientTesting()
       ]
