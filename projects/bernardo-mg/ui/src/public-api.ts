e849097b548a54e4302fd6d8bae4bce018d39051
Bernardo <programming@bernardomg.com>
2025-12-17 22:00:55 +0100

    Moved component to library

--- projects/bernardo-mg/ui/src/public-api.ts
+++ projects/bernardo-mg/ui/src/public-api.ts
@@ -4,6 +4,7 @@
 
 export * from './lib/calendar/calendar-month/calendar-month';
 export * from './lib/calendar/models/month';
+export * from './lib/details/detail-field/detail-field';
+export * from './lib/details/text-filter/text-filter';
 export * from './lib/social/google-maps/google-maps';
 export * from './lib/social/teamup-calendar/teamup-calendar';
-export * from './lib/details/detail-field/detail-field';
