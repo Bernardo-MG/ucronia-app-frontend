3fa3833e902c2a29c78e368b51b4b7b11ce6f338
Bernardo <programming@bernardomg.com>
2026-01-14 20:32:01 +0100

    Corrected skeleton usage

--- src/app/association/fees/fee-details/fee-details.ts
+++ src/app/association/fees/fee-details/fee-details.ts
@@ -3,12 +3,11 @@ import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
 import { DetailField } from '@bernardo-mg/ui';
 import { Fee } from "@ucronia/domain";
-import { CardModule } from 'primeng/card';
 import { SkeletonModule } from 'primeng/skeleton';
 
 @Component({
   selector: 'assoc-fee-details',
-  imports: [CardModule, SkeletonModule, DetailField, DatePipe],
+  imports: [SkeletonModule, DetailField, DatePipe],
   templateUrl: './fee-details.html'
 })
 export class FeeDetails {
