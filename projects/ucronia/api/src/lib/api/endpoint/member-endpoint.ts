6c15cffb38504782cb0ca4a251d88fd439a2680f
Bernardo <programming@bernardomg.com>
2026-01-21 22:39:56 +0100

    Extracted parsing

--- projects/ucronia/api/src/lib/api/endpoint/member-endpoint.ts
+++ projects/ucronia/api/src/lib/api/endpoint/member-endpoint.ts
@@ -1,8 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { PaginatedResponse, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { PaginatedResponse, Sorting, SortingProperty } from '@bernardo-mg/request';
 import { Member } from '@ucronia/domain';
 import { catchError, Observable } from 'rxjs';
 import { ErrorRequestInterceptor } from '../error-request-interceptor';
+import { toParam } from '../sorting-param-parser';
 
 export class MemberEndpoint {
 
@@ -21,7 +22,7 @@ export class MemberEndpoint {
       params = params.append('page', page);
     }
 
-    this.getFinalProperties(sort.properties, defaultProperties)
+    toParam(sort.properties, defaultProperties)
       .forEach((property) => params = params.append('sort', `${String(property.property)}|${property.direction}`));
 
     params = params.append('name', name);
@@ -30,32 +31,6 @@ export class MemberEndpoint {
       .pipe(
         catchError(this.errorInterceptor.handle)
       );
-  }
-
-  private getFinalProperties(properties: SortingProperty[], defaultProperties: SortingProperty[]): SortingProperty[] {
-    let sortFields;
-
-    // Remove unsorted fields
-    const validSortings = properties.filter(f => f.direction != SortingDirection.Unsorted);
-    if (validSortings.length === 0) {
-      // Use default sorts if no sorting was received
-      sortFields = defaultProperties;
-    } else {
-      // Merge default sorting with the received one
-
-      // Apply default sortings to those fields which are not sorted
-      const sortedProperties = validSortings.map(f => f.property);
-      const defaultSortFields = defaultProperties.filter(f =>
-        (f.direction == SortingDirection.Unsorted) || (!sortedProperties.includes(f.property))
-      );
-
-      sortFields = validSortings.concat(defaultSortFields);
-    }
-
-    // Remove duplicates
-    return sortFields.filter((field, index, self) =>
-      index === self.findIndex(f => f.property === field.property)
-    );
   }
 
 }
---
+++ projects/ucronia/api/src/lib/api/sorting-param-parser.ts
@@ -0,0 +1,27 @@
+import { SortingDirection, SortingProperty } from "@bernardo-mg/request";
+
+  export function toParam(properties: SortingProperty[], defaultProperties: SortingProperty[]): SortingProperty[] {
+    let sortFields;
+
+    // Remove unsorted fields
+    const validSortings = properties.filter(f => f.direction != SortingDirection.Unsorted);
+    if (validSortings.length === 0) {
+      // Use default sorts if no sorting was received
+      sortFields = defaultProperties;
+    } else {
+      // Merge default sorting with the received one
+
+      // Apply default sortings to those fields which are not sorted
+      const sortedProperties = validSortings.map(f => f.property);
+      const defaultSortFields = defaultProperties.filter(f =>
+        (f.direction == SortingDirection.Unsorted) || (!sortedProperties.includes(f.property))
+      );
+
+      sortFields = validSortings.concat(defaultSortFields);
+    }
+
+    // Remove duplicates
+    return sortFields.filter((field, index, self) =>
+      index === self.findIndex(f => f.property === field.property)
+    );
+  }
