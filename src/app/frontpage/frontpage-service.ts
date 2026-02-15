3791c0deccb9ec22db1c44e9560233bbbc635ceb
Bernardo <programming@bernardomg.com>
2026-02-15 21:48:18 +0100

    Read new settings and read once

--- src/app/frontpage/frontpage-service.ts
+++ src/app/frontpage/frontpage-service.ts
@@ -1,5 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { UcroniaClient } from '@ucronia/api';
+import { PublicSettings } from '@ucronia/domain';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
@@ -9,22 +10,8 @@ export class FrontpageService {
 
   private readonly ucroniaClient = inject(UcroniaClient);
 
-  public getCalendar(): Observable<string> {
-    return this.ucroniaClient.setting.public.get()
-      .pipe(map(s => s.calendar));
-  }
-
-  public getMap(): Observable<string> {
-    return this.ucroniaClient.setting.public.get()
-      .pipe(map(s => s.map));
-  }
-
-  public getInstagramUrl(): string {
-    return "https://www.instagram.com/arucronia/";
-  }
-
-  public getEmailUrl(): string {
-    return "mailto:contacto@arucronia.com";
+  public getSettings(): Observable<PublicSettings> {
+    return this.ucroniaClient.setting.public.get();
   }
 
 }
