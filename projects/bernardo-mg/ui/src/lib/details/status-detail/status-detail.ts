f8e6770d43765f5cd7317996f7127cfad11d9be9
Bernardo <programming@bernardomg.com>
2026-05-24 13:29:15 +0200

    Improved display

--- projects/bernardo-mg/ui/src/lib/details/status-detail/status-detail.ts
+++ projects/bernardo-mg/ui/src/lib/details/status-detail/status-detail.ts
@@ -12,14 +12,31 @@ export class StatusField {
   public readonly name = input('');
   public readonly value = input(false);
   public readonly icon = input('');
+  public readonly falseIcon = input<string>();
   public readonly trueText = input('');
   public readonly falseText = input('');
 
   public get color() {
-    if(this.value()) {
+    if (this.value()) {
       return 'green-600';
     } else {
       return 'red-600';
+    }
+  }
+
+  public get actualIcon() {
+    if ((!this.value()) && this.falseIcon()) {
+      return this.falseIcon();
+    } else {
+      return this.icon();
+    }
+  }
+
+  public get text() {
+    if (this.value()) {
+      return this.trueText();
+    } else {
+      return this.falseText();
     }
   }
 
