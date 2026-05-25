9d5841418cb08581a1320f859f14fee0f812939b
Bernardo <programming@bernardomg.com>
2026-05-26 00:12:53 +0200

    Activity list placeholder

---
+++ src/app/association/activity/activity-list/activity-list.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-list',
+  imports: [],
+  templateUrl: './activity-list.html'
+})
+export class ActivityList {
+
+}
--- src/app/association/activity/activity-view/activity-view.ts
+++ src/app/association/activity/activity-view/activity-view.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { PanelModule } from 'primeng/panel';
+import { ActivityList } from '../activity-list/activity-list';
 
 @Component({
-  imports: [],
+  imports: [PanelModule, ActivityList],
   templateUrl: './activity-view.html'
 })
 export class ActivityView {
