6012d1cb4b02d4d719e62f67eb080887534e3af3
Bernardo <programming@bernardomg.com>
2026-06-15 01:28:34 +0200

    Connecting events

--- src/app/association/activities/activity-list/activity-list.ts
+++ src/app/association/activities/activity-list/activity-list.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, inject, input, output } from '@angular/core';
+import { SortingEvent } from '@app/shared/request/sorting-event';
 import { Activity } from '@ucronia/domain';
 import { ConfirmationService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
@@ -12,18 +13,14 @@ import { TableModule, TablePageEvent } from 'primeng/table';
 })
 export class ActivityList {
 
-  private readonly confirmationService = inject(ConfirmationService);
-
   public readonly loading = input(false);
-  public readonly editable = input(false);
-  public readonly deletable = input(false);
   public readonly data = input<Activity[]>([]);
   public readonly rows = input(0);
   public readonly page = input(0);
   public readonly totalRecords = input(0);
-  
-  public readonly edit = output<Activity>();
-  public readonly delete = output<number>();
+
+  public readonly show = output<Activity>();
+  public readonly changeDirection = output<SortingEvent>();
   public readonly changePage = output<number>();
 
   public get first() {
@@ -33,24 +30,6 @@ export class ActivityList {
   public onPageChange(event: TablePageEvent) {
     const page = (event.first / event.rows) + 1;
     this.changePage.emit(page);
-  }
-
-  public confirmDelete(event: Event, activity: Activity) {
-    this.confirmationService.confirm({
-      target: event.currentTarget as EventTarget,
-      message: '¿Estás seguro de querer borrar? Esta acción no es revertible',
-      icon: 'pi pi-info-circle',
-      rejectButtonProps: {
-        label: 'Cancelar',
-        severity: 'secondary',
-        outlined: true
-      },
-      acceptButtonProps: {
-        label: 'Borrar',
-        severity: 'danger'
-      },
-      accept: () => this.delete.emit(activity.number)
-    });
   }
 
 }
--- src/app/association/activities/activity-view/activity-view.ts
+++ src/app/association/activities/activity-view/activity-view.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
+import { SortingEvent } from '@app/shared/request/sorting-event';
 import { AuthService } from '@bernardo-mg/authentication';
+import { Page, Sorting, SortingDirection, SortingProperty } from '@bernardo-mg/request';
+import { Activity } from '@ucronia/domain';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
 import { ActivityList } from '../activity-list/activity-list';
@@ -12,6 +15,12 @@ export class ActivityView {
 
   public readonly permissions: Permissions;
   public readonly Dialog = Dialog;
+  public readonly status: Status = {
+    loading: false
+  };
+
+  public activities = new Page<Activity>();
+  private sort = new Sorting();
 
   public dialog = Dialog.NONE;
 
@@ -26,12 +35,37 @@ export class ActivityView {
     };
   }
 
+  // EVENT HANDLERS
+
+  public onChangeDirection(sorting: SortingEvent) {
+    // TODO: should receive the actual direction, not a number
+    const direction = sorting.order === 1
+      ? SortingDirection.Ascending
+      : SortingDirection.Descending;
+    this.sort.addField(new SortingProperty(sorting.field, direction));
+
+    this.load(this.activities.page);
+  }
+
+  public onShowInfo(activity: Activity) {
+    this.dialog = Dialog.INFO;
+  }
+
+  // DATA LOADING
+
+  public load(page: number | undefined = undefined) {
+  }
+
 }
 
 interface Permissions {
   create: boolean;
   edit: boolean;
   delete: boolean;
+}
+
+interface Status {
+  loading: boolean;
 }
 
 enum Dialog {
