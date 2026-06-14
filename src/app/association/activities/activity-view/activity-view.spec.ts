62b020915c46547ff3b8b416ef944c5b1e3bc32a
Bernardo <programming@bernardomg.com>
2026-06-15 00:54:21 +0200

    Fixed test

--- src/app/association/activities/activity-view/activity-view.spec.ts
+++ src/app/association/activities/activity-view/activity-view.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { ConfirmationService } from 'primeng/api';
 import { ActivityView } from './activity-view';
 
 describe('ActivityView', () => {
@@ -8,7 +9,11 @@ describe('ActivityView', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ActivityView]
+      imports: [ActivityView],
+      providers: [
+        provideAnimationsAsync(),
+        ConfirmationService
+      ]
     })
     .compileComponents();
 
