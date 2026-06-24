4c0f7d10f8afe478b5dcf278674942424e495b67
Bernardo-MG <programming@bernardomg.com>
2026-06-24 09:00:05 +0200

    Fixed tests

--- src/app/association/activities/activity-view/activity-view.spec.ts
+++ src/app/association/activities/activity-view/activity-view.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { Page } from '@bernardo-mg/request';
+import { Activity } from '@ucronia/domain';
 import { ConfirmationService } from 'primeng/api';
+import { of } from 'rxjs';
 import { ActivityService } from '../activity-service';
 import { ActivityView } from './activity-view';
 
@@ -14,6 +17,11 @@ describe('ActivityView', () => {
   );
 
   beforeEach(async () => {
+
+    activityServiceMock.getAll.and.returnValue(
+      of(new Page<Activity>())
+    );
+
     await TestBed.configureTestingModule({
       imports: [ActivityView],
       providers: [
