fa3185eef669b1a363310190729350362b2a3ef1
Bernardo <programming@bernardomg.com>
2026-06-28 01:29:01 +0200

    Hide date range

--- src/app/shared/activity/activity-carousel/activity-carousel.ts
+++ src/app/shared/activity/activity-carousel/activity-carousel.ts
@@ -1,6 +1,6 @@
 import { DatePipe } from '@angular/common';
 import { Component, input, output } from '@angular/core';
-import { Activity } from '@ucronia/domain';
+import { Activity, ActivityDate } from '@ucronia/domain';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { CarouselModule } from 'primeng/carousel';
@@ -11,10 +11,32 @@ import { CarouselModule } from 'primeng/carousel';
   templateUrl: './activity-carousel.html'
 })
 export class ActivityCarousel {
-  
+
   public readonly loading = input(false);
   public readonly data = input<Activity[]>([]);
 
   public readonly show = output<Activity>();
-  
+
+  public readonly expanded = new Set<number>();
+
+  public toggleDates(activity: Activity): void {
+    if (this.expanded.has(activity.number)) {
+      this.expanded.delete(activity.number);
+    } else {
+      this.expanded.add(activity.number);
+    }
+  }
+
+  public isExpanded(activity: Activity): boolean {
+    return this.expanded.has(activity.number);
+  }
+
+  public firstDate(activity: Activity): ActivityDate | undefined {
+    return activity.dates?.[0];
+  }
+
+  public lastDate(activity: Activity): ActivityDate | undefined {
+    return activity.dates?.[activity.dates.length - 1];
+  }
+
 }
