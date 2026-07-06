b8ced560177ff51c0018af2b27dfa479f1418720
Bernardo <programming@bernardomg.com>
2026-07-06 13:55:06 +0200

    Fixed test import

--- src/app/association/scheduled-game/scheduled-game-service.spec.ts
+++ src/app/association/scheduled-game/scheduled-game-service.spec.ts
@@ -1,12 +1,35 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ScheduledGameService } from '../activities/scheduled-game-service';
+import { UcroniaClient } from '@ucronia/api';
+import { MessageService } from 'primeng/api';
+import { of } from 'rxjs';
+import { ScheduledGameService } from './scheduled-game-service';
 
 describe('ScheduledGameService', () => {
   let service: ScheduledGameService;
 
+  const mockUcroniaClient = {
+    scheduledGame: {
+      create: jasmine.createSpy().and.returnValue(of({})),
+      update: jasmine.createSpy().and.returnValue(of({})),
+      get: jasmine.createSpy().and.returnValue(of({})),
+      delete: jasmine.createSpy().and.returnValue(of({})),
+      page: jasmine.createSpy().and.returnValue(of({
+        content: [],
+        page: 0,
+        size: 10,
+        totalElements: 0,
+        totalPages: 0
+      }))
+    }
+  };
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        MessageService,
+        { provide: UcroniaClient, useValue: mockUcroniaClient }
+      ]
+    });
     service = TestBed.inject(ScheduledGameService);
   });
 
