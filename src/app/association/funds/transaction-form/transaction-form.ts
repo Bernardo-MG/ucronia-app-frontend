c29e8455729f223be1d2f1a80835fff4f6529d72
Bernardo <programming@bernardomg.com>
2026-08-31 01:02:12 +0200

    Transaction form

--- src/app/association/funds/transaction-form/transaction-form.ts
+++ src/app/association/funds/transaction-form/transaction-form.ts
@@ -1,4 +1,3 @@
-
 import { Component, inject, input, Input, OnChanges, output, SimpleChanges } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormStatus } from '@bernardo-mg/form';
@@ -6,14 +5,13 @@ import { FailureStore } from '@bernardo-mg/request';
 import { Transaction } from '@ucronia/domain';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
 import { MessageModule } from 'primeng/message';
 import { TextareaModule } from 'primeng/textarea';
 
 @Component({
   selector: 'assoc-transaction-form',
-  imports: [FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule, TextareaModule, FloatLabelModule, DatePickerModule, MessageModule],
+  imports: [FormsModule, ReactiveFormsModule, ButtonModule, TextareaModule, DatePickerModule, InputNumberModule, MessageModule],
   templateUrl: './transaction-form.html'
 })
 export class TransactionForm implements OnChanges {
@@ -23,10 +21,15 @@ export class TransactionForm implements OnChanges {
   public readonly failures = input(new FailureStore());
 
   @Input() public set data(value: Transaction) {
-    this.form.patchValue(value as any);
+    this.form.patchValue({
+      ...value,
+      type: value.amount < 0 ? TransactionType.EXPENSE : TransactionType.INCOME,
+      amount: Math.abs(value.amount)
+    });
   }
 
   public readonly save = output<Transaction>();
+  public readonly cancel = output<void>();
 
   public formStatus: FormStatus;
 
@@ -34,14 +37,17 @@ export class TransactionForm implements OnChanges {
 
   public today = new Date();
 
+  public readonly TransactionType = TransactionType;
+
   constructor() {
     const fb = inject(FormBuilder);
 
     this.form = fb.group({
       index: [0],
+      type: [TransactionType.INCOME, Validators.required],
       description: ['', Validators.required],
       date: [null, Validators.required],
-      amount: [0, Validators.required]
+      amount: [null, [Validators.required, Validators.min(0.01)]]
     });
 
     this.formStatus = new FormStatus(this.form);
@@ -53,18 +59,8 @@ export class TransactionForm implements OnChanges {
     }
   }
 
-  public onMonthSelect(date: Date) {
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-
-    let dateValue;
-    if (month < 10) {
-      dateValue = `${year}-0${month}`;
-    } else {
-      dateValue = `${year}-${month}`;
-    }
-
-    this.form.get('month')?.setValue(dateValue, { emitEvent: false });
+  public setTransactionType(type: TransactionType): void {
+    this.form.get('type')?.setValue(type);
   }
 
   /**
@@ -72,13 +68,23 @@ export class TransactionForm implements OnChanges {
    */
   public onSave() {
     if (this.form.valid) {
-      // Valid form, can emit data
-      this.save.emit(this.form.value);
+      const { type, ...transaction } = this.form.value;
+
+      transaction.amount = type === TransactionType.EXPENSE
+        ? -Math.abs(transaction.amount)
+        : Math.abs(transaction.amount);
+
+      this.save.emit(transaction);
     }
   }
 
   public isFieldInvalid(property: string): boolean {
-    return this.formStatus.isFormFieldInvalid(property) || (this.failures().hasFailures(property));
+    return this.formStatus.isFormFieldInvalid(property) || this.failures().hasFailures(property);
   }
 
+}
+
+enum TransactionType {
+  INCOME = 'income',
+  EXPENSE = 'expense'
 }
