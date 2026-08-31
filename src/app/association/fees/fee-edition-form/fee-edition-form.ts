d6e14316e935a9118efb56c2041ee2d163502c47
Bernardo <programming@bernardomg.com>
2026-08-31 14:05:46 +0200

    Fee edition

--- src/app/association/fees/fee-edition-form/fee-edition-form.ts
+++ src/app/association/fees/fee-edition-form/fee-edition-form.ts
@@ -1,22 +1,18 @@
-
-import { DatePipe } from '@angular/common';
+import { CurrencyPipe, DatePipe } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges, inject, input, output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormStatus } from '@bernardo-mg/form';
 import { FailureStore } from '@bernardo-mg/request';
-import { DetailField, StatusDetail } from '@bernardo-mg/ui';
 import { Fee } from '@ucronia/domain';
 import { ButtonModule } from 'primeng/button';
 import { DatePickerModule } from 'primeng/datepicker';
-import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
-import { InputTextModule } from 'primeng/inputtext';
 import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'assoc-fee-edition-form',
-  imports: [FormsModule, ReactiveFormsModule, ButtonModule, InputTextModule, FloatLabelModule, DatePickerModule, MessageModule, InputGroupModule, InputGroupAddonModule, DetailField, StatusDetail, DatePipe],
+  imports: [CurrencyPipe, DatePipe, ReactiveFormsModule, ButtonModule, DatePickerModule, MessageModule, InputGroupModule, InputGroupAddonModule],
   templateUrl: './fee-edition-form.html'
 })
 export class FeeEditionForm implements OnChanges {
@@ -25,12 +21,12 @@ export class FeeEditionForm implements OnChanges {
   public readonly failures = input(new FailureStore());
 
   @Input() public set data(value: Fee) {
-    this.form.patchValue(value as any);
-    this.onMonthSelect(value.month);
+    this.form.reset({ transaction: { date: value.transaction?.date ?? null } });
     this.fee = value;
   }
 
   public readonly save = output<FeeEditionEvent>();
+  public readonly cancel = output<void>();
 
   public formStatus: FormStatus;
 
@@ -58,29 +54,14 @@ export class FeeEditionForm implements OnChanges {
     }
   }
 
-  public onMonthSelect(date: Date) {
-    // TODO: this is just a patch
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const formatted = `${year}-${month}`;
-
-    if (this.form.get('month')?.value !== formatted) {
-      this.form.get('month')?.setValue(formatted, { emitEvent: false });
-    }
-  }
-
-  /**
-   * Handler for the save event.
-   */
   public onSave() {
     if (this.form.valid) {
-      // Valid form, can emit data
       this.save.emit(this.form.value);
     }
   }
 
   public isFieldInvalid(property: string): boolean {
-    return this.formStatus.isFormFieldInvalid(property) || (this.failures().hasFailures(property));
+    return this.formStatus.isFormFieldInvalid(property) || this.failures().hasFailures(property);
   }
 
 }
