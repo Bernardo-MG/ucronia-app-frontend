3f5b6005f20edff02be7888164254fdd99487ff9
Bernardo <programming@bernardomg.com>
2026-09-09 00:32:58 +0200

    Activities view

--- src/app/association/activities/activity-info/activity-info.ts
+++ src/app/association/activities/activity-info/activity-info.ts
@@ -1,16 +1,20 @@
 import { DatePipe } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { DetailField } from '@bernardo-mg/ui';
-import { Activity } from '@ucronia/domain';
+import { Activity, ActivityDate } from '@ucronia/domain';
+import { TagModule } from 'primeng/tag';
 
 @Component({
   selector: 'assoc-activity-info',
-  imports: [ DetailField, DatePipe ],
+  imports: [DatePipe, TagModule],
   templateUrl: './activity-info.html'
 })
 export class ActivityInfo {
 
   public readonly data = input(new Activity());
   public readonly loading = input(false);
+
+  public isUpcoming(date: ActivityDate): boolean {
+    return new Date(date.end) >= new Date();
+  }
 
 }
